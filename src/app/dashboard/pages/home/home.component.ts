6ecf38e8630e7a18c4e0c9ec458ce0073d5cbf6e
tarasov.m <tarasovm2013@gmail.com>
2019-11-22 11:39:53 +0300

    add connection

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ShiftService} from "../../services/shift.service";
 
 @Component({
   selector: 'evj-home',
@@ -7,10 +8,12 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
+  constructor(shiftService: ShiftService) {
+    shiftService.test();
   }
 
   ngOnInit() {
+
   }
 
 }
--- src/app/dashboard/services/shift.service.ts
+++ src/app/dashboard/services/shift.service.ts
@@ -1,13 +1,33 @@
 import { Injectable } from '@angular/core';
+import {ShiftPass} from '../models/shift.model';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShiftService {
 
-  constructor() { }
+  public shiftPass: ShiftPass;
 
-  public getShift() {
-    
+  private restUrl: string = 'http://192.168.0.4:5888';
+
+  constructor(private http: HttpClient) {
+    // this.restUrl = environment.restUrl;
+    this.getShiftPass();
   }
+
+  private async getShiftPassAsync(): Promise<any>  {
+    return this.http.get(this.restUrl + '/api/Shift').toPromise();
+  }
+
+  public async getShiftPass() {
+    this.shiftPass = await this.getShiftPassAsync();
+    console.log(this.shiftPass);
+  }
+
+  public test() {
+    console.log('test');
+  }
+
 }
