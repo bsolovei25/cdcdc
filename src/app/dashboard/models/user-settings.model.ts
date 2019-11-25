1fbe7e811ace09b03c89f42054a726376e3101b1
Purygin.KE <purygin.ke@gazprom-neft.ru>
2019-11-25 12:36:34 +0300

    screen save

--- src/app/dashboard/models/user-settings.model.ts
+++ src/app/dashboard/models/user-settings.model.ts
@@ -10,4 +10,22 @@ export interface UserGrid {
 }
 
 
+export interface NewUserSettings {
+  userId: number;
+  screenId: number;
+  userGrid: NewUserGrid[];
+}
+
+
+export interface NewUserGrid{
+  posX: number;
+  posY: number;
+  sizeX: number;
+  sizeY: number;
+  widgetId: string;
+  widgetType: string;
+}
+
+
+
 
---
+++ src/app/dashboard/services/new-user-settings.service.ts
@@ -0,0 +1,115 @@
+import { Injectable } from '@angular/core';
+import { NewWidgetService } from './new-widget.service';
+import { NewUserSettings, NewUserGrid } from '../models/user-settings.model';
+import {HttpClient} from '@angular/common/http';
+import {WIDGETS} from '../components/new-widgets-grid/widget-map'
+import { WidgetsService } from './widgets.service';
+import { AppConfigService } from 'src/app/services/appConfigService';
+import { GridsterItem } from 'angular-gridster2';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewUserSettingsService {
+
+  public readonly WIDGETS = WIDGETS;
+
+  constructor(
+    private widgetService: NewWidgetService,
+    private oldWidgetService: WidgetsService,
+    private http: HttpClient,
+    configService: AppConfigService
+  ) {
+    this.restUrl = configService.restUrl;
+   }
+
+  private restUrl: string;
+  
+  public UserId = 1;
+  public ScreenId = 1;
+
+  public addCellByPosition(idWidget, nameWidget, param) {
+    console.log("addcell");
+    this.widgetService.dashboard.push({
+        x: param.x,
+        y: param.y, 
+        cols: WIDGETS[nameWidget].itemCols, 
+        rows: WIDGETS[nameWidget].itemRows, 
+        id: idWidget, 
+        widgetType: nameWidget
+      });
+     this.screenSave();
+  }
+
+  public updateByPosition(obj){
+    debugger
+      for(let item of this.widgetService.dashboard){
+          if(item.id === obj.id){
+            item = obj; 
+          }
+      }
+     console.log("update");
+     this.screenSave();
+     debugger
+  }
+
+  private screenSave() {
+    debugger
+    console.log("save_info",this.widgetService.dashboard);
+    const UserId = this.UserId;
+    const ScreenId = this.ScreenId;
+    let userSettings: NewUserSettings = new class implements NewUserSettings {
+      userId = UserId;
+      screenId = ScreenId;
+      userGrid: NewUserGrid[] = [];
+    };
+    for (const i in this.widgetService.dashboard) {
+      let cell = this.widgetService.dashboard[i];
+      if (cell != null) {
+        let cellSetting: NewUserGrid = new class implements NewUserGrid {
+          widgetId = cell.id;
+          posX = cell.x;
+          posY = cell.y;
+          widgetType = cell.widgetType;
+          sizeX = cell.rows;
+          sizeY = cell.cols;
+        };
+        userSettings.userGrid.push(cellSetting);
+      }else{
+        
+      }
+    }
+    console.log(userSettings);
+
+    this.http.post(this.restUrl + '/user-management/setscreen/', userSettings)
+      .subscribe(
+        ans => {
+          debugger
+          console.log(ans);
+        },
+        error => console.log(error)
+      );
+  } 
+
+  public getUserData(){
+    debugger
+    this.http.get(this.restUrl + '/user-management/getscreen/1/1').subscribe((ref: NewUserSettings) => {
+      debugger
+      console.log(ref);
+
+      for(let item of ref.userGrid){
+       this.widgetService.dashboard.push({
+        x: item.posX,
+        y: item.posY,
+        cols: item.sizeY,
+        rows: item.sizeX,
+        id: item.widgetId, 
+        widgetType: item.widgetType 
+       });
+       debugger
+      }
+    });
+  }
+  
+}
