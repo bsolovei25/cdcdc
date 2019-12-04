41ec43b474b0cc49871abf964168808332d67b75
tarasov.m <tarasovm2013@gmail.com>
2019-12-04 16:30:42 +0300

    unity add

--- src/app/dashboard/widgets/unity-template/unity-template.component.ts
+++ src/app/dashboard/widgets/unity-template/unity-template.component.ts
@@ -17,7 +17,7 @@ export class UnityTemplateComponent implements OnInit {
   private title: string;
   private subscriptions: Subscription[] = [];
 
-  private idContainer: number;
+  private canvas: HTMLCanvasElement;
 
   static itemCols = 15;
   static itemRows = 15;
@@ -28,7 +28,6 @@ export class UnityTemplateComponent implements OnInit {
     @Inject('widgetId') public id: string,
     platformLocation: PlatformLocation
   ) {
-    this.idContainer = this.getRandomInt(1, 20);
     const location = (platformLocation as any).location;
     this.baseUrl = location.origin + location.pathname.replace('dashboard', '');
     this.subscriptions.push(this.widgetService.getWidgetChannel(id).subscribe(data => {
@@ -45,7 +44,9 @@ export class UnityTemplateComponent implements OnInit {
     for (const i in this.subscriptions) {
       this.subscriptions[i].unsubscribe();
     }
-    this.unityInstance.Quit();
+    this.unityInstance.quit(() => {
+      console.log("done!");
+    });
   }
 
   ngAfterViewInit() {
@@ -59,24 +60,26 @@ export class UnityTemplateComponent implements OnInit {
     }
   }
 
-  private getRandomInt(min, max): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
   @HostListener('document:UnityTemplate_Start', ['$event', '$event.detail.param1'])
   private OnUnityStart(event, param1) {
     this.isStart = true;
+    if (!this.unityInstance) {
+      return;
+    }
     this.CallUnityScript('Scripts', 'FromAngular');
   }
 
   @HostListener('document:UnityTemplate_Click', ['$event'])
   private OnUnityClick(event) {
+    if (!this.unityInstance) {
+      return;
+    }
     console.log('click');
   }
 
   private InitUnity() {
     window['UnityLoader'] = UnityLoader;
-    this.loadProject(`${this.baseUrl}assets/unity/webgl_template/webgl_template.json`);
+    this.loadProject(`${this.baseUrl}assets/unity/webgl_template_3d/webgl_template_3d.json`);
     setInterval(() => {
       this.resize();
     }, 500);
@@ -89,15 +92,14 @@ export class UnityTemplateComponent implements OnInit {
   }
 
   private loadProject(path) {
-    console.log('element: ' + document.getElementById('unityContainer' + this.idContainer.toString()));
     this.unityInstance = UnityLoader.instantiate('unityContainer_unity-template', path);
   }
 
   private resize() {
-    const canvas: HTMLCanvasElement = document.getElementById('#canvas') as HTMLCanvasElement;
-    if (canvas) {
-      canvas.width = canvas.parentElement.offsetWidth;
-      canvas.height = canvas.parentElement.offsetHeight;
+    this.canvas = document.getElementById('#canvas') as HTMLCanvasElement;
+    if (this.canvas) {
+      this.canvas.width = this.canvas.parentElement.offsetWidth;
+      this.canvas.height = this.canvas.parentElement.offsetHeight;
     }
   }
 }
