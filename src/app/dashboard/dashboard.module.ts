9b6541af566590918c56d6fb6097d811cb13916e
Alexey Singur <alex.singur1@gmail.com>
2019-12-06 16:26:07 +0300

    prettier

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -15,31 +15,31 @@ import { ManualInputComponent } from "./widgets/manual-input/manual-input.compon
 import { FormsModule } from "@angular/forms";
 import { LineDiagramComponent } from "./widgets/line-diagram/line-diagram.component";
 import { LineDatetimeComponent } from "./components/line-datetime/line-datetime.component";
-import { ShiftPersonComponent } from './widgets/change-shift/shift-person/shift-person.component';
-import { NewWidgetsPanelComponent } from './components/new-widgets-panel/new-widgets-panel.component';
-import { NewWidgetsGridComponent } from './components/new-widgets-grid/new-widgets-grid.component';
-import { GridsterModule } from 'angular-gridster2';
-import {UserInfoComponent} from './components/user-info/user-info.component';
-import { WidgetPiesComponent } from './widgets/widgets-pie/widget-pies/widget-pies/widget-pies.component';
-import { WidgetsPieComponent } from './widgets/widgets-pie/widget-pie-circle/widget-pie/widget-pie.component';
-import { WidgetHeaderComponent } from './components/widget-header/widget-header.component';
-import { EventsWorkSpaceComponent } from './widgets/workspace/events-workspace.component';
-import { FrameTopComponent } from './components/frame-top/frame-top.component';
-import { FrameBottomComponent } from './components/frame-bottom/frame-bottom.component';
-import { ClickOutsideModule } from 'ng-click-outside';
-import { TruncatedPieFirstComponent } from './widgets/truncated-pie-first/truncated-pie-first/truncated-pie-first.component';
-import { TruncatedPieSFirstComponent } from './widgets/truncated-pie-first/truncated-pie-s-first/truncated-pie-s-first.component';
-import { TruncatedPieSIconComponent } from './widgets/truncated-pie-icon/truncated-pie-s-icon/truncated-pie-s-icon.component';
-import { TruncatedPieIconComponent } from './widgets/truncated-pie-icon/truncated-pie-icon/truncated-pie-icon.component';
-import { PointDiagramComponent } from './widgets/point-diagram/point-diagram.component';
-import { OneColumnComponent } from './widgets/point-diagram/one-column/one-column.component';
-import { ProductionPyramidComponent } from './widgets/production-pyramid/production-pyramid.component';
-import { BarChartsComponent } from './widgets/bar-chart/bar-charts/bar-charts.component';
-import { BarChartComponent } from './widgets/bar-chart/bar-chart/bar-chart.component';
-import { MapEcologyComponent } from './widgets/map-ecology/map-ecology/map-ecology.component';
-import { UnityTemplateComponent } from './widgets/unity-template/unity-template.component';
-import { DetailedLineDiagramComponent } from './widgets/detailed-line-diagram/detailed-line-diagram.component';
-import { PowIndexPipe } from './pipes/pow-index.pipe';
+import { ShiftPersonComponent } from "./widgets/change-shift/shift-person/shift-person.component";
+import { NewWidgetsPanelComponent } from "./components/new-widgets-panel/new-widgets-panel.component";
+import { NewWidgetsGridComponent } from "./components/new-widgets-grid/new-widgets-grid.component";
+import { GridsterModule } from "angular-gridster2";
+import { UserInfoComponent } from "./components/user-info/user-info.component";
+import { WidgetPiesComponent } from "./widgets/widgets-pie/widget-pies/widget-pies/widget-pies.component";
+import { WidgetsPieComponent } from "./widgets/widgets-pie/widget-pie-circle/widget-pie/widget-pie.component";
+import { WidgetHeaderComponent } from "./components/widget-header/widget-header.component";
+import { EventsWorkSpaceComponent } from "./widgets/workspace/events-workspace.component";
+import { FrameTopComponent } from "./components/frame-top/frame-top.component";
+import { FrameBottomComponent } from "./components/frame-bottom/frame-bottom.component";
+import { ClickOutsideModule } from "ng-click-outside";
+import { TruncatedPieFirstComponent } from "./widgets/truncated-pie-first/truncated-pie-first/truncated-pie-first.component";
+import { TruncatedPieSFirstComponent } from "./widgets/truncated-pie-first/truncated-pie-s-first/truncated-pie-s-first.component";
+import { TruncatedPieSIconComponent } from "./widgets/truncated-pie-icon/truncated-pie-s-icon/truncated-pie-s-icon.component";
+import { TruncatedPieIconComponent } from "./widgets/truncated-pie-icon/truncated-pie-icon/truncated-pie-icon.component";
+import { PointDiagramComponent } from "./widgets/point-diagram/point-diagram.component";
+import { OneColumnComponent } from "./widgets/point-diagram/one-column/one-column.component";
+import { ProductionPyramidComponent } from "./widgets/production-pyramid/production-pyramid.component";
+import { BarChartsComponent } from "./widgets/bar-chart/bar-charts/bar-charts.component";
+import { BarChartComponent } from "./widgets/bar-chart/bar-chart/bar-chart.component";
+import { MapEcologyComponent } from "./widgets/map-ecology/map-ecology/map-ecology.component";
+import { UnityTemplateComponent } from "./widgets/unity-template/unity-template.component";
+import { DetailedLineDiagramComponent } from "./components/detailed-line-diagram/detailed-line-diagram.component";
+import { PowIndexPipe } from "./pipes/pow-index.pipe";
 
 @NgModule({
   declarations: [
@@ -77,7 +77,7 @@ import { PowIndexPipe } from './pipes/pow-index.pipe';
     UnityTemplateComponent,
     DetailedLineDiagramComponent
   ],
-  entryComponents:[
+  entryComponents: [
     LineChartComponent,
     WidgetPiesComponent,
     EventsComponent,
@@ -92,11 +92,9 @@ import { PowIndexPipe } from './pipes/pow-index.pipe';
     ProductionPyramidComponent,
     BarChartsComponent,
     MapEcologyComponent,
-    UnityTemplateComponent,
+    UnityTemplateComponent
   ],
-  exports: [
-    HomeComponent,
-  ],
+  exports: [HomeComponent],
   imports: [
     CommonModule,
     SharedModule,
@@ -105,7 +103,7 @@ import { PowIndexPipe } from './pipes/pow-index.pipe';
     HttpClientModule,
     FormsModule,
     GridsterModule,
-    ClickOutsideModule,
+    ClickOutsideModule
   ],
   bootstrap: []
 })
