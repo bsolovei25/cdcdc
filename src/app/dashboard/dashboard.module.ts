72f156ce9a92bb07a5dd6805dd9c60ebc0726aac
tarasov.m <tarasovm2013@gmail.com>
2019-12-16 09:55:57 +0300

    upd

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -94,7 +94,7 @@ import { EnergeticsComponent } from './widgets/energetics/energetics.component';
     CalendarPlanComponent,
     EcologySafetyComponent,
     DispatcherScreenComponent,
-    EnergeticsComponent
+    EnergeticsComponent,
   ],
   entryComponents: [
     LineChartComponent,
