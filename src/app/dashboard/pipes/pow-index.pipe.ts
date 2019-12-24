b9e81ee1c741b177240aafdb7f373c6a7a623801
tarasov.m <tarasovm2013@gmail.com>
2019-12-24 12:38:08 +0300

    point-diagram add realtime data + fixes

--- src/app/dashboard/pipes/pow-index.pipe.ts
+++ src/app/dashboard/pipes/pow-index.pipe.ts
@@ -5,16 +5,20 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class PowIndexPipe implements PipeTransform {
   transform(value: string): string {
-    let finalString = "";
-    for (let i = 0; i < value.length; i++) {
-      if (+value[i] && value[i - 1] === "^") {
-        finalString = finalString + "<sup>" + value[i] + "</sup>";
-      } else if (+value[i]) {
-        finalString = finalString + "<sub>" + value[i] + "</sub>";
-      } else {
-        finalString += value[i] !== "^" ? value[i] : "";
+    if (value.match('&')) {
+      let finalString = "";
+      value = value.slice(1);
+      for (let i = 0; i < value.length; i++) {
+        if (+value[i] && value[i - 1] === "^") {
+          finalString = finalString + "<sup>" + value[i] + "</sup>";
+        } else if (+value[i]) {
+          finalString = finalString + "<sub>" + value[i] + "</sub>";
+        } else {
+          finalString += value[i] !== "^" ? value[i] : "";
+        }
       }
+      return finalString;
     }
-    return finalString;
+    return value;
   }
 }
