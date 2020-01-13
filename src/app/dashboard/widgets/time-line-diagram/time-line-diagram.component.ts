e31c5b17123888dff5805e0482666193174e06c6
Alexey Singur <alex.singur1@gmail.com>
2020-01-13 14:22:15 +0300

    add counter

--- src/app/dashboard/widgets/time-line-diagram/time-line-diagram.component.ts
+++ src/app/dashboard/widgets/time-line-diagram/time-line-diagram.component.ts
@@ -8,8 +8,8 @@ import { TimeLineDiagram } from "../../models/time-line-diagram";
 })
 export class TimeLineDiagramComponent implements OnInit {
   data: TimeLineDiagram = {
-    dropTimeNext: Date.now() + 100000,
-    dropInterval: 500000,
+    dropTimeNext: 1578911767856 + 3000000,
+    dropInterval: 1500000,
     dropTitle: "Сброс на факел"
   };
 
@@ -29,17 +29,17 @@ export class TimeLineDiagramComponent implements OnInit {
   ngOnInit() {
     setInterval(() => {
       this.timeLeft = this.data.dropTimeNext - Date.now();
+      const date = new Date(this.timeLeft);
     }, 1000);
   }
 
   timeLine(): string {
-    const now = Date.now();
-    let percent =
-      ((now - (this.data.dropTimeNext - this.data.dropInterval)) /
-        this.data.dropInterval) *
-      100;
-    percent = +percent.toFixed(2);
-    if (percent > 100) percent = 100;
+    let percent = 0;
+    if (this.timeLeft > 0) {
+      percent = 100 - (this.timeLeft / this.data.dropInterval) * 100;
+    } else {
+      percent = 100;
+    }
     return percent + "%";
   }
 }
