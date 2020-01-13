320bfbd779b92df1ec4cb85881e2af44ebed0677
Alexey Singur <alex.singur1@gmail.com>
2020-01-13 12:04:42 +0300

    add timeline

--- src/app/dashboard/widgets/time-line-diagram/time-line-diagram.component.ts
+++ src/app/dashboard/widgets/time-line-diagram/time-line-diagram.component.ts
@@ -1,21 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'evj-time-line-diagram',
-  templateUrl: './time-line-diagram.component.html',
-  styleUrls: ['./time-line-diagram.component.scss']
+  selector: "evj-time-line-diagram",
+  templateUrl: "./time-line-diagram.component.html",
+  styleUrls: ["./time-line-diagram.component.scss"]
 })
 export class TimeLineDiagramComponent implements OnInit {
+  data = {
+    dropTimeNext: Date.now() + 100000,
+    dropInterval: 500000
+  };
 
-  aboutWidget = 'Сброс';
-  units = 'час';
+  public now = 0;
+
+  colorNormal = "#616580";
+  colorNow = "#a2e2ff";
+
+  aboutWidget = "Сброс";
+  units = "час";
 
   isMock = false;
 
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    setInterval(()=>{
+      this.now = Date.now();
+    },1000)
   }
 
+  timeLine() {
+    const now = Date.now();
+    return (
+      ((now - (this.data.dropTimeNext - this.data.dropInterval)) /
+        this.data.dropInterval) *
+        100 +
+      "%"
+    );
+  }
 }
