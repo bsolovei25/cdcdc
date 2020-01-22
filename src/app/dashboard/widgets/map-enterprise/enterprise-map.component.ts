70b8452feea629d86b489dc35cdb331e72f8ed0c
Medvedev <medvedd07@gmail.com>
2020-01-22 17:19:57 +0300

    commit 22

--- src/app/dashboard/widgets/map-enterprise/enterprise-map.component.ts
+++ src/app/dashboard/widgets/map-enterprise/enterprise-map.component.ts
@@ -30,6 +30,238 @@ export interface IEnterpriseMap {
     };
 }
 
+export interface IBuilding {
+    id: number;
+    name: string;
+}
+
+const bulds: IBuilding[] = [
+    {
+        id: 1,
+        name: 'Факельная установка(вод.тушение)'
+    },
+    {
+        id: 4,
+        name: 'АБ-городок Промфинстрой'
+    },
+    {
+        id: 10,
+        name: 'ОПС-14'
+    },
+    {
+        id: 11,
+        name: 'Установка УУГ'
+    },
+    {
+        id: 18,
+        name: 'ОПС-13'
+    },
+    {
+        id: 23,
+        name: 'Установка УПА'
+    },
+    {
+        id: 31,
+        name: 'ТАМЭ'
+    },
+    {
+        id: 42,
+        name: 'ОПС-8'
+    },
+    {
+        id: 24,
+        name: 'АБК-4 Автоматика-Сервис'
+    },
+    {
+        id: 26,
+        name: 'ЭЛОУ-2'
+    },
+    {
+        id: 30,
+        name: 'Реагентное хозяйство'
+    },
+    {
+        id: 33,
+        name: 'ОПС-9'
+    },
+    {
+        id: 43,
+        name: 'ГОБКК'
+    },
+    {
+        id: 35,
+        name: 'Здание ТП-10 УГЭ'
+    },
+    {
+        id: 37,
+        name: 'ОПС-10'
+    },
+    {
+        id: 38,
+        name: 'УПБ'
+    },
+    {
+        id: 39,
+        name: 'ПКК-75/24'
+    },
+    {
+        id: 40,
+        name: 'Раздаточный блок'
+    },
+    {
+        id: 41,
+        name: 'Здание Промжелдортранс'
+    },
+    {
+        id: 46,
+        name: 'ОПС 11'
+    },
+    {
+        id: 44,
+        name: 'ОПС-12'
+    },
+    {
+        id: 32,
+        name: 'ОПС-7'
+    },
+    {
+        id: 49,
+        name: 'БОВ №5'
+    },
+    {
+        id: 50,
+        name: 'УПВ'
+    },
+    {
+        id: 51,
+        name: 'БОВ №4'
+    },
+    {
+        id: 48,
+        name: 'ЗМОС'
+    },
+    {
+        id: 52,
+        name: 'Г-43-107'
+    },
+    {
+        id: 55,
+        name: 'ЦОК-3'
+    },
+    {
+        id: 54,
+        name: 'УОСЩС'
+    },
+    {
+        id: 56,
+        name: 'УПС'
+    },
+    {
+        id: 57,
+        name: 'ОПС-6'
+    },
+    {
+        id: 58,
+        name: 'ОПС-15'
+    },
+    {
+        id: 59,
+        name: 'ЛЧ-24-5'
+    },
+    {
+        id: 60,
+        name: 'Изомеризация'
+    },
+    {
+        id: 62,
+        name: 'ДЕ-100'
+    },
+    {
+        id: 63,
+        name: 'Котельная 4007'
+    },
+    {
+        id: 64,
+        name: 'АБК пр-ва 1'
+    },
+    {
+        id: 65,
+        name: 'ЛЧ-24-2000'
+    },
+    {
+        id: 66,
+        name: 'БОВ-6'
+    },
+    {
+        id: 67,
+        name: 'АСН'
+    },
+    {
+        id: 68,
+        name: 'ГРС'
+    },
+    {
+        id: 78,
+        name: 'ОПС 5'
+    },
+    {
+        id: 77,
+        name: 'ХВО'
+    },
+    {
+        id: 76,
+        name: 'ЛЧ-35/11-1000'
+    },
+    {
+        id: 74,
+        name: 'КЦА'
+    },
+    {
+        id: 92,
+        name: 'ОПС 4'
+    },
+    {
+        id: 89,
+        name: 'МТБЭ'
+    },
+    {
+        id: 83,
+        name: 'ЛЧ-22/4'
+    },
+    {
+        id: 84,
+        name: 'ГФУ-2'
+    },
+    {
+        id: 82,
+        name: 'ЭЛОУ АВТ-6'
+    },
+    {
+        id: 72,
+        name: 'АТ-ВБ'
+    },
+    {
+        id: 81,
+        name: 'АВТ-3'
+    },
+    {
+        id: 80,
+        name: 'ОПС 2'
+    },
+    {
+        id: 79,
+        name: 'БОВ-2'
+    },
+    {
+        id: 69,
+        name: 'БОС'
+    },
+    {
+        id: 25,
+        name: 'Насосная тит.1094'
+    },
+];
+
 @Component({
     selector: 'evj-enterprise-map',
     templateUrl: './enterprise-map.component.html',
@@ -71,7 +303,7 @@ export class EnterpriseMapComponent implements OnInit, OnDestroy, AfterViewInit 
             { id: 38, name: 'ДТ', options: { nonCritical: 1, diagnostics: 0, prognosis: 20 } },
             { id: 41, name: 'Бензин', options: { nonCritical: 25, diagnostics: 6, prognosis: 2 } },
         ],
-        weather: { temperature: 15, direction: 320, wind: 5.1, pressure: 741 },
+        weather: { temperature: 15, direction: 0, wind: 5.1, pressure: 741 },
     };
 
     svgData: any[] = [];
@@ -93,14 +325,20 @@ export class EnterpriseMapComponent implements OnInit, OnDestroy, AfterViewInit 
             this.title = data.title;
             this.previewTitle = data.widgetType;
         });
+
     }
 
-    ngOnInit() {}
+    ngOnInit() {
+
+        bulds.map(val => console.log(val)
+        );
+    }
 
     ngOnDestroy() {
         if (this.subscription) {
             this.subscription.unsubscribe();
         }
+
     }
 
     ngAfterViewInit() {
@@ -116,40 +354,63 @@ export class EnterpriseMapComponent implements OnInit, OnDestroy, AfterViewInit 
         lines.forEach((line) => {
             line.setAttribute(
                 'style',
-                `transform: translate(834.21px, 486.66px) rotate(${data}deg);transform-origin: 84px 84px 0`
+                `transform: translate(874.21px, 472.66px)
+                 rotate(${data}deg);transform-origin: 61px 86px 0`
             );
         });
 
         let atr = '';
-        if (data >= 0 && data <= 90) {
-            if (data === 0) {
-                atr = 'N';
-            } else if (data === 90) {
-                atr = 'E';
-            } else {
-                atr = 'NE';
-            }
+        if (data >= 12 && data <= 33) {
+            atr = 'NNE';
         }
-        if (data >= 91 && data <= 180) {
-            if (data === 180) {
-                atr = 'S';
-            } else {
-                atr = 'ES';
-            }
+        if (data >= 34 && data <= 56) {
+            atr = 'NE';
         }
-        if (data >= 181 && data <= 270) {
-            if (data === 270) {
-                atr = 'W';
-            } else {
-                atr = 'SW';
-            }
+        if (data >= 34 && data <= 56) {
+            atr = 'ENE';
         }
-        if (data >= 271 && data <= 359) {
-            atr = 'WN';
+        if (data >= 57 && data <= 78) {
+            atr = 'E';
+        }
+        if (data >= 102 && data <= 123) {
+            atr = 'ESE';
+        }
+        if (data >= 124 && data <= 146) {
+            atr = 'SE';
+        }
+        if (data >= 147 && data <= 168) {
+            atr = 'SSE';
+        }
+        if (data >= 169 && data <= 191) {
+            atr = 'S';
+        }
+        if (data >= 192 && data <= 213) {
+            atr = 'SSW';
+        }
+        if (data >= 214 && data <= 236) {
+            atr = 'SW';
+        }
+        if (data >= 237 && data <= 258) {
+            atr = 'WSW';
+        }
+        if (data >= 259 && data <= 281) {
+            atr = 'W';
+        }
+        if (data >= 282 && data <= 303) {
+            atr = 'WNW';
+        }
+        if (data >= 304 && data <= 326) {
+            atr = 'NW';
+        }
+        if (data >= 327 && data <= 348) {
+            atr = 'NNW';
+        }
+        if (data >= 349 && data <= 11) {
+            atr = 'N';
         }
         values.forEach((value) => {
             value.append(atr);
-            value.setAttribute('x', `${922 - (atr.length * 2 + 5)}`);
+            value.setAttribute('x', `${930 - (atr.length * 2 + 5)}`);
         });
     }
 
@@ -161,10 +422,10 @@ export class EnterpriseMapComponent implements OnInit, OnDestroy, AfterViewInit 
         buildings.forEach((b) => {
             let value:
                 | {
-                      nonCritical: number;
-                      diagnostics: number;
-                      prognosis: number;
-                  }
+                    nonCritical: number;
+                    diagnostics: number;
+                    prognosis: number;
+                }
                 | undefined;
             const dataId = b.getAttribute('data-id');
             const idx: number = this.data.build.findIndex((d) => d.id === +dataId);
