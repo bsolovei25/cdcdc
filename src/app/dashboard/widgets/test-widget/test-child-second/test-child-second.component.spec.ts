fb47b9d8c2e267e25902aaed3a18f0e917504d56
Alexey Singur <alex.singur1@gmail.com>
2020-02-03 12:31:48 +0300

    prettier fix

--- src/app/dashboard/widgets/test-widget/test-child-second/test-child-second.component.spec.ts
+++ src/app/dashboard/widgets/test-widget/test-child-second/test-child-second.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TestChildSecondComponent } from './test-child-second.component';
 
 describe('TestChildSecondComponent', () => {
-  let component: TestChildSecondComponent;
-  let fixture: ComponentFixture<TestChildSecondComponent>;
+    let component: TestChildSecondComponent;
+    let fixture: ComponentFixture<TestChildSecondComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TestChildSecondComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestChildSecondComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TestChildSecondComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TestChildSecondComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
