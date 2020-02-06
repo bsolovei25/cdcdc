5329b704978d085de5135eb28c547fac1c8160b1
Кирилл Пурыгин <Purygin.KE@gazgolder.ru>
2020-02-06 09:45:22 +0300

    lint

--- src/app/dashboard/services/mbo-data-controller.service.spec.ts
+++ src/app/dashboard/services/mbo-data-controller.service.spec.ts
@@ -3,10 +3,10 @@ import { TestBed } from '@angular/core/testing';
 import { MboDataControllerService } from './mbo-data-controller.service';
 
 describe('MboDataControllerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: MboDataControllerService = TestBed.get(MboDataControllerService);
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        const service: MboDataControllerService = TestBed.get(MboDataControllerService);
+        expect(service).toBeTruthy();
+    });
 });
