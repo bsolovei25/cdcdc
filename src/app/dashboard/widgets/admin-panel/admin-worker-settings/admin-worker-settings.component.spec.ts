d00380fa8f55faa42f1cb65cdcf706671a12cb47
Alexey Singur <alex.singur1@gmail.com>
2020-02-26 16:55:34 +0300

    prettier fix

--- src/app/dashboard/widgets/admin-panel/admin-worker-settings/admin-worker-settings.component.spec.ts
+++ src/app/dashboard/widgets/admin-panel/admin-worker-settings/admin-worker-settings.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AdminWorkerSettingsComponent } from './admin-worker-settings.component';
 
 describe('AdminWorkerSettingsComponent', () => {
-  let component: AdminWorkerSettingsComponent;
-  let fixture: ComponentFixture<AdminWorkerSettingsComponent>;
+    let component: AdminWorkerSettingsComponent;
+    let fixture: ComponentFixture<AdminWorkerSettingsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AdminWorkerSettingsComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AdminWorkerSettingsComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AdminWorkerSettingsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AdminWorkerSettingsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/dashboard/widgets/admin-panel/admin-worker-settings/aws-card/aws-card.component.spec.ts
+++ src/app/dashboard/widgets/admin-panel/admin-worker-settings/aws-card/aws-card.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AwsCardComponent } from './aws-card.component';
 
 describe('AwsCardComponent', () => {
-  let component: AwsCardComponent;
-  let fixture: ComponentFixture<AwsCardComponent>;
+    let component: AwsCardComponent;
+    let fixture: ComponentFixture<AwsCardComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AwsCardComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AwsCardComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AwsCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AwsCardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
