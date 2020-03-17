fdda1e20e605f12b2c6661342a582bf49959285e
tarasov.m <tarasovm2013@gmail.com>
2020-03-17 17:38:16 +0300

    upd

--- src/app/dashboard/widgets/events/events.component.ts
+++ src/app/dashboard/widgets/events/events.component.ts
@@ -1,18 +1,16 @@
-import { Component, Input, OnDestroy, OnInit, Inject, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, Inject, ViewChild } from '@angular/core';
 import {
     EventsWidgetCategory,
     EventsWidgetCategoryCode,
     EventsWidgetDataPreview,
     EventsWidgetNotificationPreview,
     EventsWidgetOptions,
-    ICategory,
 } from '../../models/events-widget';
 import { EventsWidgetFilter } from '../../models/events-widget';
 import {
     EventsWidgetNotification,
     EventsWidgetNotificationStatus,
 } from '../../models/events-widget';
-import { Subscription } from 'rxjs/index';
 import { NewWidgetService } from '../../services/new-widget.service';
 import { NewUserSettingsService } from '../../services/new-user-settings.service';
 import { EventService } from '../../services/event.service';
@@ -143,7 +141,7 @@ export class EventsComponent extends WidgetPlatform implements OnInit, OnDestroy
         },
     ];
 
-    statuses: { [id in EventsWidgetNotificationStatus]: string } = {
+    public statuses: { [id in EventsWidgetNotificationStatus]: string } = {
         new: 'Новое',
         inWork: 'В работе',
         closed: 'Завершено',
--- src/app/dashboard/widgets/petroleum-products-movement/petroleum-products-movement.component.ts
+++ src/app/dashboard/widgets/petroleum-products-movement/petroleum-products-movement.component.ts
@@ -4,22 +4,21 @@ import { NewWidgetService } from '../../services/new-widget.service';
 import { transition, style, animate, trigger } from '@angular/animations';
 import { IOperation } from '../../models/petroleum-products-movement.model';
 import { PetroleumScreenService } from '../../services/petroleum-screen.service';
+import {WidgetPlatform} from "../../models/widget-platform";
 
 @Component({
     selector: 'evj-petroleum-products-movement',
     templateUrl: './petroleum-products-movement.component.html',
     styleUrls: ['./petroleum-products-movement.component.scss'],
 })
-export class PetroleumProductsMovementComponent implements OnInit, OnDestroy {
-    static itemCols: number = 23;
-    static itemRows: number = 16;
+export class PetroleumProductsMovementComponent extends WidgetPlatform implements OnInit, OnDestroy {
+    public static itemCols: number = 23;
+    public static itemRows: number = 16;
 
-    public units: string = '%';
-    public title: string;
-    public code: string;
-    public previewTitle: string;
-
-    private subscriptions: Subscription[] = [];
+    // public units: string = '%';
+    // public title: string;
+    // public code: string;
+    // public previewTitle: string;
 
     public isWorkspace: boolean = true;
 
@@ -27,42 +26,42 @@ export class PetroleumProductsMovementComponent implements OnInit, OnDestroy {
 
     public isUpdateParamButton: boolean = false;
 
-    typeScreen: string = 'info';
+    public typeScreen: string = 'info';
+
+    protected isRealtimeData: boolean = false;
 
     constructor(
-        private widgetService: NewWidgetService,
+        protected widgetService: NewWidgetService,
         private petroleumService: PetroleumScreenService,
         @Inject('isMock') public isMock: boolean,
         @Inject('widgetId') public id: string,
         @Inject('uniqId') public uniqId: string
-    ) {}
+    ) {
+        super(widgetService, isMock, id, uniqId);
+    }
 
-    ngOnInit(): void {
+    public ngOnInit(): void {
+        super.widgetInit();
         this.subscriptions.push(
-            this.widgetService.getWidgetChannel(this.id).subscribe((data) => {
-                this.title = data.title;
-                // this.code = data.code;
-                this.units = data.units;
-                // this.name = data.name;
-                this.previewTitle = data.widgetType;
-            }),
+            // this.widgetService.getWidgetChannel(this.id).subscribe((data) => {
+            //     this.title = data.title;
+            //     // this.code = data.code;
+            //     this.units = data.units;
+            //     // this.name = data.name;
+            //     this.previewTitle = data.widgetType;
+            // }),
 
             this.petroleumService.date$.subscribe((data) => {
                 this.typeScreen = data;
             })
         );
-
-        if (!this.isMock) {
-            //  this.wsConnect();
-        }
     }
 
-    ngOnDestroy(): void {
-        if (this.subscriptions) {
-            for (const subscription of this.subscriptions) {
-                subscription.unsubscribe();
-            }
-        }
+    public ngOnDestroy(): void {
+        super.ngOnDestroy();
+    }
+
+    protected dataHandler(ref: any): void {
     }
 
     onChanged(type: string): void {
@@ -80,7 +79,7 @@ export class PetroleumProductsMovementComponent implements OnInit, OnDestroy {
         }
     }
 
-    onReturn(el: boolean): void {
+    public onReturn(el: boolean): void {
         this.isWorkspace = el;
     }
 }
