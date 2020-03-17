0fcf8201b69b7f1de7cbb70603692223b2dcc659
tarasov.m <tarasovm2013@gmail.com>
2020-03-17 10:18:49 +0300

    upd

---
+++ src/app/dashboard/widgets/change-shift/usb-verifier/usb-verifier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'evj-usb-verifier',
+  templateUrl: './usb-verifier.component.html',
+  styleUrls: ['./usb-verifier.component.scss']
+})
+export class UsbVerifierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
