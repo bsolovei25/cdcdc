1bf3dd2856154ebbf26f7a42e40c23a9e07bf185
Alexey Singur <alex.singur1@gmail.com>
2020-03-24 10:58:08 +0300

    create admin-groups-component

---
+++ src/app/dashboard/widgets/admin-panel/admin-groups/admin-groups.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'evj-admin-groups',
+  templateUrl: './admin-groups.component.html',
+  styleUrls: ['./admin-groups.component.scss']
+})
+export class AdminGroupsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
