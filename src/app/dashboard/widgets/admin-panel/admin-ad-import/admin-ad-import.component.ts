52503df49aecfca5c7d903f3c1662fe5fff14a4f
Alexey Singur <alex.singur1@gmail.com>
2020-04-01 10:36:25 +0300

    fix popup alert

--- src/app/dashboard/widgets/admin-panel/admin-ad-import/admin-ad-import.component.ts
+++ src/app/dashboard/widgets/admin-panel/admin-ad-import/admin-ad-import.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AdminPanelService } from '../../../services/admin-panel/admin-panel.service';
 
 @Component({
@@ -7,9 +7,23 @@ import { AdminPanelService } from '../../../services/admin-panel/admin-panel.ser
     styleUrls: ['./admin-ad-import.component.scss'],
 })
 export class AdminAdImportComponent implements OnInit {
+    @Output() private clickClose: EventEmitter<void> = new EventEmitter<void>();
+
+    private searchedWorker: string = '';
+
+    public searchIcon: string = 'assets/icons/search-icon.svg';
+
     constructor(private adminService: AdminPanelService) {}
 
     public ngOnInit(): void {
         this.adminService.getAllLDAPUsers().subscribe((data) => console.log(data));
+    }
+
+    public onSearchWorker(inputedValue: string): void {
+        this.searchedWorker = inputedValue;
+    }
+
+    public onClose(): void {
+        this.clickClose.emit();
     }
 }
