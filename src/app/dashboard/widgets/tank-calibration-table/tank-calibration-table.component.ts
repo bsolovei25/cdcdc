6d1774d3508a4da82e82e59cbb16798f04de6e1e
Medvedev <medvedd07@gmail.com>
2020-04-15 11:10:44 +0300

    fix

--- src/app/dashboard/widgets/tank-calibration-table/tank-calibration-table.component.ts
+++ src/app/dashboard/widgets/tank-calibration-table/tank-calibration-table.component.ts
@@ -82,13 +82,15 @@ export class TankCalibrationTableComponent extends WidgetPlatform implements OnI
 
     ngOnInit(): void {
         super.widgetInit();
-        if (!this.isMock) {
-            this.loadItem();
-        }
     }
 
     ngOnDestroy(): void {
         super.ngOnDestroy();
+    }
+
+    protected async dataConnect(): Promise<void> {
+        super.dataConnect();
+        this.loadItem();
     }
 
     protected dataHandler(ref: any): void {
