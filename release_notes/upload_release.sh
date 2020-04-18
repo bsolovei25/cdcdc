d8dded316eea055cf2ca79492a19b774eacf6c1f
yakubovskiy.sv@gazprom-neft.ru <serg.yakubovskiy@gmail.com>
2020-04-18 23:18:55 +0300

    all active milestones

--- release_notes/upload_release.sh
+++ release_notes/upload_release.sh
@@ -5,9 +5,9 @@ source set_env.sh
 #TAGNAME="$(curl --header "PRIVATE-TOKEN: ${GITLAB_TOKEN}" https://${GITLAB_HOST}/api/v4/projects/${CI_PROJECT_ID}/repository/tags/ --silent | jq 'sort_by(.commit.created_at) | .[].name ' | tail -n 1)"
 DESCRIPTION_FILE_PATH="${OUTPUT_DESCRIPTION}/$(ls -t1 ${OUTPUT_DESCRIPTION} | head -1)"
 RELEASE_NAME="$TAGNAME"
-RELEASE_NAME=$(ssh deploy.funcoff.club cat /tmp/current_tagname)
-TAGNAME=$(ssh deploy.funcoff.club cat /tmp/current_tagname)
-
+#RELEASE_NAME=$(ssh deploy.funcoff.club cat /tmp/current_tagname)
+#TAGNAME=$(ssh deploy.funcoff.club cat /tmp/current_tagname)
+#
 echo "Tagname: $TAGNAME"
 echo "DESCRIPTION_FILE_PATH: $DESCRIPTION_FILE_PATH"
 DESCRIPTION=''
