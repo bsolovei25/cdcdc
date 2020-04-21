ab1fa78b76d0ee59376d030bc3b4d7257430a8cc
Alexey Singur <alex.singur1@gmail.com>
2020-04-21 11:21:14 +0300

    fix

--- src/app/dashboard/services/avatar-configurator.service.ts
+++ src/app/dashboard/services/avatar-configurator.service.ts
@@ -2,17 +2,14 @@ import { Injectable } from '@angular/core';
 import { AppConfigService } from '../../services/appConfigService';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class AvatarConfiguratorService {
-
     public readonly defaultAvatarPath: string = 'assets/icons/widgets/admin/default_avatar2.svg';
 
-    constructor(private configService: AppConfigService) { }
+    constructor(private configService: AppConfigService) {}
 
     public getAvatarPath(photoId: string): string {
-        console.log('photoId');
-        console.log(photoId);
         if (photoId) {
             return `${this.configService.restUrl}/api/file-storage/${photoId}`;
         }
