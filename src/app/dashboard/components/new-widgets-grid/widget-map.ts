8eb569de315564837e630d78a909dae2d3bd63dd
TarasovMV <31613333+TarasovMV@users.noreply.github.com>
2020-04-23 14:44:06 +0300

    upd

--- src/app/dashboard/components/new-widgets-grid/widget-map.ts
+++ src/app/dashboard/components/new-widgets-grid/widget-map.ts
@@ -48,6 +48,7 @@ import { CustomReportPropertiesReferenceComponent } from '../../widgets/custom-r
 import { ProductionTrendComponent } from '../../widgets/production-trend/production-trend.component';
 import { TankInformationComponent } from '../../widgets/tank-information/tank-information.component';
 import { ReasonsDeviationsComponent } from '../../widgets/reasons-deviations/reasons-deviations.component';
+import { DocumentViewerComponent } from '../../widgets/document-viewer/document-viewer.component';
 
 export const WIDGETS = {
     'pie-diagram': WidgetPiesComponent,
@@ -101,4 +102,5 @@ export const WIDGETS = {
     'production-trend': ProductionTrendComponent,
     'tank-information': TankInformationComponent,
     'reasons-deviations': ReasonsDeviationsComponent,
+    'document-viewer': DocumentViewerComponent,
 };
--- src/app/dashboard/widgets/document-viewer/document-viewer.component.ts
+++ src/app/dashboard/widgets/document-viewer/document-viewer.component.ts
@@ -1,24 +1,94 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { PdfViewerComponent } from 'ng2-pdf-viewer';
+import { WidgetPlatform } from '../../models/widget-platform';
+import { WidgetService } from '../../services/widget.service';
 
 @Component({
   selector: 'evj-document-viewer',
   templateUrl: './document-viewer.component.html',
   styleUrls: ['./document-viewer.component.scss']
 })
-export class DocumentViewerComponent implements OnInit {
+export class DocumentViewerComponent extends WidgetPlatform implements OnInit, OnDestroy {
+    @ViewChild(PdfViewerComponent) pdfViewport: PdfViewerComponent;
 
-    public src: string = 'https://vk.com/away.php?to=https%3A%2F%2Fcloud.mail.ru%2Fpublic%2FNAzt%2FFJpjdhFpZ%2F3%2520%25D1%2581%25D0%25B5%25D0%25BC%25D0%25B5%25D1%2581%25D1%2582%25D1%2580%2F%25D0%259F%25D0%25BE%25D0%25BB%25D0%25B8%25D1%2582%25D0%25BE%25D0%25BB%25D0%25BE%25D0%25B3%25D0%25B8%25D1%258F%2FPolitologia_Albom_skhem_Makarenkov_E_V_Sushkov.pdf&cc_key=';
-    public page: number = 2;
+    public src: string;
+    public page: number;
     public totalPages: number;
+    public isLoading: boolean;
 
-    constructor() { }
+    private _scalePercent: number;
+    public set scalePercent(value: number) {
+        const max = 1;
+        const min = 0.2;
+        if (value > max) {
+            this._scalePercent = max;
+        } else if (value < min) {
+            this._scalePercent = min;
+        } else {
+            this._scalePercent = value;
+        }
+    }
+    public get scalePercent(): number {
+        if (!this._scalePercent) {
+            this._scalePercent = 1;
+        }
+        return this._scalePercent;
+    }
 
-    ngOnInit(): void {
-        setTimeout(() => this.src = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf', 5000);
+    public static itemCols: number = 20;
+    public static itemRows: number = 16;
+
+    constructor(
+        protected widgetService: WidgetService,
+        @Inject('isMock') public isMock: boolean,
+        @Inject('widgetId') public id: string,
+        @Inject('uniqId') public uniqId: string
+    ) {
+        super(widgetService, isMock, id, uniqId);
+        this.isRealtimeData = false;
+    }
+
+    public ngOnInit(): void {
+        super.widgetInit();
+        this.isLoading = true;
+        setTimeout(() => this.src = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf', 3000);
+    }
+
+    public ngOnDestroy(): void {
+        super.ngOnDestroy();
+    }
+
+    @HostListener('document:resize', ['$event'])
+    OnResize(event): void {
+        this.resizePdfViewport();
     }
 
     public afterLoadComplete(pdfData: any): void {
+        this.isLoading = false;
         console.log(pdfData);
         this.totalPages = pdfData.numPages;
+        this.page = 1;
+    }
+
+    public scaleDocument(isBigger: boolean): void {
+        const scaler: number = 0.1;
+        if (isBigger) {
+            this.scalePercent += scaler;
+        } else {
+            this.scalePercent -= scaler;
+        }
+    }
+
+    public search(stringToSearch: string): void {
+        this.pdfViewport.pdfFindController.executeCommand('find', {
+            caseSensitive: false, findPrevious: undefined, highlightAll: true, phraseSearch: true, query: stringToSearch
+        });
+    }
+
+    public resizePdfViewport(): void {
+        this.pdfViewport.onPageResize();
+    }
+
+    protected dataHandler(ref: any): void {
     }
 }
