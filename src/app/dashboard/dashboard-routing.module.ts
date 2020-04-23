6550888772c82cc553be13f42779fc79668a8d8b
TarasovMV <31613333+TarasovMV@users.noreply.github.com>
2020-04-23 15:59:56 +0300

    fix

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -5,14 +5,14 @@ import { DevGraphComponent } from './pages/dev-graph/dev-graph.component';
 // Local modules
 
 export const DashboardRoutes: Routes = [
-    // {
-    //     path: '',
-    //     component: HomeComponent,
-    // },
     {
         path: '',
-        component: DevGraphComponent,
-    }
+        component: HomeComponent,
+    },
+    // {
+    //     path: '',
+    //     component: DevGraphComponent,
+    // }
 ];
 
 export const DashboardRoutingModule = RouterModule.forChild(DashboardRoutes);
