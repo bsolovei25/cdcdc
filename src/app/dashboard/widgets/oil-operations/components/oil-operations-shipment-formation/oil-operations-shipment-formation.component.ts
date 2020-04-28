9e6de466fa971fef6089fa6b259c399494779e09
Purygin.KE <purygin.ke@gazprom-neft.ru>
2020-04-28 13:14:02 +0300

    fix build

--- src/app/dashboard/widgets/oil-operations/components/oil-operations-shipment-formation/oil-operations-shipment-formation.component.ts
+++ src/app/dashboard/widgets/oil-operations/components/oil-operations-shipment-formation/oil-operations-shipment-formation.component.ts
@@ -8,10 +8,15 @@ import { IOilShipment } from 'src/app/dashboard/models/oil-operations';
 })
 export class OilOperationsShipmentFormationComponent implements OnInit {
   @Input() public data: IOilShipment[];
+  public activeItemId: number;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  active(item: IOilShipment): void {
+    (this.activeItemId === item.id) ? this.activeItemId = null : this.activeItemId = item.id;
   }
 
 }
