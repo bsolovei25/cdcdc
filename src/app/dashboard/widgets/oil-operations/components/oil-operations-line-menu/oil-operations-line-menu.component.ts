d632416721a8ac0952641c9cc837c360dd7e2398
Purygin.KE <purygin.ke@gazprom-neft.ru>
2020-04-28 15:00:47 +0300

    oil-operations-line-chart

---
+++ src/app/dashboard/widgets/oil-operations/components/oil-operations-line-menu/oil-operations-line-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'evj-oil-operations-line-menu',
+  templateUrl: './oil-operations-line-menu.component.html',
+  styleUrls: ['./oil-operations-line-menu.component.scss']
+})
+export class OilOperationsLineMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
