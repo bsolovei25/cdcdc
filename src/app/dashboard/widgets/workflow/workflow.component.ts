918a548a825c59b730475a174105e57fe94a43eb
Medvedev <medvedd07@gmail.com>
2020-04-29 14:32:22 +0300

    commit

--- src/app/dashboard/widgets/workflow/workflow.component.ts
+++ src/app/dashboard/widgets/workflow/workflow.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy, Renderer2, ViewChild, ElementRef, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { WidgetService } from '../../services/widget.service';
 import { WidgetPlatform } from '../../models/widget-platform';
 
@@ -7,12 +7,18 @@ import { WidgetPlatform } from '../../models/widget-platform';
     templateUrl: './workflow.component.html',
     styleUrls: ['./workflow.component.scss'],
 })
-export class WorkflowComponent extends WidgetPlatform implements OnInit, OnDestroy {
+export class WorkflowComponent extends WidgetPlatform implements
+    OnInit, OnDestroy, AfterViewInit {
 
     item = 'Таблицы';
 
+    @ViewChild('splitBar') splitBar: ElementRef<HTMLElement>;
+    @ViewChild('splitTop') splitTop: ElementRef<HTMLElement>;
+    @ViewChild('containerWorkflow') containerWorkflow: ElementRef<HTMLElement>;
+
     constructor(
         public widgetService: WidgetService,
+        private renderer: Renderer2,
         @Inject('isMock') public isMock: boolean,
         @Inject('widgetId') public id: string,
         @Inject('uniqId') public uniqId: string
@@ -22,6 +28,39 @@ export class WorkflowComponent extends WidgetPlatform implements OnInit, OnDestr
 
     ngOnInit(): void {
         super.widgetInit();
+    }
+
+    ngAfterViewInit(): void {
+
+        let mouseIsDown = false;
+        this.splitBar.nativeElement.addEventListener('mousedown', (e) => {
+            mouseIsDown = true;
+        });
+
+        document.addEventListener('mousemove', (e) => {
+            if (!mouseIsDown) {
+                return;
+            }
+            const y = this.containerWorkflow.nativeElement.getBoundingClientRect().y;
+            const a = e.screenY - y - 140;
+            if (a < 50) {
+                this.renderer.setStyle(this.splitTop.nativeElement, 'height', `${50}px`);
+            } else {
+                if (a > this.containerWorkflow.nativeElement
+                    .getBoundingClientRect().height - 65) {
+                    const height = this.containerWorkflow.nativeElement
+                        .getBoundingClientRect().height;
+                    const sum = height - 65;
+                    this.renderer.setStyle(this.splitTop.nativeElement, 'height', `${sum}px`);
+                } else {
+                    this.renderer.setStyle(this.splitTop.nativeElement, 'height', `${a}px`);
+                }
+            }
+        });
+
+        document.addEventListener('mouseup', () => {
+            mouseIsDown = false;
+        });
     }
 
     ngOnDestroy(): void {
@@ -34,8 +73,7 @@ export class WorkflowComponent extends WidgetPlatform implements OnInit, OnDestr
         }
     }
 
-
-    chooseSystem(item) {
+    chooseSystem(item): void {
 
     }
 
