2f712df26cd6c06ceba68c83ddabebb0cc85f560
Alexey Singur <alex.singur1@gmail.com>
2020-05-12 13:30:08 +0300

    add main fields

--- src/app/dashboard/widgets/workspace/asus-event/asus-event.component.ts
+++ src/app/dashboard/widgets/workspace/asus-event/asus-event.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { EventsWorkspaceService } from '../../../services/widgets/events-workspace.service';
 
 @Component({
-  selector: 'evj-asus-event',
-  templateUrl: './asus-event.component.html',
-  styleUrls: ['./asus-event.component.scss']
+    selector: 'evj-asus-event',
+    templateUrl: './asus-event.component.html',
+    styleUrls: ['./asus-event.component.scss'],
 })
 export class AsusEventComponent implements OnInit {
+    public reasons: string[] = [
+        'Причина 1',
+        'Причина 2',
+        'Причина 3',
+        'Причина 4',
+        'Причина 5',
+        'Причина 6',
+    ];
 
-  constructor() { }
+    constructor(public ewService: EventsWorkspaceService) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    public compareFn(a, b): boolean {
+        return a && b && a.id === b.id;
+    }
+
+    public onChangeEventDescription(description: string): void {
+        this.ewService.event.description = description;
+    }
+
+    public onSendMessage(message: string, msgType: 'comments' | 'facts'): void {
+        this.ewService.sendMessageToEvent(message, msgType, false);
+    }
 }
