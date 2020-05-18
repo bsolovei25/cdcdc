01c03993e5dbf79b5b025fa15c80354c262d2be6
Alexey Singur <alex.singur1@gmail.com>
2020-05-18 12:18:57 +0300

    fix alert

--- src/app/@shared/components/alert-window/alert-window.component.ts
+++ src/app/@shared/components/alert-window/alert-window.component.ts
@@ -9,11 +9,11 @@ import { IAlertWindowModel } from '@shared/models/alert-window.model';
 export class AlertWindowComponent implements OnInit {
     @Input() public info: IAlertWindowModel;
 
-    public title: string = '';
-    public iconSrc: string = '';
+    public readonly attentionTitle: string = 'Обратите внимание';
+    public readonly editTitle: string = 'Редактирование информации';
 
-    private readonly attentionIcon: string = 'assets/icons/widgets/alert-window/notice.svg';
-    private readonly editIcon: string = 'assets/icons/widgets/alert-window/edit-icon.svg';
+    public readonly attentionIcon: string = 'assets/icons/widgets/alert-window/notice.svg';
+    public readonly editIcon: string = 'assets/icons/widgets/alert-window/edit-icon.svg';
 
     constructor() {}
 
@@ -26,9 +26,6 @@ export class AlertWindowComponent implements OnInit {
                 cancelText: 'Вернуться',
             } as IAlertWindowModel;
         }
-
-        this.title = this.info.input ? 'Редактирование информации' : 'Обратите внимание';
-        this.iconSrc = this.info.input ? this.editIcon : this.attentionIcon;
     }
 
     public accept(): void {
