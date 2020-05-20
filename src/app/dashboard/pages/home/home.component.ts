055473ce3e4e7c70fcccdce7130bb6c4093358e1
Alexey Singur <alex.singur1@gmail.com>
2020-05-20 16:23:14 +0300

    fix

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { WidgetService } from '../../services/widget.service';
 import { UserSettingsService } from '../../services/user-settings.service';
 import { ClaimService } from '../../services/claim.service';
 import { OverlayService } from '../../services/overlay.service';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'evj-home',
@@ -12,11 +13,16 @@ import { OverlayService } from '../../services/overlay.service';
 export class HomeComponent implements OnInit, OnDestroy {
     fullscreen: boolean = false;
 
+    public formControl: FormControl = new FormControl('123', [
+        Validators.required,
+        Validators.minLength(5),
+    ]);
+
     constructor(
         private widgetService: WidgetService,
         private userSettings: UserSettingsService,
         private claimService: ClaimService,
-        public overlayService: OverlayService,
+        public overlayService: OverlayService
     ) {}
 
     ngOnInit(): void {
