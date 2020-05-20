0d518a9bff77f880308c6ca469d4343c4433452e
Alexey Singur <alex.singur1@gmail.com>
2020-05-20 17:49:38 +0300

    delete mock

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -3,7 +3,6 @@ import { WidgetService } from '../../services/widget.service';
 import { UserSettingsService } from '../../services/user-settings.service';
 import { ClaimService } from '../../services/claim.service';
 import { OverlayService } from '../../services/overlay.service';
-import { FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'evj-home',
@@ -12,12 +11,6 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class HomeComponent implements OnInit, OnDestroy {
     fullscreen: boolean = false;
-
-    formControl: FormControl = new FormControl(
-        { value: '123', disabled: true },
-        Validators.minLength(7)
-    );
-    formControl1: FormControl = new FormControl('123', Validators.minLength(7));
 
     constructor(
         private widgetService: WidgetService,
