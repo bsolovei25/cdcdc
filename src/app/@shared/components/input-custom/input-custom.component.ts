a7069bbff999ced83a0fb2ca9949814666228bfb
Alexey Singur <alex.singur1@gmail.com>
2020-05-20 16:23:05 +0300

    add ControlValueAccessor and Validation states

--- src/app/@shared/components/input-custom/input-custom.component.ts
+++ src/app/@shared/components/input-custom/input-custom.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, ElementRef, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 interface IInputIcon {
     src: string;
@@ -8,40 +9,88 @@ interface IInputIcon {
     secState?: string;
 }
 
+interface IInputOptions {
+    type: string;
+    placeholder: string;
+    isMovingPlaceholder: boolean;
+    icon?: IInputIcon;
+}
+
 @Component({
     selector: 'evj-input-custom',
     templateUrl: './input-custom.component.html',
     styleUrls: ['./input-custom.component.scss'],
-})
-export class InputCustomComponent implements OnInit {
-    public type: string = 'text';
-    public placeholder: string = 'Введите текст';
-    public isMovingPlaceholder: boolean = false;
-
-    public icon: IInputIcon = {
-        src: 'assets/icons/login/visibility.svg',
-        svgStyle: { 'width.px': 20, 'height.px': 20 },
-        isClickable: true,
-        onClick: () => {
-            [this.icon.src, this.icon.secState] = [this.icon.secState, this.icon.src];
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => InputCustomComponent),
+            multi: true,
         },
-        secState: 'assets/icons/login/visibility_off.svg',
+    ],
+})
+export class InputCustomComponent implements OnInit, ControlValueAccessor {
+    @ViewChild('input', { static: true }) private input: ElementRef;
+
+    public options: IInputOptions = {
+        type: 'text',
+        placeholder: 'Введите текст',
+        isMovingPlaceholder: true,
+        icon: {
+            src: 'assets/icons/login/visibility.svg',
+            svgStyle: { 'width.px': 20, 'height.px': 20 },
+            isClickable: true,
+            onClick: () => {
+                [this.options.icon.src, this.options.icon.secState] = [
+                    this.options.icon.secState,
+                    this.options.icon.src,
+                ];
+            },
+            secState: 'assets/icons/login/visibility_off.svg',
+        },
     };
 
     public isInput: boolean = false;
 
-    constructor() {}
+    constructor(private renderer: Renderer2) {}
 
     public ngOnInit(): void {}
 
     public onClickIcon(): void {
-        if (this.icon.isClickable && !!this.icon.onClick) {
-            this.icon.onClick();
+        if (this.options.icon.isClickable && !!this.options.icon.onClick) {
+            this.options.icon.onClick();
         }
     }
 
-    public onBlur(event: FocusEvent): void {
-        const elem: HTMLTextAreaElement = event.target as HTMLTextAreaElement;
-        this.isInput = !!elem.value;
+    public onInput(value: string): void {
+        this.writeValue(value);
     }
+
+    public onBlur(value: string): void {
+        this.isInput = !!value;
+        this.onTouched();
+    }
+
+    private onChange: (_: any) => void = () => null;
+    private onTouched: () => void = () => null;
+
+    // #region ControlValueAccessor
+    public writeValue(value: string): void {
+        this.renderer.setProperty(this.input.nativeElement, 'value', value);
+        this.isInput = !!value;
+
+        this.onChange(value);
+    }
+
+    public registerOnChange(fn: (_: any) => void): void {
+        this.onChange = fn;
+    }
+
+    public registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
+
+    public setDisabledState(isDisabled: boolean): void {
+        this.renderer.setProperty(this.input.nativeElement, 'disabled', isDisabled);
+    }
+    //#endregion ControlValueAccessor
 }
