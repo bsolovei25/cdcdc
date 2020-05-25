f3f9d9a891ff434b34efb21dccae3df592411b08
Purygin.KE <purygin.ke@gazprom-neft.ru>
2020-05-25 11:04:35 +0300

    quality stock

--- src/app/dashboard/widgets/SMP/quality-stock/components/quality-stock-circle/quality-stock-circle.component.ts
+++ src/app/dashboard/widgets/SMP/quality-stock/components/quality-stock-circle/quality-stock-circle.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild, Input, OnChanges, ChangeDetectionStrategy } from '@angular/core';
 import * as d3 from 'd3';
 import { IQualityStockCircle } from '../../quality-stock.component';
+import { IEnergeticsCircleDiagram, IEnergeticsLimits, IEnergeticsCoordinates, IEnergeticsEndsLine } from 'src/app/dashboard/models/energetics';
 
 @Component({
   selector: 'evj-quality-stock-circle',
@@ -13,11 +14,73 @@ export class QualityStockCircleComponent implements OnInit, OnChanges {
 
   @Input() data: IQualityStockCircle;
 
-  public readonly RADIUS: number = 30;
+  public energyCircleDiagram: IEnergeticsCircleDiagram = {
+    lowerLimit: 80, // нижний предел на диаграмме в %
+    upperLimit: 90, // верхний предел на диаграмме в %
+    termo: 102, // процентная доля тепловой энергии
+    electro: 87, // процентная доля электро энергии
+    fuel: 125, // процентная доля топлива
+  };
 
-  public defaultPercent: number = 100;
+  /* Координаты центров окружностей */
 
-  public svg: any;
+  public centerX: string = '25';
+  public centerY: string = '25';
+
+  /* Цвета для диаграмм */
+
+  public colorMain: string = 'var(--color-bg-header-table)';
+  public colorBg: string = 'rgba(91, 96, 125, 0.6)';
+  public colorNormal: string = 'var(--color-text-main)';
+  public colorFull: string = 'var(--color-standard)';
+  public colorDeviation: string = 'var(--color-standard)';
+
+  /* Радиусы диаграмм */
+
+  public fuelRadius: string = (15.91549430918954).toString();
+  public electroRadius: string = (15.91549430918954 + 3).toString();
+  public termoRadius: string = (15.91549430918954 + 6).toString();
+  public radPoint: string = '0.8';
+
+  public dataN = {
+    circleDiagram: { lowerLimit: 80, upperLimit: 90, termo: 102, electro: 0, fuel: 0 },
+    curValue: 10000,
+    currentValue: 10000,
+    electroCard: {
+      cardType: "electro",
+      curValue: 3000,
+      currentValue: 3000,
+      deviation1: 142.1,
+      deviation2: 0.1,
+      plan: 1000,
+    },
+    fuelCard: {
+      cardType: "fuel",
+      curValue: 620,
+      currentValue: 620,
+      deviation1: 142.1,
+      deviation2: 0.1,
+      plan: 1000,
+    },
+    higherBorder: 0.1,
+    higherValue: 1100,
+    lowerBorder: 0.2,
+    lowerValue: 800,
+    maxValue: 1500,
+    plan: 1000,
+    termoCard: {
+      cardType: "termo",
+      curValue: 1900,
+      currentValue: 1900,
+      deviation1: 142.1,
+      deviation2: 0.1,
+      plan: 1000
+    },
+    title: "Энергетика",
+    unitsOfMeasure: "индекс",
+    widgetType: "energetics",
+  }
+
 
   constructor() { }
 
@@ -25,134 +88,103 @@ export class QualityStockCircleComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(): void {
-    if (this.svg) {
-      this.svg.remove();
-    }
-    this.d3Circle(this.data, this.circle.nativeElement);
+    this.dataN.lowerBorder =
+      Math.abs(this.dataN.lowerBorder - this.dataN.plan) / this.dataN.plan;
+    this.dataN.higherBorder =
+      Math.abs(this.dataN.higherBorder - this.dataN.plan) / this.dataN.plan;
+    this.dataN.lowerValue = this.dataN.plan * (1 - this.dataN.lowerBorder);
+    this.dataN.higherValue = this.dataN.plan * (1 + this.dataN.higherBorder);
+    this.energyCircleDiagram = this.dataN.circleDiagram;
   }
 
+  /* Отрисовка дуговых диаграмм */
 
-  public d3Circle(data, el): void {
-    const summ = this.defaultPercent - (data?.factPercent ?? 0 - data?.deviationPercent ?? 0);
-    const mass = [data?.factPercent, data?.deviationPercent, summ];
-    let color: any;
+  diaCounter(r: string): string {
+    const c: number = 2 * Math.PI * +r;
+    return 0.75 * c + ' ' + 0.25 * c;
+  }
 
-    if (summ === 0) {
-      color = d3.scaleOrdinal().range(['gray']);
-    } else {
-      color = d3.scaleOrdinal().range(['var(--color-text-main)', 'var(--color-deviation)', 'var(--color-bg-main)']);
+  diaLine(r: string, line: number): string {
+    const c: number = 2 * Math.PI * +r;
+    const percent: number = line / 100;
+    let lineLength: number = percent * 0.5 * c;
+    if (lineLength > 0.75 * c) {
+      lineLength = 0.75 * c;
     }
+    return lineLength + ' ' + (c - lineLength);
+  }
 
-    this.svg = d3
-      .select(el)
-      .append('svg')
-      .attr('min-width', '100px')
-      .attr('viewBox', '0 0 100 100');
-
-    let group = this.svg.append('g').attr('transform', 'translate(50 ,43)');
-
-    const arc = d3
-      .arc()
-      .innerRadius(28.5)
-      .outerRadius(this.RADIUS);
-
-    const pie = d3
-      .pie()
-      .value((d) => {
-        return d;
-      })
-      .sort(() => null);
-
-    const arcs = group
-      .selectAll('.arc')
-      .data(pie(mass))
-      .enter()
-      .append('g')
-      .attr('class', 'arc');
-
-    arcs.append('path')
-      .attr('d', arc)
-      .attr('fill', (d) => color(d.index));
-
-    group = group
-      .append('text')
-      .attr('text-anchor', 'middle')
-      .attr('font-size', '8px')
-      .attr('fill', 'var(--color-plan)')
-      .attr('dominant-baseline', 'middle')
-      .text(data?.value + '%');
-
-    const line = this.svg
-      .append('rect')
-      .attr('fill', 'var(--color-plan)')
-      .attr('height', '6px')
-      .attr('width', '1px')
-      .attr('x', '50')
-      .attr('y', '11');
-
-    const background = this.svg
-      .append('image')
-      .attr(
-        'xlink:href',
-        './assets/icons/widgets/SMP/border-blue-circle.svg'
-      )
-      .attr('height', '70%')
-      .attr('width', '100%')
-      .attr('x', '0')
-      .attr('y', '8');
-
-    const title = this.svg
-      .append('text')
-      .attr('font-size', '6.5px')
-      .attr('text-anchor', 'middle')
-      .attr('font-family', "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;")
-      .attr('y', '87')
-      .attr('x', '50')
-      .attr('fill', 'var(--color-text-main')
-      .text('Отклонение');
-
-    if (this.data?.deviationStatus) {
-      const value = this.svg
-        .append('text')
-        .attr('font-size', '8px')
-        .attr('font-family', "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;")
-        .attr('y', '97')
-        .attr('x', '32')
-        .attr('fill', 'var(--color-text-main')
-        .text('+ ' + this.data?.deviation);
-
-      const arrow_up = this.svg
-        .append('image')
-        .attr(
-          'xlink:href',
-          './assets/icons/widgets/SMP/double-arrow-top.svg'
-        )
-        .attr('height', '7px')
-        .attr('width', '7px')
-        .attr('y', '90')
-        .attr('x', '58');
-    } else {
-      const value = this.svg
-        .append('text')
-        .attr('font-size', '8px')
-        .attr('font-family', "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;")
-        .attr('y', '97')
-        .attr('x', '32')
-        .attr('fill', 'var(--color-text-main')
-        .text('- ' + this.data?.deviation);
-
-      const arrow_down = this.svg
-        .append('image')
-        .attr(
-          'xlink:href',
-          './assets/icons/widgets/SMP/double-arrow-top.svg'
-        )
-        .attr('style', 'transform: scale(-1)')
-        .attr('height', '7px')
-        .attr('width', '7px')
-        .attr('y', '-98')
-        .attr('x', '-68');
+  diaOffset(r: string, line: number): string {
+    const c: number = 2 * Math.PI * +r;
+    const percent: number = line / 100;
+    let lineLength: number = percent * 0.5 * c;
+    if (lineLength > 0.75 * c) {
+      lineLength = 0.75 * c;
     }
+    return (-0.75 * c + lineLength).toString();
+  }
+
+  diaLimits(line: number): IEnergeticsLimits {
+    const newLine = 100 - line; // отсчет угла от 100%
+    const t = (Math.PI * newLine) / 100 + Math.PI / 2;
+    const rMin = 13;
+    const rMax = 25;
+    const limitLine: IEnergeticsLimits = {
+      x1: (rMin * Math.cos(t) + +this.centerX).toString(),
+      y1: (rMin * Math.sin(t) + +this.centerY).toString(),
+      x2: (rMax * Math.cos(t) + +this.centerX).toString(),
+      y2: (rMax * Math.sin(t) + +this.centerY).toString(),
+    };
+    return limitLine;
+  }
+
+  diaLimitsLabels(line: number, isLowerLimit: boolean = false): IEnergeticsCoordinates {
+    const coords = this.diaLimits(line);
+    let returnedCoords: IEnergeticsCoordinates;
+    if (isLowerLimit) {
+      returnedCoords = {
+        x: (+coords.x2 - 5).toString(),
+        y: (+coords.y2 + 3).toString(),
+      };
+    } else {
+      returnedCoords = {
+        x: coords.x2,
+        y: (+coords.y2 + 3).toString(),
+      };
+    }
+    return returnedCoords;
+  }
+
+  diaFill(percent: number): string {
+    if (percent < this.energyCircleDiagram.lowerLimit) {
+      return this.colorDeviation;
+    }
+    if (
+      percent >= this.energyCircleDiagram.lowerLimit &&
+      percent < this.energyCircleDiagram.upperLimit
+    ) {
+      return this.colorNormal;
+    }
+    if (percent === this.energyCircleDiagram.upperLimit) {
+      return this.colorFull;
+    }
+    if (percent > this.energyCircleDiagram.upperLimit) {
+      return this.colorDeviation;
+    }
+  }
+
+  diaEndsLine(line: number, rad: string): IEnergeticsEndsLine {
+    let newLine = 100 - line + +this.radPoint; // отсчет угла от 100%
+    if (newLine < -50) {
+      newLine = -50 + +this.radPoint * 2;
+    }
+    const t = (Math.PI * newLine) / 100 + Math.PI / 2;
+    const r = +rad;
+    const limitLine: IEnergeticsEndsLine = {
+      xCen: (r * Math.cos(t) + +this.centerX).toString(),
+      yCen: (r * Math.sin(t) + +this.centerY).toString(),
+    };
+    return limitLine;
   }
 
 }
