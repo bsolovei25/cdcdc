57f24ebb5bd0f95a4ee1cd2496e87499f1b80665
Konstantin Medvedev <medvedd07@gmail.com>
2020-06-02 13:13:25 +0300

    fix

--- src/app/dashboard/widgets/workflow/workflow-table/workflow-table.component.ts
+++ src/app/dashboard/widgets/workflow/workflow-table/workflow-table.component.ts
@@ -81,15 +81,13 @@ export class WorkflowTableComponent implements OnInit, AfterContentInit {
 
     onNoClick(): void {}
 
-    doubleClick(element): void {}
-
     searchInput(event): void {
         this.dataSource = this.dataSourceLocal?.filter(
             (val) =>
-                val?.firstName?.toLowerCase().includes(event?.target?.value.toLowerCase()) ||
-                val?.lastName?.toLowerCase().includes(event?.target?.value.toLowerCase()) ||
-                val?.middleName?.toLowerCase().includes(event?.target?.value.toLowerCase()) ||
-                val?.positionDescription?.toLowerCase().includes(event?.target?.value.toLowerCase())
+                val?.firstName?.toLowerCase().includes(event?.toLowerCase()) ||
+                val?.lastName?.toLowerCase().includes(event?.toLowerCase()) ||
+                val?.middleName?.toLowerCase().includes(event?.toLowerCase()) ||
+                val?.positionDescription?.toLowerCase().includes(event?.toLowerCase())
         );
     }
 
