6ceaabe107217393d251241d806c656caf38139e
TarasovMV <31613333+TarasovMV@users.noreply.github.com>
2020-06-03 16:29:35 +0300

    upd

--- src/app/dashboard/widgets/tank-information/components/tank-card/tank-card.component.ts
+++ src/app/dashboard/widgets/tank-information/components/tank-card/tank-card.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit, Input, AfterViewInit, ChangeDetectionStrategy, OnChanges } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    Input,
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    OnChanges
+} from '@angular/core';
 import { ITankCardValue } from 'src/app/dashboard/models/tank-information';
 import { TooltipService } from '@shared/components/tooltip/service/tooltip.service';
 
 @Component({
-  selector: 'evj-tank-card',
-  templateUrl: './tank-card.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  styleUrls: ['./tank-card.component.scss']
+    selector: 'evj-tank-card',
+    templateUrl: './tank-card.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    styleUrls: ['./tank-card.component.scss']
 })
 export class TankCardComponent implements OnInit, OnChanges {
     @Input() public data: ITankCardValue;
@@ -15,7 +22,11 @@ export class TankCardComponent implements OnInit, OnChanges {
     public heightCard: number;
     public heightValue: number = 20;
 
+<<<<<<< Updated upstream
     public readonly operation: any = {
+=======
+    operation = {
+>>>>>>> Stashed changes
         filling: 'Заполнение',
         shipment: 'Отгрузка',
         standart: 'Без изменений',
@@ -28,12 +39,30 @@ export class TankCardComponent implements OnInit, OnChanges {
         work: 'В работе'
     };
 
+<<<<<<< Updated upstream
     constructor(private tooltipService: TooltipService) { }
 
     ngOnChanges(): void {
         this.heightCard = (this.data.attributes.length) * this.heightValue + 40;
         this.tooltipService.close();
     }
+=======
+    constructor(private tooltipService: TooltipService) {
+    }
+
+    ngOnChanges(): void {
+        if (this.data.fillLevelPercentage > 100) {
+            this.data.fillLevelPercentage = 100;
+        } else if (this.data.fillLevelPercentage < 0) {
+            this.data.fillLevelPercentage = 0;
+        }
+        this.heightCard = (this.data.attributes.length) * this.heightValue + 40;
+        this.tooltipService.close();
+    }
+
+    ngOnInit(): void {
+    }
+>>>>>>> Stashed changes
 
     ngOnInit(): void {
     }
