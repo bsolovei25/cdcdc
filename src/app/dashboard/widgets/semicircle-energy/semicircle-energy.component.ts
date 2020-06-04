3dcfc1118f993c77cec63c50a7710fb737521410
Alexey Singur <alex.singur1@gmail.com>
2020-06-04 12:51:15 +0300

    fix data and add loader

--- src/app/dashboard/widgets/semicircle-energy/semicircle-energy.component.ts
+++ src/app/dashboard/widgets/semicircle-energy/semicircle-energy.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { WidgetService } from '../../services/widget.service';
-import { Subscription } from 'rxjs';
 import {
     EnergyCircleDiagram,
     Production,
@@ -16,6 +15,8 @@ import { WidgetPlatform } from '../../models/widget-platform';
     styleUrls: ['./semicircle-energy.component.scss'],
 })
 export class SemicircleEnergyComponent extends WidgetPlatform implements OnInit, OnDestroy {
+    public isDataLoading: boolean = true;
+
     /* Параметры для круговых диаграмм */
 
     energyCircleDiagram: EnergyCircleDiagram = {
@@ -35,8 +36,8 @@ export class SemicircleEnergyComponent extends WidgetPlatform implements OnInit,
     productionList: Production[] = [
         {
             name: '',
-            plan: 106886,
-            fact: 54789,
+            plan: 0,
+            fact: 0,
         },
         {
             name: '',
@@ -103,6 +104,9 @@ export class SemicircleEnergyComponent extends WidgetPlatform implements OnInit,
     }
 
     dataHandler(ref: SemicircleEnergy): void {
+        if (this.isDataLoading) {
+            setTimeout(() => (this.isDataLoading = false), 500);
+        }
         this.copyData(ref);
         this.logoType();
         this.warningControl();
