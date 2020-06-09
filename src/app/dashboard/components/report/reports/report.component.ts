3efee3bbd39514dff2719753af158e73d79d4b86
TarasovMV <31613333+TarasovMV@users.noreply.github.com>
2020-06-09 14:39:17 +0300

    add smotr graph

--- src/app/dashboard/components/report/reports/report.component.ts
+++ src/app/dashboard/components/report/reports/report.component.ts
@@ -187,7 +187,9 @@ export class ReportComponent implements OnInit {
         };
         try {
             const a = await this.reportsService.postTemplate(template.id, body);
-            window.open(`${this.restUrl}/api/file-storage/${a.data.fileId}`);
+            // window.open(`${this.restUrl}/api/file-storage/${a.data.fileId}`);
+            const downloadUrl = `${this.restUrl}/api/file-storage/${a.data.fileId}`;
+            window.open(downloadUrl, 'download_window', 'toolbar=0,location=no,directories=0,status=0,scrollbars=0,resizeable=0,width=1,height=1,top=0,left=0');
             this.isLoading = false;
         } catch (error) {
             this.snackBar.openSnackBar('Файл не сформирован', 'snackbar-red');
