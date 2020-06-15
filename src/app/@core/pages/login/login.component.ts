562e981d46980d659bbb9146487e904a28c15a85
TarasovMV <31613333+TarasovMV@users.noreply.github.com>
2020-06-15 14:39:45 +0300

    Update login.component.ts

--- src/app/@core/pages/login/login.component.ts
+++ src/app/@core/pages/login/login.component.ts
@@ -61,15 +61,6 @@ export class LoginComponent implements OnInit, AfterViewInit {
         this.router.routeReuseStrategy.shouldReuseRoute = () => {
             return false;
         };
-
-        this.router.events.subscribe((evt) => {
-            if (evt instanceof NavigationEnd) {
-                // trick the Router into believing it's last link wasn't previously loaded
-                this.router.navigated = false;
-                // if you need to scroll back to top, here is the right place
-                window.scrollTo(0, 0);
-            }
-        });
     }
 
     ngOnInit(): void {
