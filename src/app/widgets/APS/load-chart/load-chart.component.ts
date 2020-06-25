f8f7e1d49b9ba2c1488b90b40ab5dc38c6c4276d
Alexey Singur <alex.singur1@gmail.com>
2020-06-25 16:59:16 +0300

    add grid

--- src/app/widgets/APS/load-chart/load-chart.component.ts
+++ src/app/widgets/APS/load-chart/load-chart.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    Inject,
+    ViewChild,
+    ElementRef,
+    Renderer2,
+    AfterViewInit,
+} from '@angular/core';
 import { WidgetPlatform } from '../../../dashboard/models/widget-platform';
 import { WidgetService } from '../../../dashboard/services/widget.service';
 
@@ -7,9 +15,13 @@ import { WidgetService } from '../../../dashboard/services/widget.service';
     templateUrl: './load-chart.component.html',
     styleUrls: ['./load-chart.component.scss'],
 })
-export class LoadChartComponent extends WidgetPlatform implements OnInit {
+export class LoadChartComponent extends WidgetPlatform implements OnInit, AfterViewInit {
+    @ViewChild('grid_hor', { static: true }) private gridHorizontal: ElementRef;
+    @ViewChild('grid_ver', { static: true }) private gridVertical: ElementRef;
+
     constructor(
         protected widgetService: WidgetService,
+        private renderer: Renderer2,
         @Inject('isMock') public isMock: boolean,
         @Inject('widgetId') public id: string,
         @Inject('uniqId') public uniqId: string
@@ -21,7 +33,29 @@ export class LoadChartComponent extends WidgetPlatform implements OnInit {
         super.widgetInit();
     }
 
+    ngAfterViewInit(): void {
+        this.createGrid();
+    }
+
     protected dataHandler(ref: any): void {}
+
+    private createGrid(): void {
+        const horLines = 6;
+        const verLines = 33;
+
+        for (let i = 0; i < horLines; i++) {
+            const lineHor = this.renderer.createElement('div');
+            this.renderer.addClass(lineHor, 'line-hor');
+            this.renderer.appendChild(this.gridHorizontal, lineHor);
+        }
+
+        for (let i = 0; i < verLines; i++) {
+            const lineVer = this.renderer.createElement('div');
+            this.renderer.addClass(lineVer, 'line-ver');
+            this.renderer.appendChild(this.gridVertical, lineVer);
+        }
+        // this.renderer.appendChild(this.gridHorizontal,)
+    }
 
     public onClick(): void {
         console.log('click');
