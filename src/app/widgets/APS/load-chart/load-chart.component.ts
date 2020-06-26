487486f2dbf5a4431a3e4ea790da875592801bc5
Alexey Singur <alex.singur1@gmail.com>
2020-06-26 12:27:15 +0300

    add simple canvas chart

--- src/app/widgets/APS/load-chart/load-chart.component.ts
+++ src/app/widgets/APS/load-chart/load-chart.component.ts
@@ -6,6 +6,7 @@ import {
     ElementRef,
     Renderer2,
     AfterViewInit,
+    HostListener,
 } from '@angular/core';
 import { WidgetPlatform } from '../../../dashboard/models/widget-platform';
 import { WidgetService } from '../../../dashboard/services/widget.service';
@@ -18,6 +19,211 @@ import { WidgetService } from '../../../dashboard/services/widget.service';
 export class LoadChartComponent extends WidgetPlatform implements OnInit, AfterViewInit {
     @ViewChild('grid_hor', { static: true }) private gridHor: ElementRef;
     @ViewChild('grid_ver', { static: true }) private gridVer: ElementRef;
+    @ViewChild('canvas', { static: true }) private canvas: ElementRef;
+    @ViewChild('chartContainer', { static: true }) private container: ElementRef;
+
+    private data: { value: number; date: Date }[] = [
+        {
+            value: 130,
+            date: new Date(2020, 1, 1, 4),
+        },
+        {
+            value: 10,
+            date: new Date(2020, 1, 1, 12),
+        },
+        {
+            value: 210,
+            date: new Date(2020, 1, 1, 21),
+        },
+        {
+            value: 130,
+            date: new Date(2020, 1, 2, 4),
+        },
+        {
+            value: 130,
+            date: new Date(2020, 1, 2, 12),
+        },
+        {
+            value: 210,
+            date: new Date(2020, 1, 2, 21),
+        },
+        {
+            value: 210,
+            date: new Date(2020, 1, 2, 22),
+        },
+        {
+            value: 210,
+            date: new Date(2020, 1, 2, 23),
+        },
+        {
+            value: 30,
+            date: new Date(2020, 1, 3, 4),
+        },
+        {
+            value: 10,
+            date: new Date(2020, 1, 3, 12),
+        },
+        {
+            value: 210,
+            date: new Date(2020, 1, 3, 21),
+        },
+        {
+            value: 130,
+            date: new Date(2020, 1, 4, 4),
+        },
+        {
+            value: 10,
+            date: new Date(2020, 1, 4, 12),
+        },
+        {
+            value: 10,
+            date: new Date(2020, 1, 4, 21),
+        },
+        {
+            value: 130,
+            date: new Date(2020, 1, 5, 4),
+        },
+        {
+            value: 190,
+            date: new Date(2020, 1, 5, 12),
+        },
+        {
+            value: 210,
+            date: new Date(2020, 1, 5, 21),
+        },
+        {
+            value: 160,
+            date: new Date(2020, 1, 6, 4),
+        },
+        {
+            value: 120,
+            date: new Date(2020, 1, 7, 12),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 8, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 9, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 10, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 11, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 12, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 13, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 14, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 15, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 16, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 17, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 18, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 19, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 20, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 21, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 22, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 23, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 24, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 25, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 26, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 27, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 28, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 29, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 30, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 31, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 32, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 33, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 34, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 35, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 36, 21),
+        },
+        {
+            value: 0,
+            date: new Date(2020, 1, 37, 21),
+        },
+    ];
+
+    private transformedData = [];
+
+    private readonly colWidth: number = 37;
 
     constructor(
         protected widgetService: WidgetService,
@@ -29,34 +235,104 @@ export class LoadChartComponent extends WidgetPlatform implements OnInit, AfterV
         super(widgetService, isMock, id, uniqId);
     }
 
-    ngOnInit(): void {
+    public ngOnInit(): void {
         super.widgetInit();
+        this.getData();
     }
 
-    ngAfterViewInit(): void {
+    public ngAfterViewInit(): void {
         this.createGrid();
+        this.drawChart();
     }
 
     protected dataHandler(ref: any): void {}
+
+    @HostListener('document:resize')
+    onResize(): void {
+        this.drawChart();
+    }
+
+    private getData(): void {
+        const arr = [];
+        let date = null;
+        let dateArr = null;
+        this.data.forEach((item) => {
+            if (
+                date &&
+                item.date.getDate() === date.getDate() &&
+                item.date.getMonth() === date.getMonth()
+            ) {
+                dateArr.push(item);
+            } else if (date && dateArr) {
+                arr.push(dateArr);
+                dateArr = [];
+                dateArr.push(item);
+                date = new Date(item.date.getFullYear(), item.date.getMonth(), item.date.getDate());
+            } else {
+                dateArr = [];
+                dateArr.push(item);
+                date = new Date(item.date.getFullYear(), item.date.getMonth(), item.date.getDate());
+            }
+        });
+
+        this.transformedData = arr.map((item) => {
+            const average = item.reduce((acc, point, index, array) => {
+                if (array.length - 1 === index) {
+                    return (acc + point.value) / array.length;
+                }
+                return acc + point.value;
+            }, 0);
+            const newDate = item[0].date;
+            return {
+                value: average,
+                date: new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate()),
+            };
+        });
+    }
 
     private createGrid(): void {
         const horLines = 6;
         const verLines = 33;
 
-        for (let i = 0; i < horLines; i++) {
-            const lineHor = this.renderer.createElement('div');
-            this.renderer.addClass(lineHor, 'line-hor');
-            this.renderer.appendChild(this.gridHor.nativeElement, lineHor);
-        }
+        const appendLine = (elemRef: ElementRef, type: 'hor' | 'ver') => {
+            const line = this.renderer.createElement('div');
+            this.renderer.addClass(line, `line-${type}`);
+            this.renderer.appendChild(elemRef.nativeElement, line);
+        };
 
         for (let i = 0; i < verLines; i++) {
-            const lineVer = this.renderer.createElement('div');
-            this.renderer.addClass(lineVer, 'line-ver');
-            this.renderer.appendChild(this.gridVer.nativeElement, lineVer);
+            if (i < horLines) {
+                appendLine(this.gridHor, 'hor');
+            }
+            appendLine(this.gridVer, 'ver');
         }
     }
 
-    public onClick(): void {
-        console.log('click');
+    private drawChart(): void {
+        const canv = this.canvas.nativeElement as HTMLCanvasElement;
+        const ctx = canv.getContext('2d') as CanvasRenderingContext2D;
+
+        const width = this.transformedData.length * this.colWidth;
+        const height = this.gridHor.nativeElement.offsetHeight;
+
+        ctx.clearRect(0, 0, width, height);
+
+        canv.setAttribute('height', `${height}`);
+        canv.setAttribute('width', `${width}`);
+
+        const step = this.colWidth / 2;
+
+        ctx.beginPath();
+
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = '#3fa9f5';
+
+        ctx.moveTo(0, this.transformedData[0].value);
+        this.transformedData.forEach((point, index) => {
+            ctx.lineTo(index * this.colWidth + step, point.value);
+        });
+        ctx.stroke();
+
+        ctx.closePath();
     }
 }
