088d2af1abde734509abf6b084e9eee253c6f605
Alexey Singur <alex.singur1@gmail.com>
2020-07-07 09:40:54 +0300

    add widgetPlatform

--- src/app/widgets/APS/indicator-load-deviation/indicator-load-deviation.component.ts
+++ src/app/widgets/APS/indicator-load-deviation/indicator-load-deviation.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { WidgetPlatform } from '../../../dashboard/models/widget-platform';
+import { WidgetService } from '../../../dashboard/services/widget.service';
 
 @Component({
-  selector: 'evj-indicator-load-deviation',
-  templateUrl: './indicator-load-deviation.component.html',
-  styleUrls: ['./indicator-load-deviation.component.scss']
+    selector: 'evj-indicator-load-deviation',
+    templateUrl: './indicator-load-deviation.component.html',
+    styleUrls: ['./indicator-load-deviation.component.scss'],
 })
-export class IndicatorLoadDeviationComponent implements OnInit {
+export class IndicatorLoadDeviationComponent extends WidgetPlatform implements OnInit, OnDestroy {
+    constructor(
+        protected widgetService: WidgetService,
+        @Inject('isMock') public isMock: boolean,
+        @Inject('widgetId') public id: string,
+        @Inject('uniqId') public uniqId: string
+    ) {
+        super(widgetService, isMock, id, uniqId);
+    }
 
-  constructor() { }
+    public ngOnInit(): void {
+        super.widgetInit();
+    }
 
-  ngOnInit(): void {
-  }
+    public ngOnDestroy(): void {
+        super.ngOnDestroy();
+    }
 
+    protected dataHandler(ref: any): void {}
 }
