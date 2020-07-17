dc01a96f68b4bbe02b093091aea9fe9e293ea390
Medvedev <medvedd07@gmail.com>
2020-07-17 15:51:17 +0300

    complete cumulative-chart

--- src/app/widgets/KPE/kpe-quality/kpe-quality.module.ts
+++ src/app/widgets/KPE/kpe-quality/kpe-quality.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KpeQualityComponent } from './kpe-quality.component';
 import { KpeLineChartComponent } from '../shared/kpe-line-chart/kpe-line-chart.component';
+import { KpeSharedModule } from '../shared/kpe-shared.module';
+import { KpeLineDiagramComponent } from '../shared/kpe-line-diagram/kpe-line-diagram.component';
 
 @NgModule({
   declarations: [KpeQualityComponent, KpeLineChartComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, KpeSharedModule],
 })
 export class KpeQualityModule {
   enterComponent = KpeQualityComponent;
