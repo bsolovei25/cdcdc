77325f58405472943608a27073a1d4a881b3b3a8
Medvedev <medvedd07@gmail.com>
2020-07-20 08:48:13 +0300

    commit

--- src/app/widgets/KPE/kpe-quality/kpe-quality.module.ts
+++ src/app/widgets/KPE/kpe-quality/kpe-quality.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KpeQualityComponent } from './kpe-quality.component';
-import { KpeLineChartComponent } from '../shared/kpe-line-chart/kpe-line-chart.component';
 import { KpeSharedModule } from '../shared/kpe-shared.module';
-import { KpeLineDiagramComponent } from '../shared/kpe-line-diagram/kpe-line-diagram.component';
 
 @NgModule({
-  declarations: [KpeQualityComponent, KpeLineChartComponent],
+  declarations: [KpeQualityComponent],
   imports: [CommonModule, KpeSharedModule],
 })
 export class KpeQualityModule {
