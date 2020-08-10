35acd4ee1c163f479a0d267af0babdb274d3713f
Alexey Singur <alex.singur1@gmail.com>
2020-08-10 17:33:34 +0300

    change chart

--- src/app/widgets/ASTUE-ONPZ/astue-onpz-product-charts/components/astue-onpz-product-card/astue-onpz-product-card.component.ts
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-product-charts/components/astue-onpz-product-card/astue-onpz-product-card.component.ts
@@ -1,15 +1,232 @@
 import { Component, OnInit } from '@angular/core';
+import { IProductionTrend } from '../../../../../dashboard/models/production-trends.model';
 
 @Component({
-  selector: 'evj-astue-onpz-product-card',
-  templateUrl: './astue-onpz-product-card.component.html',
-  styleUrls: ['./astue-onpz-product-card.component.scss']
+    selector: 'evj-astue-onpz-product-card',
+    templateUrl: './astue-onpz-product-card.component.html',
+    styleUrls: ['./astue-onpz-product-card.component.scss'],
 })
 export class AstueOnpzProductCardComponent implements OnInit {
+    public data: IProductionTrend[] = [
+        {
+            graphType: 'plan',
+            graphStyle: 'main',
+            graph: [
+                {
+                    value: 1000,
+                    timeStamp: new Date(2020, 1, 1),
+                },
+                {
+                    value: 700,
+                    timeStamp: new Date(2020, 1, 2),
+                },
+                {
+                    value: 900,
+                    timeStamp: new Date(2020, 1, 3),
+                },
+                {
+                    value: 800,
+                    timeStamp: new Date(2020, 1, 4),
+                },
+                {
+                    value: 1200,
+                    timeStamp: new Date(2020, 1, 5),
+                },
+                {
+                    value: 1100,
+                    timeStamp: new Date(2020, 1, 6),
+                },
+                {
+                    value: 800,
+                    timeStamp: new Date(2020, 1, 7),
+                },
+                {
+                    value: 1000,
+                    timeStamp: new Date(2020, 1, 8),
+                },
+            ],
+        },
+        {
+            graphType: 'fact',
+            graphStyle: 'main',
+            graph: [
+                {
+                    value: 800,
+                    timeStamp: new Date(2020, 1, 1),
+                },
+                {
+                    value: 1000,
+                    timeStamp: new Date(2020, 1, 2),
+                },
+                {
+                    value: 200,
+                    timeStamp: new Date(2020, 1, 3),
+                },
+                {
+                    value: 900,
+                    timeStamp: new Date(2020, 1, 4),
+                },
+                {
+                    value: 800,
+                    timeStamp: new Date(2020, 1, 5),
+                },
+                {
+                    value: 900,
+                    timeStamp: new Date(2020, 1, 6),
+                },
+                {
+                    value: 1600,
+                    timeStamp: new Date(2020, 1, 7),
+                },
+                {
+                    value: 1200,
+                    timeStamp: new Date(2020, 1, 8),
+                },
+            ],
+        },
+        {
+            graphType: 'higherBorder',
+            graphStyle: 'area',
+            graph: [
+                {
+                    value: 1300,
+                    timeStamp: new Date(2020, 1, 1),
+                },
+                {
+                    value: 1400,
+                    timeStamp: new Date(2020, 1, 2),
+                },
+                {
+                    value: 1500,
+                    timeStamp: new Date(2020, 1, 3),
+                },
+                {
+                    value: 1100,
+                    timeStamp: new Date(2020, 1, 4),
+                },
+                {
+                    value: 1300,
+                    timeStamp: new Date(2020, 1, 5),
+                },
+                {
+                    value: 1400,
+                    timeStamp: new Date(2020, 1, 6),
+                },
+                {
+                    value: 1200,
+                    timeStamp: new Date(2020, 1, 7),
+                },
+                {
+                    value: 1500,
+                    timeStamp: new Date(2020, 1, 8),
+                },
+                {
+                    value: 1100,
+                    timeStamp: new Date(2020, 1, 9),
+                },
+                {
+                    value: 1500,
+                    timeStamp: new Date(2020, 1, 10),
+                },
+                {
+                    value: 1400,
+                    timeStamp: new Date(2020, 1, 11),
+                },
+                {
+                    value: 1200,
+                    timeStamp: new Date(2020, 1, 12),
+                },
+                {
+                    value: 1400,
+                    timeStamp: new Date(2020, 1, 13),
+                },
+                {
+                    value: 1100,
+                    timeStamp: new Date(2020, 1, 14),
+                },
+                {
+                    value: 1500,
+                    timeStamp: new Date(2020, 1, 15),
+                },
+                {
+                    value: 1400,
+                    timeStamp: new Date(2020, 1, 16),
+                },
+            ],
+        },
+        {
+            graphType: 'lowerBorder',
+            graphStyle: 'area',
+            graph: [
+                {
+                    value: 500,
+                    timeStamp: new Date(2020, 1, 1),
+                },
+                {
+                    value: 600,
+                    timeStamp: new Date(2020, 1, 2),
+                },
+                {
+                    value: 400,
+                    timeStamp: new Date(2020, 1, 3),
+                },
+                {
+                    value: 500,
+                    timeStamp: new Date(2020, 1, 4),
+                },
+                {
+                    value: 700,
+                    timeStamp: new Date(2020, 1, 5),
+                },
+                {
+                    value: 400,
+                    timeStamp: new Date(2020, 1, 6),
+                },
+                {
+                    value: 500,
+                    timeStamp: new Date(2020, 1, 7),
+                },
+                {
+                    value: 800,
+                    timeStamp: new Date(2020, 1, 8),
+                },
+                {
+                    value: 500,
+                    timeStamp: new Date(2020, 1, 9),
+                },
+                {
+                    value: 500,
+                    timeStamp: new Date(2020, 1, 10),
+                },
+                {
+                    value: 400,
+                    timeStamp: new Date(2020, 1, 11),
+                },
+                {
+                    value: 700,
+                    timeStamp: new Date(2020, 1, 12),
+                },
+                {
+                    value: 500,
+                    timeStamp: new Date(2020, 1, 13),
+                },
+                {
+                    value: 600,
+                    timeStamp: new Date(2020, 1, 14),
+                },
+                {
+                    value: 300,
+                    timeStamp: new Date(2020, 1, 15),
+                },
+                {
+                    value: 500,
+                    timeStamp: new Date(2020, 1, 16),
+                },
+            ],
+        },
+    ];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
--- src/app/widgets/ASTUE-ONPZ/astue-onpz-shared/components/limits-chart/limits-chart.component.ts
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-shared/components/limits-chart/limits-chart.component.ts
@@ -1,15 +1,321 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnChanges, HostListener } from '@angular/core';
+import * as d3Selection from 'd3-selection';
+import * as d3 from 'd3';
+import {
+    IProductionTrend,
+    ProductionTrendType,
+} from '../../../../../dashboard/models/production-trends.model';
+import { IChartD3, IChartMini } from '../../../../../@shared/models/smart-scroll.model';
 
 @Component({
-  selector: 'evj-limits-chart',
-  templateUrl: './limits-chart.component.html',
-  styleUrls: ['./limits-chart.component.scss']
+    selector: 'evj-limits-chart',
+    templateUrl: './limits-chart.component.html',
+    styleUrls: ['./limits-chart.component.scss'],
 })
-export class LimitsChartComponent implements OnInit {
+export class LimitsChartComponent implements OnChanges {
+    @Input() private data: IProductionTrend[] = [];
 
-  constructor() { }
+    @ViewChild('chart', { static: true }) private chart: ElementRef;
 
-  ngOnInit(): void {
-  }
+    private chartData: {
+        graphType: ProductionTrendType;
+        graph: IChartD3[];
+    }[] = [];
 
+    private svg;
+
+    private graphMaxX: number = 0;
+    private graphMaxY: number = 0;
+    private dataMax: number = 0;
+    private dataMin: number = 0;
+
+    public scaleFuncs: { x: any; y: any } = { x: null, y: null };
+    private axis: { axisX: any; axisY: any } = { axisX: null, axisY: null };
+
+    private readonly MAX_COEF: number = 0.1;
+    private readonly MIN_COEF: number = 0.3;
+
+    private readonly padding: { left: number; right: number; top: number; bottom: number } = {
+        left: 50,
+        right: 30,
+        top: 40,
+        bottom: 30,
+    };
+
+    constructor() {}
+
+    public ngOnChanges(): void {
+        if (this.data.length) {
+            this.startDrawChart();
+        }
+    }
+
+    @HostListener('document:resize', ['$event'])
+    public OnResize(): void {
+        if (this.data.length) {
+            this.startDrawChart();
+        }
+    }
+
+    private startDrawChart(): void {
+        this.initData();
+        this.findMinMax();
+        this.defineScale();
+        this.transformData();
+        this.drawGridlines();
+        this.drawChart();
+        this.drawAxisYLabels();
+        this.drawAxisXLabels();
+        this.drawPoints();
+        this.customizeAreas();
+    }
+
+    private initData(): void {
+        if (this.svg) {
+            this.svg.remove();
+            this.svg = undefined;
+        }
+
+        this.svg = d3Selection.select(this.chart.nativeElement).append('svg');
+
+        this.graphMaxX = +d3Selection
+            .select(this.chart.nativeElement)
+            .style('width')
+            .slice(0, -2);
+        this.graphMaxY = +d3Selection
+            .select(this.chart.nativeElement)
+            .style('height')
+            .slice(0, -2);
+
+        this.svg
+            .attr('width', '100%')
+            .attr('height', '100%')
+            .attr('viewBox', `0 0 ${this.graphMaxX} ${this.graphMaxY - 5}`);
+    }
+
+    private findMinMax(): void {
+        const maxValues: number[] = [];
+        const minValues: number[] = [];
+
+        this.data.forEach((graph) => {
+            maxValues.push(d3.max(graph.graph, (item: IChartMini) => item.value));
+            minValues.push(d3.min(graph.graph, (item: IChartMini) => item.value));
+        });
+
+        this.dataMax = d3.max(maxValues) * (1 + this.MAX_COEF);
+        this.dataMin = d3.min(minValues) * (1 - this.MIN_COEF);
+    }
+
+    private defineScale(): void {
+        const plan = this.data.find((chart) => chart.graphType === 'higherBorder');
+
+        const domainDates = d3.extent(plan.graph, (item: IChartMini) => item.timeStamp);
+        const rangeX = [this.padding.left, this.graphMaxX - this.padding.right];
+
+        this.scaleFuncs.x = d3
+            .scaleTime()
+            .domain(domainDates)
+            .rangeRound(rangeX);
+
+        const domainValues = [this.dataMax, this.dataMin];
+        const rangeY = [this.padding.top, this.graphMaxY - this.padding.bottom];
+        this.scaleFuncs.y = d3
+            .scaleLinear()
+            .domain(domainValues)
+            .range(rangeY);
+
+        this.axis.axisX = d3
+            .axisBottom(this.scaleFuncs.x)
+            .ticks(12)
+            .tickFormat(d3.timeFormat('%d'))
+            .tickSizeOuter(0);
+        this.axis.axisY = d3
+            .axisLeft(this.scaleFuncs.y)
+            .ticks(5)
+            .tickSize(0);
+    }
+
+    private transformData(): void {
+        this.chartData = [];
+        this.data.forEach((item) => this.transformOneChartData(item));
+    }
+
+    private transformOneChartData(chart: IProductionTrend): void {
+        const chartData: {
+            graphType: ProductionTrendType;
+            graph: IChartD3[];
+        } = {
+            graphType: chart.graphType,
+            graph: [],
+        };
+
+        chart.graph.forEach((item) => {
+            chartData.graph.push({
+                x: this.scaleFuncs.x(item.timeStamp),
+                y: this.scaleFuncs.y(item.value),
+            });
+        });
+
+        this.chartData.push(chartData);
+    }
+
+    private drawChart(): void {
+        this.chartData.forEach((chart) => {
+            const line = d3
+                .line()
+                .x((item: IChartD3) => item.x)
+                .y((item: IChartD3) => item.y);
+
+            const areaBottom = d3
+                .area()
+                .x((item: IChartD3) => item.x)
+                .y0(this.graphMaxY - this.padding.bottom)
+                .y1((item: IChartD3) => item.y);
+
+            const areaTop = d3
+                .area()
+                .x((item: IChartD3) => item.x)
+                .y0((item: IChartD3) => item.y)
+                .y1(this.padding.top);
+
+            const opacity: number = 1;
+
+            this.svg
+                .append('path')
+                .attr('class', `graph-line-${chart.graphType}`)
+                .attr('d', line(chart.graph));
+
+            if (chart.graphType === 'higherBorder' || chart.graphType === 'lowerBorder') {
+                const areaFn = chart.graphType === 'lowerBorder' ? areaBottom : areaTop;
+                this.svg
+                    .append('path')
+                    .attr('class', `graph-area-${chart.graphType}`)
+                    .attr('d', areaFn(chart.graph));
+            }
+        });
+    }
+
+    private drawPoints(): void {
+        const pointsG = this.svg.append('g').attr('class', 'chart-points');
+        this.chartData.forEach((item) => {
+            if (item.graphType !== 'fact' && item.graphType !== 'plan') {
+                return;
+            }
+            const color = item.graphType === 'fact' ? '#0089FF' : '#FFFFFF';
+            const r = 4;
+            item.graph.forEach((point, idx, arr) => {
+                if (!!arr.length && idx === arr.length - 1) {
+                    pointsG
+                        .append('circle')
+                        .attr('class', 'point')
+                        .attr('cx', point.x)
+                        .attr('cy', point.y)
+                        .attr('r', r)
+                        .style('fill', '#1C1F2B')
+                        .style('stroke', color)
+                        .style('stroke-width', 2);
+                }
+            });
+        });
+    }
+
+    private drawGridlines(): void {
+        const hb = this.chartData.find((chart) => chart.graphType === 'higherBorder');
+        this.svg
+            .append('g')
+            .attr('class', 'grid')
+            .attr('transform', `translate(0,${this.graphMaxY - this.padding.bottom})`)
+            .call(
+                d3
+                    .axisBottom(this.scaleFuncs.x)
+                    .ticks(hb.graph.length)
+                    .tickSize(-(this.graphMaxY - this.padding.bottom - this.padding.top))
+                    .tickFormat('')
+            )
+            .style('color', '#272A38');
+        this.svg
+            .append('g')
+            .attr('class', 'grid')
+            .attr('transform', `translate(${this.padding.left},0)`)
+            .call(
+                d3
+                    .axisLeft(this.scaleFuncs.y)
+                    .ticks(5)
+                    .tickSize(-(this.graphMaxX - this.padding.left - this.padding.right))
+                    .tickFormat('')
+            )
+            .style('color', '#272A38');
+    }
+
+    private drawAxisXLabels(): void {
+        this.svg
+            .append('g')
+            .attr('transform', `translate(0,${this.graphMaxY - this.padding.bottom})`)
+            .attr('class', 'axisX')
+            .call(this.axis.axisX)
+            .selectAll('text')
+            .style('font-size', '12px')
+            .style('fill', '#8c99b2');
+
+        const axisG = this.svg.select('g.axisX');
+        axisG.select('path.domain').remove();
+        axisG.selectAll('g.tick line').remove();
+
+        const colors = {
+            last: '#606580',
+            active: '#0089FF',
+            future: '#303549',
+        };
+
+        const activeIdx =
+            this.chartData.find((chart) => chart.graphType === 'fact').graph.length - 1;
+
+        const gArr = Array.from(axisG.selectAll('g.tick')._groups[0]);
+        gArr.forEach((g: HTMLElement, idx: number) => {
+            const fill =
+                idx < activeIdx ? colors.last : idx === activeIdx ? colors.active : colors.future;
+            d3Selection
+                .select(g)
+                .select('text')
+                .style('fill', fill);
+        });
+    }
+
+    private drawAxisYLabels(): void {
+        this.svg
+            .append('g')
+            .attr('transform', `translate(${this.padding.left},0)`)
+            .attr('class', 'axisY')
+            .call(this.axis.axisY)
+            .selectAll('text')
+            .style('font-size', '12px')
+            .style('fill', '#606580');
+
+        const axisG = this.svg.select('g.axisY');
+        axisG.select('path.domain').remove();
+        axisG.selectAll('g.tick line').remove();
+    }
+
+    private customizeAreas(): void {
+        const fact = this.data.find((item) => item.graphType === 'fact')?.graph ?? [];
+        const higher = this.data.find((item) => item.graphType === 'higherBorder')?.graph ?? [];
+        const lower = this.data.find((item) => item.graphType === 'lowerBorder')?.graph ?? [];
+        fact.forEach((val, idx, arr) => {
+            if (higher[idx].value < val.value) {
+                this.svg
+                    .select('path.graph-line-higherBorder')
+                    .attr('class', 'graph-line-higherBorder graph-line_warning');
+                this.svg
+                    .select('path.graph-area-higherBorder')
+                    .attr('class', 'graph-area-higherBorder graph-area_warning');
+            } else if (lower[idx].value > val.value) {
+                this.svg
+                    .select('path.graph-line-lowerBorder')
+                    .attr('class', 'graph-line-lowerBorder graph-line_normal');
+                this.svg
+                    .select('path.graph-area-lowerBorder')
+                    .attr('class', 'graph-area-lowerBorder graph-area_normal');
+            }
+        });
+    }
 }
