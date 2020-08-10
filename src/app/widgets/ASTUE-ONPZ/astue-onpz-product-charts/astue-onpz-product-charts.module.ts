1ab9856c22a9a6bc2cc7621bab52c94dce9744b1
Alexey Singur <alex.singur1@gmail.com>
2020-08-10 12:27:33 +0300

    add card-header

--- src/app/widgets/ASTUE-ONPZ/astue-onpz-product-charts/astue-onpz-product-charts.module.ts
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-product-charts/astue-onpz-product-charts.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AstueOnpzProductChartsComponent } from './astue-onpz-product-charts.component';
 import { AstueOnpzSharedModule } from '../astue-onpz-shared/astue-onpz-shared.module';
 import { AstueOnpzProductCardComponent } from './components/astue-onpz-product-card/astue-onpz-product-card.component';
+import { SharedModule } from '../../../@shared/shared.module';
 
 @NgModule({
     declarations: [AstueOnpzProductChartsComponent, AstueOnpzProductCardComponent],
-    imports: [CommonModule, AstueOnpzSharedModule],
+    imports: [CommonModule, AstueOnpzSharedModule, SharedModule],
 })
 export class AstueOnpzProductChartsModule {
     enterComponent = AstueOnpzProductChartsComponent;
