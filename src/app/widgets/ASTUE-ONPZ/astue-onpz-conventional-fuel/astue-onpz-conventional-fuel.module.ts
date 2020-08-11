a255738b1ea2c4d1150d767f42f30cb6d37c54c8
Medvedev <medvedd07@gmail.com>
2020-08-11 16:56:34 +0300

    commit

--- src/app/widgets/ASTUE-ONPZ/astue-onpz-conventional-fuel/astue-onpz-conventional-fuel.module.ts
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-conventional-fuel/astue-onpz-conventional-fuel.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { AstueOnpzConventionalFuelComponent } from './astue-onpz-conventional-fuel.component';
 import { AstueOnpzInteractiveIndicatorsComponent } from './components/astue-onpz-interactive-indicators/astue-onpz-interactive-indicators.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     declarations: [AstueOnpzConventionalFuelComponent, AstueOnpzInteractiveIndicatorsComponent],
     imports: [
         CommonModule,
-        AngularSvgIconModule
+        AngularSvgIconModule,
+        MatFormFieldModule,
+        MatSelectModule,
     ]
 })
 export class AstueOnpzConventionalFuelModule {
