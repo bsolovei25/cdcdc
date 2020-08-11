eafd440f339c3aed0d0e22ded1dbc473c497db13
TarasovMV <31613333+TarasovMV@users.noreply.github.com>
2020-08-11 16:05:25 +0300

    add widget

---
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-deviation-cards/astue-onpz-deviation-cards.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AstueOnpzDeviationCardsComponent } from './astue-onpz-deviation-cards.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { SharedModule } from '@shared/shared.module';
+
+
+@NgModule({
+    declarations: [AstueOnpzDeviationCardsComponent],
+    imports: [
+        CommonModule,
+        AngularSvgIconModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        SharedModule,
+    ]
+})
+export class AstueOnpzDeviationCardsModule {
+    enterComponent = AstueOnpzDeviationCardsComponent;
+}
