ff01093e0bd38ea40e2fd67a99cbf374731fd1d7
Medvedev <medvedd07@gmail.com>
2020-08-11 10:14:30 +0300

    commit

--- src/app/widgets/ASTUE-ONPZ/astue-onpz-main-indicators/astue-onpz-main-indicators.module.ts
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-main-indicators/astue-onpz-main-indicators.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AstueOnpzMainIndicatorsComponent } from './astue-onpz-main-indicators.component';
+import { SharedModule } from '@shared/shared.module';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
 @NgModule({
     declarations: [AstueOnpzMainIndicatorsComponent],
     imports: [
-        CommonModule
+        CommonModule,
+        SharedModule,
+        AngularSvgIconModule,
+        MatSlideToggleModule,
     ]
 })
 export class AstueOnpzMainIndicatorsModule {
