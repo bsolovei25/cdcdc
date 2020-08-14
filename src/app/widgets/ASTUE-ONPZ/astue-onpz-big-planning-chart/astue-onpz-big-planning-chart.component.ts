feda3b8982d7ed384ec5473752b58102b60f5c75
Alexey Singur <alex.singur1@gmail.com>
2020-08-14 11:57:18 +0300

    create big-planning-chart widget

---
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-big-planning-chart/astue-onpz-big-planning-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'evj-astue-onpz-big-planning-chart',
+  templateUrl: './astue-onpz-big-planning-chart.component.html',
+  styleUrls: ['./astue-onpz-big-planning-chart.component.scss']
+})
+export class AstueOnpzBigPlanningChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-big-planning-chart/astue-onpz-big-planning-chart.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AstueOnpzBigPlanningChartComponent } from './astue-onpz-big-planning-chart.component';
+
+@NgModule({
+    declarations: [AstueOnpzBigPlanningChartComponent],
+    imports: [CommonModule],
+})
+export class AstueOnpzBigPlanningChartModule {
+    enterComponent = AstueOnpzBigPlanningChartComponent;
+}
