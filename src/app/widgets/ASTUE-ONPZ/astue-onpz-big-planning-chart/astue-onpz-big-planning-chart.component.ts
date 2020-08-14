44bf7f6947940de9a66c6fd25493064ac9b67a28
Alexey Singur <alex.singur1@gmail.com>
2020-08-14 14:55:50 +0300

    add header to big-planning-chart

--- src/app/widgets/ASTUE-ONPZ/astue-onpz-big-planning-chart/astue-onpz-big-planning-chart.component.ts
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-big-planning-chart/astue-onpz-big-planning-chart.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { WidgetPlatform } from '../../../dashboard/models/widget-platform';
+import { WidgetService } from '../../../dashboard/services/widget.service';
 
 @Component({
-  selector: 'evj-astue-onpz-big-planning-chart',
-  templateUrl: './astue-onpz-big-planning-chart.component.html',
-  styleUrls: ['./astue-onpz-big-planning-chart.component.scss']
+    selector: 'evj-astue-onpz-big-planning-chart',
+    templateUrl: './astue-onpz-big-planning-chart.component.html',
+    styleUrls: ['./astue-onpz-big-planning-chart.component.scss'],
 })
-export class AstueOnpzBigPlanningChartComponent implements OnInit {
+export class AstueOnpzBigPlanningChartComponent extends WidgetPlatform
+    implements OnInit, OnDestroy {
+    constructor(
+        protected widgetService: WidgetService,
+        @Inject('isMock') public isMock: boolean,
+        @Inject('widgetId') public id: string,
+        @Inject('uniqId') public uniqId: string
+    ) {
+        super(widgetService, isMock, id, uniqId);
+    }
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+    public ngOnDestroy(): void {
+        super.ngOnDestroy();
+    }
 
+    protected dataHandler(ref: any): void {}
 }
--- src/app/widgets/ASTUE-ONPZ/astue-onpz-big-planning-chart/astue-onpz-big-planning-chart.module.ts
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-big-planning-chart/astue-onpz-big-planning-chart.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AstueOnpzBigPlanningChartComponent } from './astue-onpz-big-planning-chart.component';
+import { SharedModule } from '../../../@shared/shared.module';
 
 @NgModule({
     declarations: [AstueOnpzBigPlanningChartComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, SharedModule],
 })
 export class AstueOnpzBigPlanningChartModule {
     enterComponent = AstueOnpzBigPlanningChartComponent;
