376b2dd0437616f8bf78529d2dd3efcaa2b54a35
Alexey Singur <alex.singur1@gmail.com>
2020-08-14 10:42:46 +0300

    create planning-charts widget

---
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-planning-charts/astue-onpz-planning-charts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'evj-astue-onpz-planning-charts',
+  templateUrl: './astue-onpz-planning-charts.component.html',
+  styleUrls: ['./astue-onpz-planning-charts.component.scss']
+})
+export class AstueOnpzPlanningChartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-planning-charts/astue-onpz-planning-charts.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AstueOnpzPlanningChartsComponent } from './astue-onpz-planning-charts.component';
+
+@NgModule({
+    declarations: [AstueOnpzPlanningChartsComponent],
+    imports: [CommonModule],
+})
+export class AstueOnpzPlanningChartsModule {
+    enterComponent = AstueOnpzPlanningChartsComponent;
+}
