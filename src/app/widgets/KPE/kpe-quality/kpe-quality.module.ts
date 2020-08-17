62d68d075cd8e8513e51d3245f6d4da0961ccb8d
Дмитрий Сандул <sandul.da@gazgolder.ru>
2020-08-17 16:05:15 +0300

    Resolve "КПЭ. Компоновка виджета "КПЭ по качеству""

--- src/app/widgets/KPE/kpe-quality/kpe-quality.module.ts
+++ src/app/widgets/KPE/kpe-quality/kpe-quality.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KpeQualityComponent } from './kpe-quality.component';
 import { KpeSharedModule } from '../shared/kpe-shared.module';
-import { KpeEqualizerChartComponent } from '../kpe-equalizer-chart/kpe-equalizer-chart.component';
-import { BarDiagramComponent } from '../kpe-equalizer-chart/components/bar-diagram/bar-diagram.component';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
     declarations: [
         KpeQualityComponent,
-        KpeEqualizerChartComponent,
-        BarDiagramComponent
     ],
-    imports: [CommonModule, KpeSharedModule],
+    imports: [
+        CommonModule,
+        KpeSharedModule,
+        SharedModule,
+    ],
 })
 export class KpeQualityModule {
     enterComponent = KpeQualityComponent;
