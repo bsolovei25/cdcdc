5357f1928476d83271311e9a4919ac26ec86790f
Medvedev <medvedd07@gmail.com>
2020-09-01 14:14:02 +0300

    complete predictors onpz

--- src/app/widgets/ASTUE-ONPZ/astue-onpz-predictors/astue-onpz-predictors.component.ts
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-predictors/astue-onpz-predictors.component.ts
@@ -3,6 +3,7 @@ import { WidgetPlatform } from '../../../dashboard/models/widget-platform';
 import { WidgetService } from '../../../dashboard/services/widget.service';
 
 interface IPredictors {
+    id: number;
     name: string;
     label: string;
     color: number;
@@ -18,30 +19,35 @@ export class AstueOnpzPredictorsComponent extends WidgetPlatform implements OnIn
 
     data: IPredictors[] = [
         {
+            id: 1,
             name: 'Скорость ветра',
             label: 'Meteo:SP',
             color: 1,
             isActive: true
         },
         {
+            id: 2,
             name: 'Газ из Е-2 (FI103 пл лаб)',
             label: 'AVT10:PL103_O',
             color: 2,
             isActive: true
         },
         {
+            id: 3,
             name: 'Температура канала 1',
             label: 'AVT10:PL103_O',
             color: 3,
             isActive: false
         },
         {
+            id: 4,
             name: 'Скорость ветра',
             label: 'Meteo:SP',
             color: 3,
             isActive: true
         },
         {
+            id: 5,
             name: 'Газ из Е-2 (FI103 пл лаб)',
             label: 'AVT10:PL103_O',
             color: 4,
