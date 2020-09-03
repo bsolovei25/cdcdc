ec9cdceb03351a051b2e9089ad4329c9b51af0c2
Medvedev <medvedd07@gmail.com>
2020-09-03 10:50:39 +0300

    add-predictors-back

--- src/app/widgets/ASTUE-ONPZ/astue-onpz-predictors/astue-onpz-predictors.component.ts
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-predictors/astue-onpz-predictors.component.ts
@@ -6,7 +6,7 @@ interface IPredictors {
     id: number;
     name: string;
     label: string;
-    color: number;
+    colorIndex: number;
     isActive?: boolean;
 }
 
@@ -22,35 +22,35 @@ export class AstueOnpzPredictorsComponent extends WidgetPlatform implements OnIn
             id: 1,
             name: 'Скорость ветра',
             label: 'Meteo:SP',
-            color: 1,
+            colorIndex: 1,
             isActive: true
         },
         {
             id: 2,
             name: 'Газ из Е-2 (FI103 пл лаб)',
             label: 'AVT10:PL103_O',
-            color: 2,
+            colorIndex: 2,
             isActive: true
         },
         {
             id: 3,
             name: 'Температура канала 1',
             label: 'AVT10:PL103_O',
-            color: 3,
+            colorIndex: 3,
             isActive: false
         },
         {
             id: 4,
             name: 'Скорость ветра',
             label: 'Meteo:SP',
-            color: 3,
+            colorIndex: 3,
             isActive: true
         },
         {
             id: 5,
             name: 'Газ из Е-2 (FI103 пл лаб)',
             label: 'AVT10:PL103_O',
-            color: 4,
+            colorIndex: 4,
             isActive: true
         }
     ];
@@ -73,6 +73,7 @@ export class AstueOnpzPredictorsComponent extends WidgetPlatform implements OnIn
     }
 
     protected dataHandler(ref: {predictors: IPredictors[] }): void {
+        console.log(ref);
         this.data = ref.predictors;
     }
 
