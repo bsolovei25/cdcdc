a91f3322cec654ffbfb3862c7a8d29dee2e73318
Khachatur <khacha1998@gmail.com>
2020-09-14 21:05:35 +0700

    v1.0

---
+++ src/app/widgets/OZSM/ozsm-shared/ozsm-loading-parks/components/ozsm-loading-parks-diagram/ozsm-loading-parks-diagram.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'evj-ozsm-loading-parks-diagram',
+  templateUrl: './ozsm-loading-parks-diagram.component.html',
+  styleUrls: ['./ozsm-loading-parks-diagram.component.scss']
+})
+export class OzsmLoadingParksDiagramComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
