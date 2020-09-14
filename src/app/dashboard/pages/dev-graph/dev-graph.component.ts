06cd36a4c3c69205efcd02bfb415825a242cfef3
TarasovMV <31613333+TarasovMV@users.noreply.github.com>
2020-09-14 16:30:03 +0300

    add resize feature

--- src/app/dashboard/pages/dev-graph/dev-graph.component.ts
+++ src/app/dashboard/pages/dev-graph/dev-graph.component.ts
@@ -1,6 +1,7 @@
 import {
-    Component,
+    Component, ElementRef, ViewChild
 } from '@angular/core';
+
 
 @Component({
     selector: 'evj-dev-graph',
@@ -8,5 +9,21 @@ import {
     styleUrls: ['./dev-graph.component.scss']
 })
 export class DevGraphComponent {
+    @ViewChild('graphContainer') public graphContainer: ElementRef;
+
+    private readonly staticWidth: number = 1220;
+    private readonly staticHeight: number = 660;
+
+    get areaScale(): string {
+        const scaleY =
+            (this.graphContainer?.nativeElement?.offsetHeight ?? this.staticHeight)
+            / this.staticHeight;
+        const scaleX =
+            (this.graphContainer?.nativeElement?.offsetWidth ?? this.staticWidth)
+            / this.staticWidth;
+        const scale: number = scaleX < scaleY ? scaleX : scaleY;
+        console.log(this.graphContainer?.nativeElement?.offsetHeight);
+        return `transform: translate(-50%, -50%) scale(${scale})`;
+    }
 }
 
