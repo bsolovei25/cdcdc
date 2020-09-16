88249c5414d1d7b12a6bd74022500644617a54ad
kirill.kim <kim.k@gazgolder.ru>
2020-09-16 18:04:13 +0300

    layout

--- src/app/widgets/OZSM/ozsm-main-toggle/ozsm-main-toggle.component.ts
+++ src/app/widgets/OZSM/ozsm-main-toggle/ozsm-main-toggle.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { WidgetPlatform } from '../../../dashboard/models/widget-platform';
+import { WidgetService } from '../../../dashboard/services/widget.service';
 
 @Component({
   selector: 'evj-ozsm-main-toggle',
   templateUrl: './ozsm-main-toggle.component.html',
   styleUrls: ['./ozsm-main-toggle.component.scss']
 })
-export class OzsmMainToggleComponent implements OnInit {
+export class OzsmMainToggleComponent extends WidgetPlatform implements OnInit, OnDestroy {
 
-  constructor() { }
+    constructor(
+        protected widgetService: WidgetService,
+        @Inject('isMock') public isMock: boolean,
+        @Inject('widgetId') public id: string,
+        @Inject('uniqId') public uniqId: string
+    ) {
+        super(widgetService, isMock, id, uniqId);
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        super.widgetInit();
+    }
 
+    ngOnDestroy(): void {
+        super.ngOnDestroy();
+    }
+
+    protected dataHandler(ref: any): void {
+    }
 }
