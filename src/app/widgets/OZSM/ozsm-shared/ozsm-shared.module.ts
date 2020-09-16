2c5a349d10e1600753143a92ff2d86726ae47c92
tarasov.mv <tarasovm2013@gmail.com>
2020-09-16 10:15:39 +0300

    fix import

--- src/app/widgets/OZSM/ozsm-shared/ozsm-shared.module.ts
+++ src/app/widgets/OZSM/ozsm-shared/ozsm-shared.module.ts
@@ -5,6 +5,10 @@ import { OzsmTripleDiagramCardComponent } from './ozsm-triple-diagram-card/ozsm-
 import { OzsmCircleDiagramComponent } from './ozsm-circle-diagram/ozsm-circle-diagram.component';
 import { OzsmStorageTankComponent } from './ozsm-triple-diagram-card/ozsm-storage-tank/ozsm-storage-tank.component';
 import { OzsmEqualizerChartComponent } from './ozsm-equalizer-chart/ozsm-equalizer-chart.component';
+import { OzsmLoadingParksComponent } from "./ozsm-loading-parks/ozsm-loading-parks.component";
+import { OzsmLoadingParksBodyComponent } from "./ozsm-loading-parks/components/ozsm-loading-parks-body/ozsm-loading-parks-body.component";
+import { OzsmLoadingParksDiagramComponent } from "./ozsm-loading-parks/components/ozsm-loading-parks-diagram/ozsm-loading-parks-diagram.component";
+import { OzsmLoadingParksHeaderComponent } from "./ozsm-loading-parks/components/ozsm-loading-parks-header/ozsm-loading-parks-header.component";
 
 @NgModule({
     declarations: [
@@ -12,12 +16,20 @@ import { OzsmEqualizerChartComponent } from './ozsm-equalizer-chart/ozsm-equaliz
         OzsmCircleDiagramComponent,
         OzsmStorageTankComponent,
         OzsmEqualizerChartComponent,
+        OzsmLoadingParksComponent,
+        OzsmLoadingParksBodyComponent,
+        OzsmLoadingParksDiagramComponent,
+        OzsmLoadingParksHeaderComponent,
     ],
     exports: [
         OzsmTripleDiagramCardComponent,
         OzsmCircleDiagramComponent,
         OzsmStorageTankComponent,
         OzsmEqualizerChartComponent,
+        OzsmLoadingParksComponent,
+        OzsmLoadingParksBodyComponent,
+        OzsmLoadingParksDiagramComponent,
+        OzsmLoadingParksHeaderComponent,
     ],
     imports: [
         CommonModule,
