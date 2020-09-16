6150fc9c37b18eb76504a404e04fce2f1d0d2b3d
kirill.kim <kim.k@gazgolder.ru>
2020-09-16 13:14:10 +0300

    fix font

--- src/app/widgets/OZSM/ozsm-shared/ozsm-card-line-diagram/ozsm-card-line-diagram.component.ts
+++ src/app/widgets/OZSM/ozsm-shared/ozsm-card-line-diagram/ozsm-card-line-diagram.component.ts
@@ -10,6 +10,8 @@ import { mockData } from './ozsm-card-line-mock';
 export class OzsmCardLineDiagramComponent implements OnInit {
     public data: ICardLineDiagramModel = mockData;
     public percentValue: number = 0;
+    public iconStyleTitle: object = {'line-height': '14px', 'font-size': '12px'};
+    public iconStyleValue: object = {'line-height': '16px', 'font-size': '16px'};
 
     constructor() {
     }
