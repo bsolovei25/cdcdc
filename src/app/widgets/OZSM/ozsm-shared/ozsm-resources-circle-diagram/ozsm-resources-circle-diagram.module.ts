042d074e2c00966d2c708869b8a64b5a16e8ec1b
Khachatur <khacha1998@gmail.com>
2020-09-16 12:15:04 +0700

    fixPipeLine
    Добваил AngularSvgIconModule

--- src/app/widgets/OZSM/ozsm-shared/ozsm-resources-circle-diagram/ozsm-resources-circle-diagram.module.ts
+++ src/app/widgets/OZSM/ozsm-shared/ozsm-resources-circle-diagram/ozsm-resources-circle-diagram.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { OzsmResourcesCircleDiagramVisualComponent } from './components/ozsm-resources-circle-diagram-visual/ozsm-resources-circle-diagram-visual.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
     declarations: [
@@ -13,7 +14,8 @@ import { OzsmResourcesCircleDiagramVisualComponent } from './components/ozsm-res
     imports: [
         CommonModule, 
         HttpClientModule,
-        SharedModule
+        SharedModule,
+        AngularSvgIconModule.forRoot()
     ]
 })
 export class OzsmResourcesCircleDiagramModule {
