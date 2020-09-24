8ec0b0b5523241f8cfae37f9551fec09c9b3ce11
Khachatur <khacha1998@gmail.com>
2020-09-25 00:37:50 +0700

    Update ozsm-list-packing-diagrams-item.component.ts

--- src/app/widgets/OZSM/ozsm-shared/ozsm-list-packing-diagrams/components/ozsm-list-packing-diagrams-item/ozsm-list-packing-diagrams-item.component.ts
+++ src/app/widgets/OZSM/ozsm-shared/ozsm-list-packing-diagrams/components/ozsm-list-packing-diagrams-item/ozsm-list-packing-diagrams-item.component.ts
@@ -10,6 +10,7 @@ import * as d3 from 'd3';
 export class OzsmListPackingDiagramsItemComponent implements OnInit {
 
   @Input() index: string;
+  @Input() fillPercent: number = 60;
   @ViewChild('chart') chart: ElementRef;
 
   constructor() { }
@@ -42,10 +43,10 @@ export class OzsmListPackingDiagramsItemComponent implements OnInit {
       .attr('r', 33.5)
       .attr('class', 'transparent')
       .style('transform-origin', 'center')
-      .style('transform', 'rotate(234deg)')
+      .style('transform', `rotate(${90 + this.fillPercent / 100 * 217}deg)`)
       .style('stroke-width', '8px')
       .style('stroke-dasharray', '211px 211px')
-      .style('stroke-dashoffset', `${0.8 * 211}`);
+      .style('stroke-dashoffset', `${85 + this.fillPercent / 100 * 126}`);
 
     svg.append('rect')
       .attr('x', 40)
@@ -69,7 +70,7 @@ export class OzsmListPackingDiagramsItemComponent implements OnInit {
       .attr('x', 40)
       .attr('y', 69.5)
       .style('transform-origin', 'center center')
-      .style('transform', `rotate(${146}deg)`)
+      .style('transform', `rotate(${this.fillPercent / 100 * 217}deg)`)
       .attr('width', 1)
       .attr('height', 8)
       .attr('class', 'white');
