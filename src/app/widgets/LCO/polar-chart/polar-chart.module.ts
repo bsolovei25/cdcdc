e029acef227f6664d56a0bb1559eeb382e462a36
kirill.kim <kim.k@gazgolder.ru>
2020-09-24 13:24:31 +0300

    polar-chart

--- src/app/widgets/LCO/polar-chart/polar-chart.module.ts
+++ src/app/widgets/LCO/polar-chart/polar-chart.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PolarChartComponent } from './polar-chart.component';
+import { SharedModule } from '@shared/shared.module';
+import { DashboardModule } from '../../../dashboard/dashboard.module';
 
 
 
 @NgModule({
   declarations: [PolarChartComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        SharedModule,
+        DashboardModule
+    ]
 })
 export class PolarChartModule {
     enterComponent = PolarChartComponent;
