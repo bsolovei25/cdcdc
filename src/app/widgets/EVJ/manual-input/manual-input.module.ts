6c2d21fee97b36e5d4d9073ca6c3a387cf503081
kirill.kim <kim.k@gazgolder.ru>
2020-09-25 09:28:58 +0300

    fixed ngModel in manual input

--- src/app/widgets/EVJ/manual-input/manual-input.module.ts
+++ src/app/widgets/EVJ/manual-input/manual-input.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ManualInputComponent } from './manual-input.component';
 import { SharedModule } from '@shared/shared.module';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
     declarations: [ManualInputComponent],
-  imports: [
-    CommonModule,
-    SharedModule,
-    AngularSvgIconModule
-  ]
+    imports: [
+        CommonModule,
+        SharedModule,
+        AngularSvgIconModule,
+        FormsModule
+    ]
 })
 export class ManualInputModule {
     enterComponent = ManualInputComponent;
