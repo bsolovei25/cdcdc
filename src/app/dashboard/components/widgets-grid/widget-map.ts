b8e1999168c7bbaf7f8530f29748ad3668253f1f
Kirill Kim <kirill.kim@hotmail.com>
2020-10-06 17:38:47 +0300

    fixed

--- src/app/dashboard/components/widgets-grid/widget-map.ts
+++ src/app/dashboard/components/widgets-grid/widget-map.ts
@@ -39,7 +39,7 @@ import { CdMatBalanceChartCardComponent } from '../../../widgets/CD/cd-mat-balan
 export const WIDGETS = {
     'pie-diagram': WidgetContainerComponent,
     'line-chart': WidgetContainerComponent,
-    'manual-input': ManualInputComponent,
+    'manual-input': WidgetContainerComponent,
     'events-workspace': WidgetContainerComponent,
     'industrial-pyramid': WidgetContainerComponent,
     'circle-diagram': CircleDiagramComponent,
