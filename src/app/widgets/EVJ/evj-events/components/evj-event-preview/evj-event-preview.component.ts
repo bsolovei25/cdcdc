47e6751f332d1771937f5bead8447ad042475a31
Дмитрий Сандул <sandul.da@gazgolder.ru>
2020-10-09 11:57:58 +0300

    Resolve "События. Превью рабочей области события"

---
+++ src/app/widgets/EVJ/evj-events/components/evj-event-preview/evj-event-preview.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { EventService } from '../../../../../dashboard/services/widgets/event.service';
+
+@Component({
+    selector: 'evj-evj-event-preview',
+    templateUrl: './evj-event-preview.component.html',
+    styleUrls: ['./evj-event-preview.component.scss']
+})
+export class EvjEventPreviewComponent implements OnInit, OnDestroy {
+    @Input()
+    public isPreviewOpened: boolean = false;
+
+    @Output()
+    public close: EventEmitter<number> = new EventEmitter<number>();
+
+    constructor() {
+    }
+
+    public ngOnInit(): void {
+    }
+
+    public ngOnDestroy(): void {
+    }
+
+    public onClose(): void {
+        this.close.emit();
+    }
+}
