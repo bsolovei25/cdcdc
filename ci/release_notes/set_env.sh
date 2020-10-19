71af4cfee505127c69ab20eea3051428a8a4fa54
Sergey Yakubovskiy <serg.yakubovskiy@gmail.com>
2020-10-19 10:16:12 +0300

    packing to master

--- ci/release_notes/set_env.sh
+++ ci/release_notes/set_env.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-#export gitlab_token="gA7qc5E4F4kC3HsVNsSi"
-
 export GITLAB_TOKEN="zh1Thw-dZVw5sx58KZjp" # yakubovskiy.sv
-# export GITLAB_TOKEN="NzNK3Z-QxgvVzAxuyPrK" # rebrain.me
-# export GITLAB_TOKEN="hmBxtxWCXucySW_N1F18" # sinkevich.iv
 export GITLAB_HOST="gitlab.funcoff.club"
-
-#export GITLAB_HOST="gitlab.rebrainme.com"
-# export MILESTONE="Friday Sprint"
-
-# PROJECT_ID should be obtain from pipeline CI_PROJECT_ID
-# export PROJECT_ID='43' # test gitlab.funcoff.club
-#export PROJECT_ID='7752'
 export OUTPUT_DESCRIPTION='./output'
 
