7c20db1ef80726a9b45f1a95727e6989c2469b58
Kirill Kim <kirill.kim@hotmail.com>
2020-10-19 12:24:57 +0300

    svg

--- src/app/widgets/LCO/deviations-table/deviations-table.module.ts
+++ src/app/widgets/LCO/deviations-table/deviations-table.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DeviationsTableComponent } from './deviations-table.component';
 import { SharedModule } from '@shared/shared.module';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 
 @NgModule({
     declarations: [DeviationsTableComponent],
     imports: [
         CommonModule,
-        SharedModule
+        SharedModule,
+        AngularSvgIconModule
     ]
 })
 export class DeviationsTableModule {
