2648b48f57d8d10d3bd17c6e7f22b55a9484d577
Kirill Kim <kirill.kim@hotmail.com>
2020-10-22 13:23:13 +0300

    to code-review

---
+++ src/app/@shared/components/rectangle-input/rectangle-input.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'evj-rectangle-input',
+  templateUrl: './rectangle-input.component.html',
+  styleUrls: ['./rectangle-input.component.scss']
+})
+export class RectangleInputComponent {
+
+    public searchValue: string;
+    @Output() searchChange: EventEmitter<string> = new EventEmitter<string>();
+    @Input()
+    get search(): string {
+        return this.searchValue;
+    }
+    set search(val: string) {
+        this.searchValue = val;
+        this.searchChange.emit(this.searchValue);
+    }
+    @Input() placeholder: string = 'Поиск...';
+    public isInput: boolean = false;
+}
