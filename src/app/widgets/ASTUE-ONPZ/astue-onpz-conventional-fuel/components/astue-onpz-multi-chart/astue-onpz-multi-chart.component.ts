6f6da9dbf96ed4e4a4112a54c18c25d5757dc021
Konstantin Medvedev <medvedd07@gmail.com>
2020-10-27 15:06:48 +0300

    fix

--- src/app/widgets/ASTUE-ONPZ/astue-onpz-conventional-fuel/components/astue-onpz-multi-chart/astue-onpz-multi-chart.component.ts
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-conventional-fuel/components/astue-onpz-multi-chart/astue-onpz-multi-chart.component.ts
@@ -137,7 +137,7 @@ export class AstueOnpzMultiChartComponent implements OnInit, OnChanges, OnDestro
 
     @HostListener('mouseleave') onMouseLeave(): void {
         if (this.eventListenerFn) {
-            this.svg.select('g.mouse-over').remove();
+            this.svg?.select('g.mouse-over')?.remove();
             this.eventListenerFn();
         }
     }
@@ -252,8 +252,10 @@ export class AstueOnpzMultiChartComponent implements OnInit, OnChanges, OnDestro
         let max: number = -Infinity;
 
         this.charts.forEach((chart) => {
-            if (chart.graphType !== 'fact' && chart.graphType !== 'plan'
-                && chart.graphType !== 'forecast' && chart.graphType !== 'higherBorder'
+            if (chart.graphType !== 'fact'
+                && chart.graphType !== 'plan'
+                && chart.graphType !== 'forecast'
+                && chart.graphType !== 'higherBorder'
                 && chart.graphType !== 'lowerBorder'
             ) {
                 this.axisLabels[chart.graphType] = this.defineAxisYLabels(
@@ -313,7 +315,7 @@ export class AstueOnpzMultiChartComponent implements OnInit, OnChanges, OnDestro
         const deltaDomainDates = domainDates[1].getTime() - domainDates[0].getTime();
         domainDates = [
             new Date(domainDates[0].getTime() + this.scroll.left / 100 * deltaDomainDates),
-            new Date(domainDates[1].getTime() - this.scroll.right / 100 * deltaDomainDates),
+            new Date(domainDates[1].getTime() - this.scroll.right / 100 * deltaDomainDates)
         ];
 
         this.scaleFuncs.x = d3
@@ -606,34 +608,34 @@ export class AstueOnpzMultiChartComponent implements OnInit, OnChanges, OnDestro
 
     // TODO clear on currentDates
     private drawFutureRect(): void {
-        // const values = [];
-        // let plan: IChartMini;
-        // let fact: IChartMini;
-        // const currentDatetime: Date = new Date();
-        // currentDatetime.setMinutes(0, 0, 0);
-        // const x: number = this.scaleFuncs.x(currentDatetime);
-        // this.charts.forEach((chart) => {
-        //     const filterChart = chart.graph
-        //         .filter((item) => item.timeStamp.getTime() <= currentDatetime.getTime());
-        //     const statValue =
-        //         filterChart?.length > 0
-        //             ? filterChart[filterChart.length - 1]
-        //             : chart?.graph[0] ?? null;
-        //     if (chart.graphType === 'plan') {
-        //         plan = chart.graph[chart.graph.length - 1];
-        //     } else if (chart.graphType === 'fact') {
-        //         fact = chart.graph[chart.graph.length - 1];
-        //     } else if (chart.graphType === 'forecast') {
-        //         // TODO add some
-        //     } else {
-        //         values.push({
-        //             val: statValue,
-        //             color: lineColors[this.colors?.get(chart.tagName)],
-        //             units: chart.units ?? '',
-        //             iconType: chart.graphType ?? 'volume'
-        //         });
-        //     }
-        // });
+        const values = [];
+        let plan: IChartMini;
+        let fact: IChartMini;
+        const currentDatetime: Date = new Date();
+        currentDatetime.setMinutes(0, 0, 0);
+        const x: number = this.scaleFuncs.x(currentDatetime);
+        this.charts.forEach((chart) => {
+            const filterChart = chart.graph
+                .filter((item) => item.timeStamp.getTime() <= currentDatetime.getTime());
+            const statValue =
+                filterChart?.length > 0
+                    ? filterChart[filterChart.length - 1]
+                    : chart?.graph[0] ?? null;
+            if (chart.graphType === 'plan') {
+                plan = chart.graph[chart.graph.length - 1];
+            } else if (chart.graphType === 'fact') {
+                fact = chart.graph[chart.graph.length - 1];
+            } else if (chart.graphType === 'forecast') {
+                // TODO add some
+            } else {
+                values.push({
+                    val: statValue,
+                    color: lineColors[this.colors?.get(chart.tagName)],
+                    units: chart.units ?? '',
+                    iconType: chart.graphType ?? 'volume'
+                });
+            }
+        });
 
         this.drawMouseGroup();
 
@@ -642,7 +644,7 @@ export class AstueOnpzMultiChartComponent implements OnInit, OnChanges, OnDestro
         // const width = this.graphMaxX - this.padding.right - x;
         // const height = this.graphMaxY - this.padding.top - this.padding.bottom + this.topMargin;
         // if (width > 0) {
-        // const g = this.svg.append('g').attr('class', 'picker');
+        const g = this.svg.append('g').attr('class', 'picker');
         // g.append('rect')
         //     .attr('x', x)
         //     .attr('y', this.padding.top - this.topMargin)
@@ -666,53 +668,54 @@ export class AstueOnpzMultiChartComponent implements OnInit, OnChanges, OnDestro
 
 
         // Иконки и значения возле линии
-        // let start = this.padding.top - this.topMargin;
-        // const step = 10;
-        // const cardWidth = this.axisYWidth * 2;
-        // const cardHeight = this.axisYWidth * 0.5;
-        //
-        // values.forEach((val) => {
-        //     const rect = g.append('g').attr('class', 'val');
-        //     const bg = rect
-        //         .append('g')
-        //         .attr('class', 'bg')
-        //         .style('opacity', 0.25);
-        //
-        //     start += step + cardHeight;
-        //
-        //     bg.append('rect')
-        //         .attr('x', x + step)
-        //         .attr('y', start)
-        //         .attr('width', cardWidth)
-        //         .attr('height', cardHeight)
-        //         .attr('rx', 5);
-        //     bg.append('rect')
-        //         .attr('x', x + step * 1.5)
-        //         .attr('y', start + step * 0.5)
-        //         .attr('width', cardHeight - step)
-        //         .attr('height', cardHeight - step)
-        //         .attr('rx', 5)
-        //         .style('fill', val.color);
-        //
-        //     rect.append('text')
-        //         .attr('x', x + step * 1.5 + cardHeight)
-        //         .attr('y', start + cardHeight - step * 0.9)
-        //         .text(`${val.val.value?.toFixed(2)} ${val.units}`);
-        //
-        //     if (this.options.isIconsShowing) {
-        //         rect.append('image')
-        //             .attr(
-        //                 'xlink:href',
-        //                 `assets/icons/widgets/ASTUE-ONPZ/astue-onpz-conventional-fuel/` +
-        //                 `${val.iconType}.svg`
-        //             )
-        //             .attr('x', x + step * 1.7)
-        //             .attr('y', start + step * 0.7)
-        //             .attr('width', cardHeight - step * 1.4)
-        //             .attr('height', cardHeight - step * 1.4);
-        //     }
-        // });
-        // }
+        let start = this.padding.top - this.topMargin;
+        const step = 10;
+        const cardWidth = this.axisYWidth * 2;
+        const cardHeight = this.axisYWidth * 0.5;
+
+        values.forEach((val) => {
+            const rect = g.append('g').attr('class', 'val');
+            const bg = rect
+                .append('g')
+                .attr('class', 'bg')
+                .style('opacity', 0.25);
+
+            start += step + cardHeight;
+
+            bg.append('rect')
+                .attr('x', x + step)
+                .attr('y', start)
+                .attr('width', cardWidth)
+                .attr('height', cardHeight)
+                .attr('rx', 5);
+            bg.append('rect')
+                .attr('x', x + step * 1.5)
+                .attr('y', start + step * 0.5)
+                .attr('width', cardHeight - step)
+                .attr('height', cardHeight - step)
+                .attr('rx', 5)
+                .style('fill', val.color);
+
+            rect.append('text')
+                .attr('x', x + step * 1.5 + cardHeight)
+                .attr('y', start + cardHeight - step * 0.9)
+                .text(`${val.val.value?.toFixed(2)} ${val.units}`);
+
+            if (this.options.isIconsShowing) {
+                rect.append('image')
+                    .attr(
+                        'xlink:href',
+                        `assets/icons/widgets/ASTUE-ONPZ/astue-onpz-conventional-fuel/` +
+                        `${val.iconType}.svg`
+                    )
+                    .attr('x', x + step * 1.7)
+                    .attr('y', start + step * 0.7)
+                    .attr('width', cardHeight - step * 1.4)
+                    .attr('height', cardHeight - step * 1.4);
+            }
+        });
+
+
         // if (!!this.currentDates) {
         //     return;
         // }
