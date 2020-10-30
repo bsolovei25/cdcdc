31f94d613dca51d2023145103abf0ae2e3ab528c
Kirill Kim <kirill.kim@hotmail.com>
2020-10-30 21:12:19 +0300

    menu

--- src/app/widgets/EVJ/evj-events-workspace/components/evj-main-toggle/evj-main-toggle.component.ts
+++ src/app/widgets/EVJ/evj-events-workspace/components/evj-main-toggle/evj-main-toggle.component.ts
@@ -1,22 +1,94 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+    ChangeDetectorRef,
+    Component,
+    EventEmitter,
+    Input, OnChanges,
+    OnDestroy,
+    OnInit,
+    Output,
+    SimpleChanges
+} from '@angular/core';
+import { IEjcoOnpzUnit } from '../../../../EJCO-ONPZ/ejco-onpz-unit-sou/ejco-onpz-unit-sou.component';
+import { Subscription } from 'rxjs';
+import { OverlayService } from '../../../../../dashboard/services/overlay.service';
+import { WidgetService } from '../../../../../dashboard/services/widget.service';
+import { UserSettingsService } from '../../../../../dashboard/services/user-settings.service';
+import { ClaimService, EnumClaimWidgets } from '../../../../../dashboard/services/claim.service';
 
 @Component({
   selector: 'evj-evj-main-toggle',
   templateUrl: './evj-main-toggle.component.html',
   styleUrls: ['./evj-main-toggle.component.scss']
 })
-export class EvjMainToggleComponent implements OnInit {
+export class EvjMainToggleComponent implements OnInit, OnDestroy, OnChanges {
+
+    @Input() isEventOpen: boolean;
+    @Input() eventProdTask: string;
+    @Input() tankInfo: boolean;
+    @Input() blockWorkspaceButton: boolean;
+    @Input() public toggleAstue: boolean = true;
+    public localeSelect: { name: string; id: number }[];
+    @Input() set select(data) {
+        if (data) {
+            this.localeSelect = data;
+            this.selectValue = data?.[0];
+            this.selected.emit(this.selectValue);
+            this.chDet.detectChanges();
+        }
+    }
+    @Input() public ejcoTabs?: IEjcoOnpzUnit[] = [];
+    @Output() public eventProdTaskChange: EventEmitter<void> = new EventEmitter<void>();
     @Output() eventCreated: EventEmitter<boolean> = new EventEmitter<boolean>();
     @Output() eventBack: EventEmitter<null> = new EventEmitter<null>();
-    @Output() public eventProdTaskChange: EventEmitter<void> = new EventEmitter<void>();
-    @Input() blockWorkspaceButton: boolean;
-    @Input() eventProdTask: string;
-    public createIcon: boolean = true;
-  constructor() { }
+    @Output() public selected: EventEmitter<any> = new EventEmitter<any>();
+    @Output() public selectedMenu: EventEmitter<any> = new EventEmitter<any>();
+    @Output() private toggleAstueChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+    @Output() public ejcoTabClicked?: EventEmitter<string | null> = new EventEmitter<string | null>();
+    private subscriptions: Subscription[] = [];
+    claimWidgets: EnumClaimWidgets[] = [];
+    EnumClaimWidgets: typeof EnumClaimWidgets = EnumClaimWidgets;
 
-  ngOnInit(): void {
-  }
-    public createEvent(event: boolean): void {
+    public selectValue: { name: string; id: number };
+
+    public createIcon: boolean = true;
+
+    public filterTankInfo: boolean = false;
+
+    public ejcoActiveTab: string | null = null;
+
+    constructor(
+        public overlayService: OverlayService,
+        public widgetService: WidgetService,
+        public userSettings: UserSettingsService,
+        private claimService: ClaimService,
+        private chDet: ChangeDetectorRef
+    ) {}
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        this.filterTankInfo = this.tankInfo;
+        this.createIcon = this.isEventOpen;
+        if (changes.ejcoTabs && this.ejcoTabs && !this.ejcoActiveTab) {
+            this.handleEjcoTabClick(this.ejcoTabs[0].caption);
+        }
+    }
+
+    public ngOnInit(): void {
+        this.subscriptions.push(
+            this.claimService.claimWidgets$.subscribe((data) => {
+                this.claimWidgets = data;
+            })
+        );
+    }
+
+    public ngOnDestroy(): void {
+        if (this.subscriptions.length > 0) {
+            for (const subscribe of this.subscriptions) {
+                subscribe.unsubscribe();
+            }
+        }
+    }
+
+    public createEvent(event): void {
         this.createIcon = false;
         this.blockWorkspaceButton = true;
         this.eventCreated.emit(event);
@@ -28,5 +100,18 @@ export class EvjMainToggleComponent implements OnInit {
 
     public backEvent(): void {
         this.eventBack.emit();
+    }
+
+    compareFn(o1: any, o2: any): boolean {
+        return o1.name === o2.name && o1.id === o2.id;
+    }
+
+    public handleEjcoTabClick(caption: string | null = null): void {
+        if (!caption) {
+            this.ejcoTabClicked.emit();
+            return;
+        }
+        this.ejcoActiveTab = caption;
+        this.ejcoTabClicked.emit(caption);
     }
 }
