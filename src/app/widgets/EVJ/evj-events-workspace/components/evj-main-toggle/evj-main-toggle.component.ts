165a0452491870424f3c1333997931f5919a08bd
Kirill Kim <kirill.kim@hotmail.com>
2020-11-01 20:56:17 +0300

    header and buttons fixed

--- src/app/widgets/EVJ/evj-events-workspace/components/evj-main-toggle/evj-main-toggle.component.ts
+++ src/app/widgets/EVJ/evj-events-workspace/components/evj-main-toggle/evj-main-toggle.component.ts
@@ -21,6 +21,14 @@ import { ClaimService, EnumClaimWidgets } from '../../../../../dashboard/service
   styleUrls: ['./evj-main-toggle.component.scss']
 })
 export class EvjMainToggleComponent implements OnInit, OnDestroy, OnChanges {
+    @Input() isPreview: boolean;
+    @Input() widgetType: string;
+    @Input() title: string;
+    @Input() units: string;
+    @Input() code: string;
+    @Input() id: string;
+    @Input() uniqId: string;
+    @Input() icon: string = 'shedule';
 
     @Input() isEventOpen: boolean;
     @Input() eventProdTask: string;
@@ -44,6 +52,7 @@ export class EvjMainToggleComponent implements OnInit, OnDestroy, OnChanges {
     @Output() public selectedMenu: EventEmitter<any> = new EventEmitter<any>();
     @Output() private toggleAstueChange: EventEmitter<boolean> = new EventEmitter<boolean>();
     @Output() public ejcoTabClicked?: EventEmitter<string | null> = new EventEmitter<string | null>();
+    public readonly iconRoute: string = 'assets/icons/widget-title-icons/';
     private subscriptions: Subscription[] = [];
     claimWidgets: EnumClaimWidgets[] = [];
     EnumClaimWidgets: typeof EnumClaimWidgets = EnumClaimWidgets;
@@ -51,6 +60,7 @@ export class EvjMainToggleComponent implements OnInit, OnDestroy, OnChanges {
     public selectValue: { name: string; id: number };
 
     public createIcon: boolean = true;
+    public isReportButton: boolean = true;
 
     public filterTankInfo: boolean = false;
 
@@ -88,6 +98,10 @@ export class EvjMainToggleComponent implements OnInit, OnDestroy, OnChanges {
         }
     }
 
+    public async onRemoveButton(): Promise<void> {
+        await this.userSettings.removeItem(this.uniqId);
+    }
+
     public createEvent(event): void {
         this.createIcon = false;
         this.blockWorkspaceButton = true;
@@ -102,8 +116,33 @@ export class EvjMainToggleComponent implements OnInit, OnDestroy, OnChanges {
         this.eventBack.emit();
     }
 
+    public onSelected(event): void {
+        if (event) {
+            this.selected.emit(event.value);
+        }
+    }
+
+    public onSelectedMenu(event: boolean): void {
+        this.isEventOpen = event;
+        this.selectedMenu.emit(event);
+    }
+
+    public reportSelected(event: boolean): void {
+        this.selected.emit(event);
+        this.isReportButton = event;
+    }
+
     compareFn(o1: any, o2: any): boolean {
         return o1.name === o2.name && o1.id === o2.id;
+    }
+
+    public onFilterTankInfo(): void {
+        this.filterTankInfo = true;
+        this.selected.emit(true);
+    }
+
+    public toggleAstueEfficiency(isInitialDataShow: boolean): void {
+        this.toggleAstueChange.emit(isInitialDataShow);
     }
 
     public handleEjcoTabClick(caption: string | null = null): void {
