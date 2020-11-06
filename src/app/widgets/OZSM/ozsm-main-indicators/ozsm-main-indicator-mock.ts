7725c7f45faf283186cc9c869b995efb76200c8f
KMatevosyan <71639310+KMatevosyan@users.noreply.github.com>
2020-11-06 20:14:27 +0700

    Update ozsm-main-indicator-mock.ts

--- src/app/widgets/OZSM/ozsm-main-indicators/ozsm-main-indicator-mock.ts
+++ src/app/widgets/OZSM/ozsm-main-indicators/ozsm-main-indicator-mock.ts
@@ -2,15 +2,15 @@ export const mockData = [
     {
         id: '0',
         title: 'Смесь гудрона',
-        fact: 17586,
-        plan: 15208,
+        fact: 17940,
+        plan: 17978,
         icon: 'attention'
     },
     {
         id: '1',
-        title: 'Смесь гудрона',
-        fact: 15208,
-        plan: 17586,
+        title: 'Масляные дистиляты 12638asdsadsadsadsadsadad761',
+        fact: 17586,
+        plan: 15208,
         icon: 'lines'
     }
 ];
