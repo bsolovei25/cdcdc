81973540de94c85556599f63111b9b4f7835f4ae
KMatevosyan <71639310+KMatevosyan@users.noreply.github.com>
2020-11-19 19:44:31 +0700

    Update kpe-line-chart.component.ts

--- src/app/widgets/KPE/shared/kpe-line-chart/kpe-line-chart.component.ts
+++ src/app/widgets/KPE/shared/kpe-line-chart/kpe-line-chart.component.ts
@@ -264,37 +264,6 @@ export class KpeLineChartComponent implements OnChanges, AfterViewInit {
             r = r / 2;
             opacity = opacity * 4;
         }
-
-
-        // this.chartData.forEach((item) => {
-        //     if (item.graphType !== 'fact' && item.graphType !== 'plan') {
-        //         return;
-        //     }
-        //     if (item.graphType === 'plan') {
-        //         const x = fact[fact.length - 1].x;
-        //         const y = plan.find(corrdinate => corrdinate.x === x)?.y;
-        //         pointsG
-        //             .append('circle')
-        //             .attr('class', 'point point_plan')
-        //             .attr('cx', x)
-        //             .attr('cy', y)
-        //             .attr('r', 2);
-        //     } else {
-        //         const g = pointsG.append('g').attr('class', 'fact-point');
-        //         let r = 8;
-        //         let opacity = 0.05;
-        //         for (let i = 0; i < 4; i++) {
-        //             g.append('circle')
-        //                 .attr('class', this.lastPointColor)
-        //                 .attr('cx', item.graph[item.graph.length - 1].x)
-        //                 .attr('cy', item.graph[item.graph.length - 1].y)
-        //                 .attr('r', r)
-        //                 .style('opacity', opacity);
-        //             r = r / 2;
-        //             opacity = opacity * 4;
-        //         }
-        //     }
-        // });
     }
 
     private ColorizeDraw(ColorizeCoordinates: IChartD3[]): void {
