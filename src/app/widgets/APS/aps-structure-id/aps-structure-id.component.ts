6841cbaa615ea0ec8c705e2b5e60ae2e162c47e0
Kirill Kim <kirill.kim@hotmail.com>
2020-11-20 14:45:25 +0300

    structure-id

--- src/app/widgets/APS/aps-structure-id/aps-structure-id.component.ts
+++ src/app/widgets/APS/aps-structure-id/aps-structure-id.component.ts
@@ -6,15 +6,69 @@ import { IParams } from '../../CD/cd-mat-balance/cd-mat-balance.component';
 import { heatBalanceData } from '../../ASTUE-ONPZ/astue-onpz-heat-balance/astue-onpz-heat-balance-mock';
 import { IColumnsToDisplay } from '../aps-recipe-diagram/aps-recipe-diagram.component';
 
+export interface IStructure {
+    unit: { name: string };
+    list: IStructureList[];
+}
+
+export interface IStructureList {
+    id: string;
+    name: string;
+}
+
 @Component({
-  selector: 'evj-aps-structure-id',
-  templateUrl: './aps-structure-id.component.html',
-  styleUrls: ['./aps-structure-id.component.scss']
+    selector: 'evj-aps-structure-id',
+    templateUrl: './aps-structure-id.component.html',
+    styleUrls: ['./aps-structure-id.component.scss']
 })
 export class ApsStructureIdComponent extends WidgetPlatform<unknown> implements OnInit, OnDestroy {
-    data: IParams[] = heatBalanceData;
+    public data: IStructure[] = [{
+        unit: { name: 'Технологическая устрановка' },
+        list: [
+            {
+                id: '0',
+                name: 'Справочник'
+            },
+            {
+                id: '1',
+                name: 'Режимы работы ТУ'
+            },
+            {
+                id: '2',
+                name: 'Скорость изменнеия загрузки'
+            }
+            ,
+            {
+                id: '3',
+                name: 'Диапазон производительности по регламенту'
+            }
+            ]
+    },
+        {
+            unit: { name: 'Материальный поток' },
+            list: [
+                {
+                    id: '00',
+                    name: 'Справочник'
+                },
+                {
+                    id: '11',
+                    name: 'Режимы работы ТУ'
+                },
+                {
+                    id: '22',
+                    name: 'Скорость изменнеия загрузки'
+                }
+                ,
+                {
+                    id: '33',
+                    name: 'Диапазон производительности по регламенту'
+                }
+            ]
+        }
+    ];
     columnsToDisplay: IColumnsToDisplay[] = [
-        { name: 'Показатели, Дж', id: 0, date: new Date() },
+        { name: 'Показатели, Дж', id: 0, date: new Date() }
     ];
 
     expandedElement: SelectionModel<string> = new SelectionModel(true);
@@ -33,12 +87,14 @@ export class ApsStructureIdComponent extends WidgetPlatform<unknown> implements 
     ngOnInit(): void {
         super.widgetInit();
     }
+
     ngOnDestroy(): void {
         super.ngOnDestroy();
     }
 
     protected dataHandler(ref: any): void {
     }
+
     onClickTr(event: MouseEvent, element: any): void {
         event.stopPropagation();
         if (this.expandedElement.isSelected(element.name)) {
