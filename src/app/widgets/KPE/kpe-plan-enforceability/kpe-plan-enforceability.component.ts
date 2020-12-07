746d410f0317876db58dc7cf780c71f484a7f0fa
KMatevosyan <71639310+KMatevosyan@users.noreply.github.com>
2020-12-07 13:48:44 +0700

    fixes

--- src/app/widgets/KPE/kpe-plan-enforceability/kpe-plan-enforceability.component.ts
+++ src/app/widgets/KPE/kpe-plan-enforceability/kpe-plan-enforceability.component.ts
@@ -29,4 +29,11 @@ export class KpePlanEnforceabilityComponent extends WidgetPlatform<unknown> impl
   protected dataHandler(ref: any): void {
   }
 
+  public chartHeight(container: HTMLDivElement): string {
+    if (!(container?.offsetHeight > 0)) {
+      return;
+    }
+    const height = container.offsetHeight;
+    return `height: ${height * 0.6}px`;
+}
 }
