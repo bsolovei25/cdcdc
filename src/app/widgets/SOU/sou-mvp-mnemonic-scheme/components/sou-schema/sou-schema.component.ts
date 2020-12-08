081cf39cce913ed929147480bd5b2194af95968b
Medvedev <medvedd07@gmail.com>
2020-12-08 23:46:09 +0300

    add code

--- src/app/widgets/SOU/sou-mvp-mnemonic-scheme/components/sou-schema/sou-schema.component.ts
+++ src/app/widgets/SOU/sou-mvp-mnemonic-scheme/components/sou-schema/sou-schema.component.ts
@@ -2,9 +2,11 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 interface IDataSou {
     id: number;
-    deviation: boolean;
+    deviation?: boolean;
     text: string;
-    value: number;
+    value?: number;
+    active: boolean;
+    percent?: number;
 }
 
 @Component({
@@ -12,16 +14,49 @@ interface IDataSou {
     templateUrl: './sou-schema.component.html',
     styleUrls: ['./sou-schema.component.scss'],
 })
-export class SouSchemaComponent implements OnInit, AfterViewInit {
+export class SouSchemaComponent implements AfterViewInit {
     elementsNode: Element[] = []; // все элементы
-    dataAttribute: Map<number, Element> = new Map();
+    dataAttribute: Map<number, Element> = new Map(); // id элемента, элемент
+    nameAttribute: string = 'node-id';
 
-    constructor() {}
-
-    ngOnInit(): void {}
+    data: IDataSou[] = [
+        {
+            id: 14,
+            text: 'FR-92',
+            value: 10,
+            percent: 20,
+            deviation: true,
+            active: true,
+        },
+        {
+            id: 15,
+            text: 'FR-92',
+            value: 10,
+            percent: 20,
+            deviation: true,
+            active: true,
+        },
+    ];
 
     ngAfterViewInit(): void {
         this.loadSchema();
+        this.loadData();
+    }
+
+    loadData(): void {
+        this.data.forEach((value) => {
+            const element = this.dataAttribute.get(value.id);
+            const mode = value.deviation ? 'deviation' : value.active ? 'standard' : 'disabled';
+            this.element_3(
+                mode,
+                element,
+                value.percent,
+                value.value,
+                value.text,
+                value.active,
+                value.deviation
+            );
+        });
     }
 
     loadSchema(): void {
@@ -39,6 +74,7 @@ export class SouSchemaComponent implements OnInit, AfterViewInit {
     }
 
     element_3(
+        mode: 'standard' | 'deviation' | 'disabled' | 'reset',
         element: Element,
         percent?: number,
         value?: number,
@@ -46,44 +82,145 @@ export class SouSchemaComponent implements OnInit, AfterViewInit {
         active?: boolean,
         deviation?: boolean
     ): void {
-        const el = Array.from(element.children);
+        if (element?.children) {
+            if (mode === 'reset') {
+                value = 0;
+                percent = 0;
+                text = '';
+                active = false;
+                deviation = false;
+            }
+            const el = Array.from(element?.children);
+            const rects: Element[] = [];
+            const points: Element[] = [];
+            const arrows: Element[] = [];
+            const texts: Element[] = [];
 
-        const rects = [];
-        const points = [];
-        const texts = [];
-        el.forEach((elem) => {
-            const name = elem.getAttribute('id');
-            if (name.includes('rect')) {
-                rects.push(elem);
+            let circle: Element;
+            let textValue: Element;
+            let textPercent: Element;
+
+            // Search
+            el?.forEach((elem) => {
+                const name = elem.getAttribute('id');
+                if (name.includes('rect')) {
+                    rects.push(elem);
+                }
+                if (name.includes('point')) {
+                    points.push(elem);
+                }
+                if (name.includes('text')) {
+                    if (name.includes('text_value')) {
+                        textValue = elem;
+                    } else if (name.includes('text_percent')) {
+                        textPercent = elem;
+                    } else {
+                        texts.push(elem);
+                    }
+                }
+                if (name.includes('arrow-group')) {
+                    arrows.push(...this.searchArrow(elem?.children));
+                }
+                if (name.includes('circle')) {
+                    circle = elem;
+                }
+            });
+            element.setAttribute('class', 'element');
+            // Event
+
+            rects.forEach((item) => {
+                item.classList.remove('standard', 'deviation', 'disabled', 'reset');
+                item.classList.add(mode);
+            });
+            points.forEach((item) => {
+                item.classList.remove('standard', 'deviation', 'disabled', 'reset');
+                item.classList.add(mode);
+            });
+            texts.forEach((item) => {
+                item.classList.toggle(`${mode}-text`);
+                this.addTextToTspan(item, String(text));
+            });
+            arrows.forEach((item) => {
+                item.classList.toggle(`${mode}-arrow`);
+            });
+            circle?.classList.remove();
+            circle?.classList.add(mode);
+            this.addTextToTspan(textPercent, `${String(percent)}%`);
+            textPercent?.classList.remove(
+                'standard-text',
+                'deviation-text',
+                'disabled-text',
+                'reset-text'
+            );
+            textPercent?.classList.add(`${mode}-text`);
+            this.addTextToTspan(textValue, `${String(value)} тн`);
+            textValue?.classList.remove(
+                'standard-text',
+                'deviation-text',
+                'disabled-text',
+                'reset-text'
+            );
+            textValue?.classList.add(`${mode}-text`);
+            console.log('1');
+            if (mode === 'reset') {
+                element.addEventListener('click', () => {
+                    console.log('2');
+                    rects.forEach((item) => {
+                        item.classList.toggle('active');
+                    });
+                    points.forEach((item) => {
+                        item.classList.toggle('active');
+                    });
+                    texts.forEach((item) => {
+                        item.classList.toggle('active-text');
+                    });
+                    arrows.forEach((item) => {
+                        item.classList.toggle('active-arrow');
+                    });
+                    circle?.classList.toggle('active');
+                    textPercent?.classList.toggle('active-text');
+                    textValue?.classList.toggle('active-text');
+                });
             }
-            if (name.includes('point')) {
-                points.push(elem);
-            }
-            if (name.includes('text')) {
-                texts.push(elem);
+        }
+    }
+
+    addTextToTspan(element: Element, text: string): void {
+        if (element?.children) {
+            const children = Array.from(element?.children);
+            children?.forEach((child) => {
+                child.textContent = text;
+            });
+        }
+    }
+
+    searchArrow(elements: HTMLCollection): Element[] {
+        const arrow: Element[] = [];
+        Array.from(elements).forEach((element) => {
+            const name = element.getAttribute('id');
+            if (
+                name?.includes('arrow') &&
+                !name?.includes('arrow_double') &&
+                !name?.includes('arrow_right_simple')
+            ) {
+                arrow.push(element);
+                if (element.children.length) {
+                    arrow.push(...this.searchArrow(element.children));
+                }
+            } else {
+                if (element.children.length) {
+                    arrow.push(...this.searchArrow(element.children));
+                }
             }
         });
-        console.log(rects, points, texts);
-
-        element.setAttribute('class', 'element-3');
-        element.addEventListener('click', () => {
-            rects.forEach((rect) => {
-                rect.setAttribute('class', 'active');
-            });
-            points.forEach((rect) => {
-                rect.setAttribute('class', 'active');
-            });
-            texts.forEach((rect) => {
-                rect.setAttribute('class', 'active-text');
-            });
-        });
+        return arrow;
     }
 
     searchAttribute(): void {
         this.elementsNode.forEach((element) => {
-            const id = element.getAttribute('node-id');
+            const id = element.getAttribute(this.nameAttribute);
             if (id) {
-                this.dataAttribute.set(+id, element); // Заполнение Map атрибутом и элементовd
+                this.dataAttribute.set(+id, element); // Заполнение Map атрибутом и элементов
             }
         });
     }
@@ -96,15 +233,14 @@ export class SouSchemaComponent implements OnInit, AfterViewInit {
                 // только для первого элемента - id=element-2
                 const element = document.querySelector(`#element-${elementName}`);
                 if (element) {
-                    if (elementName === 3) {
-                        this.element_3(element);
-                    }
+                    this.element_3('reset', element);
                     localElements.push(element);
                 }
             }
             // поиск по id  - id=element-1_2
             const element1 = document.querySelector(`#element-${elementName}_${i}`);
             if (element1) {
+                this.element_3('reset', element1);
                 localElements.push(element1);
             } else {
                 break;
