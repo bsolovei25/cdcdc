afbbc8a28c4956e278c9fc9cdc9fa1431860335a
TarasovMV <31613333+TarasovMV@users.noreply.github.com>
2020-12-13 10:13:47 +0300

    upd channel

--- src/app/dashboard/models/@PLATFORM/channel-platform.ts
+++ src/app/dashboard/models/@PLATFORM/channel-platform.ts
@@ -1,34 +1,29 @@
 import { Directive, Inject, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { WidgetService } from '../../services/widget.service';
 
 @Directive()
 // tslint:disable-next-line:directive-class-suffix
 export abstract class ChannelPlatform<T, O = null> implements OnInit, OnDestroy {
-
     public subscriptions: Subscription[] = [];
+    wsSubscription: Subscription = null;
 
     protected constructor(
         @Inject('widgetId') public widgetId: string,
         @Inject('channelId') public channelId: string,
         protected widgetService: WidgetService
-    ) {
-    }
+    ) {}
 
     ngOnInit(): void {
         if (!this.widgetId || !this.channelId) {
             return;
         }
-        this.subscriptions.push(
-            this.widgetService.getChannelLiveDataFromWs(this.channelId, this.widgetId)
-                .subscribe(this.dataHandler.bind(this))
-        );
+        this.connectWs();
     }
 
     ngOnDestroy(): void {
         this.subscriptions.forEach((s) => s.unsubscribe());
         this.subscriptions = [];
-        this.widgetService.removeChannel(this.widgetId, this.channelId);
     }
 
     protected appendWsOptions(options: O): void {
@@ -36,4 +31,22 @@ export abstract class ChannelPlatform<T, O = null> implements OnInit, OnDestroy 
     }
 
     protected abstract dataHandler(ref: T): void;
+
+    protected connectWs(
+        channelId: string = this.channelId,
+        widgetId: string = this.widgetId
+    ): void {
+        this.channelId = this.channelId !== channelId ? channelId : this.channelId;
+        this.wsSubscription = this.widgetService
+            .getChannelLiveDataFromWs(channelId, widgetId)
+            .subscribe(this.dataHandler.bind(this));
+    }
+
+    protected disconnectWs(): void {
+        if (!this.wsSubscription) {
+            return;
+        }
+        this.wsSubscription.unsubscribe();
+        this.widgetService.removeChannel(this.widgetId, this.channelId);
+    }
 }
