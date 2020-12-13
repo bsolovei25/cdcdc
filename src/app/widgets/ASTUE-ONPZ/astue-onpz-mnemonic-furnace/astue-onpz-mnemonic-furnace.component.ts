2e4f50cfc43fc89fd8d114f0e6a751bdfd3e3b29
TarasovMV <31613333+TarasovMV@users.noreply.github.com>
2020-12-13 17:59:04 +0300

    fix

--- src/app/widgets/ASTUE-ONPZ/astue-onpz-mnemonic-furnace/astue-onpz-mnemonic-furnace.component.ts
+++ src/app/widgets/ASTUE-ONPZ/astue-onpz-mnemonic-furnace/astue-onpz-mnemonic-furnace.component.ts
@@ -94,6 +94,11 @@ export class AstueOnpzMnemonicFurnaceComponent extends WidgetPlatform implements
     ) {
         super(widgetService, isMock, id, uniqId);
         mnemonicFurnaceService.furnaceOptionsReferences = this.selectReferences;
+        this.mnemonicFurnaceService.furnaceOptions$ = combineLatest([
+            this.selectManufacture.valueChanges,
+            this.selectUnit.valueChanges,
+            this.selectOven.valueChanges,
+        ]).pipe(map((x) => ({ manufactureId: x[0], unitId: x[1], ovenId: x[2] })));
     }
 
     ngOnInit(): void {
@@ -112,12 +117,6 @@ export class AstueOnpzMnemonicFurnaceComponent extends WidgetPlatform implements
         this.selectOven.valueChanges.pipe(takeUntil(this.onDestroy)).subscribe((value) => {
             this.setData();
         });
-
-        this.mnemonicFurnaceService.furnaceOptions$ = combineLatest([
-            this.selectManufacture.valueChanges,
-            this.selectUnit.valueChanges,
-            this.selectOven.valueChanges,
-        ]).pipe(map((x) => ({ manufactureId: x[0], unitId: x[1], ovenId: x[2] })));
 
         this.mnemonicFurnaceService.selectedItem$
             .pipe(takeUntil(this.onDestroy))
