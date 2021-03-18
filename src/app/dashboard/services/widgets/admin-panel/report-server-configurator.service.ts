5066092eca446e7f08f4ffd8bd9aaa364ddd4bce
Константин Медведев <medvedev.ko@gazprom-neft.com>
2021-03-18 23:14:37 +0300

    add drag n drop

--- src/app/dashboard/services/widgets/admin-panel/report-server-configurator.service.ts
+++ src/app/dashboard/services/widgets/admin-panel/report-server-configurator.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { AppConfigService } from '@core/service/app-config.service';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { AppConfigService } from "@core/service/app-config.service";
+import { Observable, BehaviorSubject } from "rxjs";
 import {
     IFileTemplate,
     IReportTemplate,
@@ -9,12 +9,12 @@ import {
     ISystemOptions,
     IReportFile,
     IFolder,
-    IPostSystemOptionsTemplate,
-} from '../../../models/ADMIN/report-server';
-import { IAlertWindowModel } from '@shared/models/alert-window.model';
+    IPostSystemOptionsTemplate, ICustomOptionsTemplate
+} from "../../../models/ADMIN/report-server";
+import { IAlertWindowModel } from "@shared/models/alert-window.model";
 
 @Injectable({
-    providedIn: 'root',
+    providedIn: "root"
 })
 export class ReportServerConfiguratorService {
     private restUrl: string;
@@ -26,89 +26,98 @@ export class ReportServerConfiguratorService {
     public alertWindow$: BehaviorSubject<IAlertWindowModel> = new BehaviorSubject<IAlertWindowModel>(null);
 
     public getReportFileTemplate(): Observable<IReportFile[]> {
-        return this.http.get<IReportFile[]>(this.restUrl + '/api/report-filetemplate/all');
+        return this.http.get<IReportFile[]>(this.restUrl + "/api/report-filetemplate/all");
     }
 
     public getReportTemplate(): Observable<IReportTemplate[]> {
-        return this.http.get<IReportTemplate[]>(this.restUrl + '/api/report-template/all');
+        return this.http.get<IReportTemplate[]>(this.restUrl + "/api/report-template/all");
     }
 
     public getTepmplate(id: number): Observable<any[]> {
-        return this.http.get<any[]>(this.restUrl + '/api/report-filetemplate/' + id + '/templates');
+        return this.http.get<any[]>(this.restUrl + "/api/report-filetemplate/" + id + "/templates");
     }
 
     public getReporting(id: number): Observable<IReportTemplate> {
-        return this.http.get<IReportTemplate>(this.restUrl + '/api/report-template/' + id);
+        return this.http.get<IReportTemplate>(this.restUrl + "/api/report-template/" + id);
     }
 
     public getSystemOptions(): Observable<ISystemOptions[]> {
-        return this.http.get<ISystemOptions[]>(this.restUrl + '/api/report-options/system/all');
+        return this.http.get<ISystemOptions[]>(this.restUrl + "/api/report-options/system/all");
     }
 
     public getUserOptions(): Observable<any[]> {
-        return this.http.get<any[]>(this.restUrl + '/api/report-options/custom/all');
+        return this.http.get<any[]>(this.restUrl + "/api/report-options/custom/all");
     }
 
     public getTemplateFolder(): Observable<IFolder> {
-        return this.http.get<IFolder>(this.restUrl + '/api/report-folders/all');
+        return this.http.get<IFolder>(this.restUrl + "/api/report-folders/all");
     }
 
     public pushReportFile(file: any): Observable<any> {
         const body: FormData = new FormData();
-        body.append('uploadFile', file, file.name);
-        return this.http.post<any>(this.restUrl + '/api/file-storage', body);
+        body.append("uploadFile", file, file.name);
+        return this.http.post<any>(this.restUrl + "/api/file-storage", body);
     }
 
     public postReportFileTemplate(body): Observable<IFileTemplate> {
-        return this.http.post<IFileTemplate>(this.restUrl + '/api/report-filetemplate/loaded', body);
+        return this.http.post<IFileTemplate>(this.restUrl + "/api/report-filetemplate/loaded", body);
     }
 
     public postSystemOptions(id, options): Observable<IPostSystemOptionsTemplate> {
         return this.http.post<IPostSystemOptionsTemplate>(
-            this.restUrl + '/api/report-template/' + id + '/system-options',
+            this.restUrl + "/api/report-template/" + id + "/system-options",
             options
         );
     }
 
     public postCustomOptions(id, options): Observable<any> {
-        return this.http.post<any>(this.restUrl + '/api/report-template/' + id + '/options', options);
+        return this.http.post<any>(this.restUrl + "/api/report-template/" + id + "/options", options);
     }
 
     public postReportTemplate(template): Observable<any> {
-        return this.http.post<any>(this.restUrl + '/api/report-template', template);
+        return this.http.post<any>(this.restUrl + "/api/report-template", template);
     }
 
     public postTemplateFolder(folder): Observable<any> {
-        return this.http.post<any>(this.restUrl + '/api/report-folders', folder);
+        return this.http.post<any>(this.restUrl + "/api/report-folders", folder);
     }
 
     public putReportFileTemplate(filetemplate): Observable<any> {
-        return this.http.put<any>(this.restUrl + '/api/report-filetemplate', filetemplate);
+        return this.http.put<any>(this.restUrl + "/api/report-filetemplate", filetemplate);
     }
 
     public putReportTemplate(template): Observable<any> {
-        return this.http.put<any>(this.restUrl + '/api/report-template', template);
+        return this.http.put<any>(this.restUrl + "/api/report-template", template);
     }
 
     public putFolderTemplate(folder): Observable<any> {
-        return this.http.put<any>(this.restUrl + '/api/report-folders', folder);
+        return this.http.put<any>(this.restUrl + "/api/report-folders", folder);
     }
 
     public deleteReportFileTemplate(id: number): Observable<any> {
-        return this.http.delete<any>(this.restUrl + '/api/report-filetemplate/' + id);
+        return this.http.delete<any>(this.restUrl + "/api/report-filetemplate/" + id);
     }
 
     public deleteReportTemplate(id: number): Observable<any> {
-        return this.http.delete<any>(this.restUrl + '/api/report-template/' + id);
+        return this.http.delete<any>(this.restUrl + "/api/report-template/" + id);
     }
 
     public deleteFolder(id: number): Observable<any> {
-        return this.http.delete<any>(this.restUrl + '/api/report-folders/' + id);
+        return this.http.delete<any>(this.restUrl + "/api/report-folders/" + id);
     }
 
     public closeAlert(): void {
         this.alertWindow$.next(null);
     }
 
-    ///PROMISE-STYLE
+    public getReportFileNameOptions(templateId: number): Observable<ICustomOptionsTemplate> {
+        return this.http.get<ICustomOptionsTemplate>(this.restUrl + `/api/report-template/${templateId}/filename-options`);
+    }
+
+    public postReportFileNameOptions(templateId: number, template: ICustomOptionsTemplate): Observable<void> {
+        return this.http.post<void>(this.restUrl + `/api/report-template/${templateId}/filename-options`, template);
+    }
+
+
+    // PROMISE-STYLE
 }
