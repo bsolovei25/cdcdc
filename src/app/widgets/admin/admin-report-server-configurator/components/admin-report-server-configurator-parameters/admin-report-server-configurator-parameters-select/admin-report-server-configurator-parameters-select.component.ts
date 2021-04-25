219b47c06418e556a72f100c01270e231a111dfd
Kirill Kim <kirill.kim@hotmail.com>
2021-04-26 01:50:03 +0300

    fix

--- src/app/widgets/admin/admin-report-server-configurator/components/admin-report-server-configurator-parameters/admin-report-server-configurator-parameters-select/admin-report-server-configurator-parameters-select.component.ts
+++ src/app/widgets/admin/admin-report-server-configurator/components/admin-report-server-configurator-parameters/admin-report-server-configurator-parameters-select/admin-report-server-configurator-parameters-select.component.ts
@@ -2,7 +2,7 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit, ChangeDetectionStrategy, Inject, Input } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ISystemOptions, ISystemOptionsTemplate } from '@dashboard/models/ADMIN/report-server.model';
-import { IReportTemplate } from '@widgets/admin/admin-report-server-configurator/models/admin-report-server-configurator.model';
+import { IPostSystemOptionsTemplate, IReportTemplate } from '@widgets/admin/admin-report-server-configurator/models/admin-report-server-configurator.model';
 import { AdminReportServerConfiguratorRootService } from '@widgets/admin/admin-report-server-configurator/services/admin-report-server-configurator-root.service';
 import { AdminReportConfiguratorService } from '@widgets/admin/admin-report-server-configurator/services/admin-report-server-configurator.service';
 
@@ -16,7 +16,6 @@ export class AdminReportServerConfiguratorParametersSelectComponent implements O
 
   public readonly closePopupIcon = 'assets/icons/widgets/admin/admin-report-server-configurator/close-popup.svg';
   public readonly listIcon = 'assets/icons/widgets/admin/admin-report-server-configurator/list.svg';
-  public selectParameter: SelectionModel<ISystemOptions> = new SelectionModel<ISystemOptions>(true);
   public selectedParameters: ISystemOptions[] = [];
   @Input() public report: IReportTemplate = null;
 
@@ -49,6 +48,14 @@ export class AdminReportServerConfiguratorParametersSelectComponent implements O
       console.log(this.selectedParameters);
       
     }
+  }
+
+  public onSave(): void {
+  //   const postSystemOptionsTemplate: IPostSystemOptionsTemplate = {
+  //     systemOptionValues: optionObject,
+  //     fileTemplate: this.data.data,
+  //     periodType: this.data.data.periodType,
+  // };
   }
 
 }
