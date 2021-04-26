da5abf31530d40124360858183efb3f13d1f2d3e
TaisiiaFenz <51312876+TaisiiaFenz@users.noreply.github.com>
2021-04-26 09:39:00 +0300

    1658 - finish table

--- src/app/widgets/SOU/sou-streams/sou-streams.component.ts
+++ src/app/widgets/SOU/sou-streams/sou-streams.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, ChangeDetectionStrategy, Inject } from "@angular/cor
 import { WidgetService } from "@dashboard/services/widget.service";
 import { WidgetPlatform } from "@dashboard/models/@PLATFORM/widget-platform";
 
+import { TITLES_OF_TABLE } from "@widgets/SOU/sou-streams/config";
+import { TABLE_CELLS } from "@widgets/SOU/sou-streams/mock";
+
 @Component({
   selector: 'evj-sou-streams',
   templateUrl: './sou-streams.component.html',
@@ -10,18 +13,23 @@ import { WidgetPlatform } from "@dashboard/models/@PLATFORM/widget-platform";
 })
 export class SouStreamsComponent extends WidgetPlatform implements OnInit {
 
-  constructor(
-      protected widgetService: WidgetService,
+    public titlesOfTable: { name: string, widthOfBlock: string }[] = TITLES_OF_TABLE;
+    public tableRows: {} = TABLE_CELLS;
 
-      @Inject('widgetId') public id: string,
-      @Inject('uniqId') public uniqId: string
-  ) {
-      super(widgetService, id, uniqId);
-  }
+    public heightOfTable: number = 70;
 
-  ngOnInit(): void {
-      super.widgetInit();
-  }
+    constructor(
+        protected widgetService: WidgetService,
+
+        @Inject('widgetId') public id: string,
+        @Inject('uniqId') public uniqId: string
+    ) {
+        super(widgetService, id, uniqId);
+    }
+
+    ngOnInit(): void {
+        super.widgetInit();
+    }
 
   protected dataHandler(ref: unknown): void {}
 
--- src/app/widgets/SOU/sou-streams/sou-streams.module.ts
+++ src/app/widgets/SOU/sou-streams/sou-streams.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SouStreamsComponent } from "@widgets/SOU/sou-streams/sou-streams.component";
 import { SharedModule } from "@shared/shared.module";
+import { MatSelectModule } from "@angular/material/select";
+import { AngularSvgIconModule } from "angular-svg-icon";
+import { ScrollingModule } from "@angular/cdk/scrolling";
+import { MatRadioModule } from "@angular/material/radio";
 
 
 
@@ -9,7 +13,7 @@ import { SharedModule } from "@shared/shared.module";
   declarations: [
       SouStreamsComponent
   ],
-    imports: [CommonModule, SharedModule]
+  imports: [CommonModule, SharedModule, MatSelectModule, AngularSvgIconModule, ScrollingModule, MatRadioModule]
 })
 export class SouStreamsModule {
     enterComponent = SouStreamsComponent;
