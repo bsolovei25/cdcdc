7ae7a9c7fa83cf4efcc97940f04f35e04f822d3b
TaisiiaFenz <51312876+TaisiiaFenz@users.noreply.github.com>
2021-05-13 13:39:31 +0300

    feat(SOU): create structure of open trend chart. #1859

--- src/app/widgets/SOU/sou-workspace/sou-workspace.module.ts
+++ src/app/widgets/SOU/sou-workspace/sou-workspace.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from '@shared/shared.module';
 import { AngularSvgIconModule } from "angular-svg-icon";
 import { SouWorkspaceInfoBarComponent } from './components/sou-workspace-info-bar/sou-workspace-info-bar.component';
 import { MatSelectModule } from "@angular/material/select";
+import { WorkspaceChartComponent } from './components/workspace-chart/workspace-chart.component';
 
 @NgModule({
-    declarations: [SouWorkspaceComponent, SouWorkspaceInfoBarComponent],
+    declarations: [SouWorkspaceComponent, SouWorkspaceInfoBarComponent, WorkspaceChartComponent],
     imports: [CommonModule, SharedModule, AngularSvgIconModule, MatSelectModule]
 })
 export class SouWorkspaceModule {
