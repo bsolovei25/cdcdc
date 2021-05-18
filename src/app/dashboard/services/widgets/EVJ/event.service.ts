1b6f55230917eb835a0ce9eed0eeb14406241bb7
Timur Samatov <t.samatau@andersenlab.com>
2021-05-18 11:43:21 +0300

    feat(Notifications): Добавил поле Корректирующие мероприятия, исправил высоту секций в категории СМОТР. front#1889

--- src/app/dashboard/services/widgets/EVJ/event.service.ts
+++ src/app/dashboard/services/widgets/EVJ/event.service.ts
@@ -28,6 +28,7 @@ import {
 } from "../../../models/EVJ/events-widget";
 import { AppConfigService } from '@core/service/app-config.service';
 import { catchError } from "rxjs/operators";
+import { of } from 'rxjs';
 
 export interface IEventsFilter {
     unitNames?: string[];
@@ -226,6 +227,33 @@ export class EventService {
                 console.error(error);
                 return EMPTY;
             }));
+    }
+
+    public getSmotrCorrects(): Observable<ICorrect[]> {
+        // return this.http
+        //     .get<ISmotrCorrect[]>(`${this.restUrl}/api/notification-reference/smotr/events`)
+        //     .pipe(catchError((error) => {
+        //         console.error(error);
+        //         return EMPTY;
+        //     }));
+
+        // Корректирующие мероприятия СМОТР
+        return of([
+            {id: "be4b7cd3-9455-11ea-a2cf-005056a5c7f6", name: "Прочие"},
+            {id: "94ad7506-62ee-11eb-a2d1-005056a5c7f6", name: "Внесение информации о режиме работы в технологический регламент"}, 
+            {id: "a24fe9f2-62ee-11eb-a2d1-005056a5c7f6", name: "Проработка корректности действий с технологическим персоналом"}, 
+            {id: "acb70e28-62ee-11eb-a2d1-005056a5c7f6", name: "Техническое перевооружение и модернизация"}, 
+            {id: "acb70e29-62ee-11eb-a2d1-005056a5c7f6", name: "Чистка оборудования"}, 
+            {id: "1dae9b40-6403-11e8-b4a4-005056b52c10", name: "Корректировка параметров технологического режима,направленная на компенсацию либо устранение отклонения"}, 
+            {id: "1dae9b41-6403-11e8-b4a4-005056b52c10", name: "Корректировка параметров либо качества сырья"}, 
+            {id: "1dae9b42-6403-11e8-b4a4-005056b52c10", name: "Ремонт оборудования"}, 
+            {id: "1dae9b44-6403-11e8-b4a4-005056b52c10", name: "Повторное проведение лабораторного анализа"}, 
+            {id: "1dae9b45-6403-11e8-b4a4-005056b52c10", name: "Рассмотрение возможности пересмотра норматива технологического регламента"}, 
+            {id: "1dae9b47-6403-11e8-b4a4-005056b52c10", name: "Восстановление работоспособности КиП и систем автоматизации"}, 
+            {id: "1dae9b48-6403-11e8-b4a4-005056b52c10", name: "Замена оборудования"}, 
+            {id: "1dae9b49-6403-11e8-b4a4-005056b52c10", name: "Актуализация данных в системе мониторинга (СМОТР)"}, 
+            {id: "1dae9b4a-6403-11e8-b4a4-005056b52c10", name: "Корректировка параметров либо качества энергоносителя и вспомогательных потоков ОЗХ"}
+        ]);
     }
 
     public getReasons(): Observable<IReason[]> {
