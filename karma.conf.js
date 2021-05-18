f1e8bf858594087a118573bd5df117ca41cf631f
Sergey Yakubovskiy <serg.yakubovskiy@gmail.com>
2021-05-18 22:26:13 +0300

    karma.js

--- karma.conf.js
+++ karma.conf.js
@@ -46,4 +46,6 @@ module.exports = function (config) {
     singleRun: true,
     restartOnFileChange: true
   });
+  process.env.NO_PROXY = 'localhost, 0.0.0.0/4201, 0.0.0.0/9876';
+  process.env.no_proxy = 'localhost, 0.0.0.0/4201, 0.0.0.0/9876';
 };
