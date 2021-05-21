835e0974585a7a057c1d24f1380e6911ab359e38
TaisiiaFenz <51312876+TaisiiaFenz@users.noreply.github.com>
2021-05-21 13:08:20 +0300

    feat(SOU): create structure for dropdowns. #1931

--- src/app/widgets/SOU/sou-streams/sou-streams.component.ts
+++ src/app/widgets/SOU/sou-streams/sou-streams.component.ts
@@ -11,7 +11,22 @@ import { WidgetPlatform } from "@dashboard/models/@PLATFORM/widget-platform";
 
 import { TITLES_OF_TABLE } from "@widgets/SOU/sou-streams/config";
 import { TABLE_CELLS } from "@widgets/SOU/sou-streams/mock";
+import { BehaviorSubject, combineLatest, Observable } from "rxjs";
+import { ISouReservoir, ISouManufacture, ISouOptions, ISouUnit } from "@dashboard/models/SOU/sou-streams.model";
+import { FormControl, FormGroup } from "@angular/forms";
+import { map } from "rxjs/operators";
 
+interface ISouSelectionOptionsForm {
+    manufacture: string;
+    unit: string;
+    reservoir: string;
+}
+
+interface ISouSelectionOptions {
+    manufactures$: Observable<ISouManufacture[]>;
+    units$: Observable<ISouUnit[]>;
+    reservoirs$: Observable<ISouReservoir[]>;
+}
 
 @Component({
   selector: 'evj-sou-streams',
@@ -19,6 +34,42 @@ import { TABLE_CELLS } from "@widgets/SOU/sou-streams/mock";
   styleUrls: ['./sou-streams.component.scss']
 })
 export class SouStreamsComponent extends WidgetPlatform implements OnInit, AfterViewChecked  {
+
+    optionsGroup: FormGroup = new FormGroup({
+        manufacture: new FormControl(null),
+        unit: new FormControl(null),
+        reservoir: new FormControl(null)
+    });
+    options$: BehaviorSubject<ISouOptions> = new BehaviorSubject<ISouOptions>({ manufactures: [] } as ISouOptions);
+    selectionOptions: ISouSelectionOptions = {
+        manufactures$: this.options$.pipe(
+            map((x) => {
+                return x.manufactures;
+            })
+        ),
+        units$: combineLatest([this.options$, this.optionsGroup.valueChanges]).pipe(
+            map(([options, group]) =>
+                options
+                    ?.manufactures
+                    ?.find((x) => x.name === group?.manufacture)
+                    ?.units
+                ?? []
+            )
+        ),
+        reservoirs$: combineLatest([this.options$, this.optionsGroup.valueChanges]).pipe(
+            map(
+                ([options, group]) =>
+                    options
+                        ?.manufactures
+                        ?.find((x) => x.name === group?.manufacture)
+                        ?.units
+                        ?.find((x) => x.id === group?.unit)
+                        ?.reservoirs
+                    ?? []
+            )
+        )
+    };
+    chosenSetting$: Observable<number>;
 
     public titlesOfTable: { name: string, widthOfBlock: string }[] = TITLES_OF_TABLE;
     public tableRows: {} = TABLE_CELLS;
