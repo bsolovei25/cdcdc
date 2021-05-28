8995f46cffa17ee44224a0a04a03baee0c357979
(А) Михаил Нимченко <nimchenko.m@gazgolder.ru>
2021-05-28 14:06:01 +0000

    feat(ЦМИД) Реализация виджета Карта производства МНПЗ

---
+++ src/app/widgets/CMID/cmid-mnpz-production-map/models/cmid-mnpz-production-map.interface.ts
@@ -0,0 +1,24 @@
+export interface ICmidMnpzProductionMapInterface {
+    builds: ICmidMnpzProductionMapInterfaceBuild[];
+    weather: ICmidMnpzProductionMapInterfaceWeather;
+}
+
+export interface ICmidMnpzProductionMapInterfaceBuild {
+    id: number,
+    name: string,
+    options: ICmidMnpzProductionMapInterfaceOptions;
+}
+
+export interface ICmidMnpzProductionMapInterfaceOptions {
+    safety: string,
+    reliability: string,
+    ecology: string
+}
+
+export interface ICmidMnpzProductionMapInterfaceWeather {
+    precipitation: number,
+    temperature: number,
+    pressure: number,
+    wind: number,
+    windDirection: string
+}
