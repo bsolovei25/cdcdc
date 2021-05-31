b75e2c8168567ddcebc57db526d9a225097fdcdd
Константин Медведев <medvedev.ko@gazprom-neft.com>
2021-05-31 07:26:11 +0300

    add animation

--- src/app/widgets/SOU/sou-streams/sou-streams.component.ts
+++ src/app/widgets/SOU/sou-streams/sou-streams.component.ts
@@ -5,20 +5,44 @@ import {
     ViewChild,
     ElementRef,
     AfterViewChecked, ChangeDetectorRef
-} from "@angular/core";
-import { WidgetService } from "@dashboard/services/widget.service";
-import { WidgetPlatform } from "@dashboard/models/@PLATFORM/widget-platform";
+} from '@angular/core';
+import { WidgetService } from '@dashboard/services/widget.service';
+import { WidgetPlatform } from '@dashboard/models/@PLATFORM/widget-platform';
 
-import { TITLES_OF_TABLE } from "@widgets/SOU/sou-streams/config";
-import { TABLE_CELLS } from "@widgets/SOU/sou-streams/mock";
+import { TITLES_OF_TABLE } from '@widgets/SOU/sou-streams/config';
+import { TABLE_CELLS } from '@widgets/SOU/sou-streams/mock';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 
 @Component({
-  selector: 'evj-sou-streams',
-  templateUrl: './sou-streams.component.html',
-  styleUrls: ['./sou-streams.component.scss']
+    selector: 'evj-sou-streams',
+    templateUrl: './sou-streams.component.html',
+    styleUrls: ['./sou-streams.component.scss'],
+    animations: [
+        trigger(
+            'inOutAnimation',
+            [
+                transition(
+                    ':enter',
+                    [
+                        style({ height: 0, opacity: 0, transform: 'translateY(-593px)' }),
+                        animate('1s ease-out',
+                            style({ height: 593, opacity: 1, transform: 'translateY(0px)' }))
+                    ]
+                ),
+                transition(
+                    ':leave',
+                    [
+                        style({ height: 300, opacity: 1 }),
+                        animate('1s ease-in',
+                            style({ height: 0, opacity: 0 }))
+                    ]
+                )
+            ]
+        )
+    ]
 })
-export class SouStreamsComponent extends WidgetPlatform implements OnInit, AfterViewChecked  {
+export class SouStreamsComponent extends WidgetPlatform implements OnInit, AfterViewChecked {
 
     public titlesOfTable: { name: string, bigBlock?: boolean }[] = TITLES_OF_TABLE;
     public tableRows: {} = TABLE_CELLS;
@@ -28,10 +52,12 @@ export class SouStreamsComponent extends WidgetPlatform implements OnInit, After
     public widthOfTable: string = '1943.2px';
     public widthOfGraphic: number = 70;
 
+    public showWorkspace: boolean = false;
+    public show: boolean = true;
+
     constructor(
         private cdr: ChangeDetectorRef,
         protected widgetService: WidgetService,
-
         @Inject('widgetId') public id: string,
         @Inject('uniqId') public uniqId: string
     ) {
@@ -43,6 +69,7 @@ export class SouStreamsComponent extends WidgetPlatform implements OnInit, After
     }
 
     @ViewChild('widget') child: ElementRef;
+
     ngAfterViewChecked(): void {
         this.heightOfTable = this.child.nativeElement.clientHeight - 40 + 'px';
         this.heightOfViewPort = this.child.nativeElement.clientHeight - 115 + 'px';
@@ -57,5 +84,23 @@ export class SouStreamsComponent extends WidgetPlatform implements OnInit, After
         this.cdr.detectChanges();
     }
 
-  protected dataHandler(ref: unknown): void {}
+    get stateName(): string {
+        return this.show ? 'show' : 'hide';
+    }
+
+    protected dataHandler(ref: unknown): void {
+    }
+
+    toggle(): void {
+        this.show = !this.show;
+    }
+
+    public addNewOperation(): void {
+        this.showWorkspace = true;
+        this.toggle();
+        const height: number = this.showWorkspace ? 593 : 0;
+        this.heightOfTable = this.child.nativeElement.clientHeight - 40 - height + 'px';
+        this.heightOfViewPort = this.child.nativeElement.clientHeight - 115 - height + 'px';
+        this.cdr.detectChanges();
+    }
 }
--- src/app/widgets/SOU/sou-streams/sou-streams.module.ts
+++ src/app/widgets/SOU/sou-streams/sou-streams.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SouStreamsComponent } from "@widgets/SOU/sou-streams/sou-streams.component";
-import { SharedModule } from "@shared/shared.module";
-import { MatSelectModule } from "@angular/material/select";
-import { AngularSvgIconModule } from "angular-svg-icon";
-import { ScrollingModule } from "@angular/cdk/scrolling";
-import { MatRadioModule } from "@angular/material/radio";
-
+import { SouStreamsComponent } from '@widgets/SOU/sou-streams/sou-streams.component';
+import { SharedModule } from '@shared/shared.module';
+import { MatSelectModule } from '@angular/material/select';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatRadioModule } from '@angular/material/radio';
+import { SouWorkspaceModule } from '@widgets/SOU/sou-workspace/sou-workspace.module';
 
 
 @NgModule({
-  declarations: [
-      SouStreamsComponent
-  ],
-  imports: [CommonModule, SharedModule, MatSelectModule, AngularSvgIconModule, ScrollingModule, MatRadioModule]
+    declarations: [
+        SouStreamsComponent
+    ],
+    imports: [CommonModule, SharedModule, MatSelectModule, AngularSvgIconModule, ScrollingModule, MatRadioModule, SouWorkspaceModule]
 })
 export class SouStreamsModule {
     enterComponent = SouStreamsComponent;
