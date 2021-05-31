d9a85402bf9d32617548069d957007566eab8572
TaisiiaFenz <51312876+TaisiiaFenz@users.noreply.github.com>
2021-05-31 18:30:55 +0300

    feat(SOU): provide chart. #1989

--- src/app/widgets/SOU/sou-streams/smart-trend/components/trends/mock.ts
+++ src/app/widgets/SOU/sou-streams/smart-trend/components/trends/mock.ts
@@ -1,5 +1,30 @@
 export const CHART_DATA = [
     {
+        graphType: 'normal',
+        graph: [
+            {
+                value: 223,
+                timeStamp: new Date(2011, 0, 1, 11, 30, 4, 567)
+            },
+            {
+                value: 228,
+                timeStamp: new Date(2011, 0, 1, 12, 15, 4, 567)
+            },
+            {
+                value: 210,
+                timeStamp: new Date(2011, 0, 1, 12, 45, 4, 567)
+            },
+            {
+                value: 184,
+                timeStamp: new Date(2011, 0, 1, 13, 15, 4, 567)
+            },
+            {
+                value: 210,
+                timeStamp: new Date(2011, 0, 1, 16, 45, 4, 567)
+            },
+        ]
+    },
+    {
         graphType: 'lowerBorder',
         graph: [
             {
