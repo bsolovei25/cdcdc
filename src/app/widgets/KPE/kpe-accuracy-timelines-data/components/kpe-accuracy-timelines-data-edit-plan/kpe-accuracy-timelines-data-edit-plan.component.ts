e3e69d55d9f49d19fa41efced4974248b9e8944f
(А) Вероника Лобань <loban.v@gazgolder.ru>
2021-06-04 12:38:38 +0000

    feat(KPE): Реализована форма для отправки корректировки для виджета Точность и своевременность исходных данных КПЭ. front#1405

--- src/app/widgets/KPE/kpe-accuracy-timelines-data/components/kpe-accuracy-timelines-data-edit-plan/kpe-accuracy-timelines-data-edit-plan.component.ts
+++ src/app/widgets/KPE/kpe-accuracy-timelines-data/components/kpe-accuracy-timelines-data-edit-plan/kpe-accuracy-timelines-data-edit-plan.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { KpeAccuracyTimelineDataService } from '@dashboard/services/widgets/KPE/kpe-accuracy-timeline-data.service';
+import { IKpeAccTimelinesDataEditPlan } from '@dashboard/models/KPE/kpe-accuracy-timelines-data.model';
+import { IAlertWindowModel } from '@shared/interfaces/alert-window.model';
 
 @Component({
   selector: 'evj-kpe-accuracy-timelines-data-edit-plan',
@@ -8,9 +11,55 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./kpe-accuracy-timelines-data-edit-plan.component.scss']
 })
 export class KpeAccuracyTimelinesDataEditPlanComponent implements OnInit {
-    constructor(@Inject(MAT_DIALOG_DATA) public data: { date: string }) {}
+    public correctionForm: FormGroup;
+    public alert: IAlertWindowModel = null;
 
-    ngOnInit(): void {}
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: { date: string },
+        private fb: FormBuilder,
+        private kpeAccuracyTimelineDataService: KpeAccuracyTimelineDataService,
+        private dialogRef: MatDialogRef<KpeAccuracyTimelinesDataEditPlanComponent>
+        ) {}
 
-    public dateControl: FormControl = new FormControl({ value: new Date(), disabled: false });
+    public ngOnInit(): void {
+        this.initForm();
+        this.initAlertWindow();
+    }
+
+    private initAlertWindow(): void {
+        this.alert = this.kpeAccuracyTimelineDataService.planCorrectionFormAlert;
+    }
+
+    private initForm(): void {
+        this.correctionForm = this.fb.group({
+            correctionText: [null, [Validators.required]],
+            correctionDate: [new Date()]
+        })
+    }
+
+    public submitForm(): void {
+        if (this.correctionForm.invalid) {
+            this.openWarningModal();
+        } else {
+            this.sendPlanCorrection();
+        }
+    }
+
+    private openWarningModal(): void {
+        this.alert.isShow = true;
+
+        this.alert.acceptFunction = () => {
+            this.dialogRef.close()
+        }
+    }
+
+    private sendPlanCorrection(): void {
+        const dataToSend: IKpeAccTimelinesDataEditPlan = {
+            adjustmentComment: this.correctionForm.value.correctionText,
+            dateOfCreation: new Date().toISOString(),
+            dateOfCorrection: this.correctionForm.value.correctionDate.toISOString()
+        };
+
+        this.kpeAccuracyTimelineDataService.submitPlanCorrection(dataToSend).then(() => this.dialogRef.close());
+    }
 }
