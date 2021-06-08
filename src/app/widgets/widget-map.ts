d8fd37e321c82970e9049fef7359d423e21e9aac
(А) Денис Яринских <yarinskih.d@gazgolder.ru>
2021-06-08 08:59:07 +0000

    fix(ЦМИД): Рефакторинг. front#1784-

--- src/app/widgets/widget-map.ts
+++ src/app/widgets/widget-map.ts
@@ -1835,7 +1835,7 @@ export const WIDGETS_LAZY = {
         itemCols: 21,
         itemRows: 12,
         minItemCols: 18,
-        minItemRows: 5,
+        minItemRows: 8,
         preview: 'default',
     },
     'cmid-number-of-outages': {
