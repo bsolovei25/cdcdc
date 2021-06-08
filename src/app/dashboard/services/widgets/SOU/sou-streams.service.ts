ecfb1f311c38673d9b868dc245d213d234c35449
TaisiiaFenz <51312876+TaisiiaFenz@users.noreply.github.com>
2021-06-08 17:58:43 +0300

    feat(SOU): add start/end time for url. #1931

--- src/app/dashboard/services/widgets/SOU/sou-streams.service.ts
+++ src/app/dashboard/services/widgets/SOU/sou-streams.service.ts
@@ -32,9 +32,12 @@ export class SouStreamsService {
     }
 
     public async getTableContent(startTime: string, endTime: string): Promise<ISouStreamsTableContent[]> {
+        // startTime = 2020-02-13 endTime = 2020-02-15
         const params = new HttpParams()
             .set('startTime', startTime)
             .set('endTime', endTime);
-        return await this.http.get<ISouStreamsTableContent[]>(this.restUrl + `/api/file-storage/info`, {params}).toPromise();
+        return await this.http.get<ISouStreamsTableContent[]>(this.restUrl +
+            `/api/Oms/transfer/${startTime}&${endTime}isOpen=false`,
+            {params}).toPromise();
     }
 }
