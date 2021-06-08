881c01acc49105d2c8ba773be130651342d57f93
(А) Федор Девиченский <devitchenskiy.fa@gazgolder.ru>
2021-06-08 06:05:25 +0000

    feat(SUUTP) Реализация верстки виджета Табличного отображения #1958

--- src/app/widgets/SUUTP/suutp-complexes-table/suutp-complexes-table.module.ts
+++ src/app/widgets/SUUTP/suutp-complexes-table/suutp-complexes-table.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SuutpComplexesTableComponent } from './suutp-complexes-table.component';
-import { SuutpHeaderComponent } from './components/suutp-header/suutp-header.component';
 import { MatTableModule } from '@angular/material/table';
 import { SharedModule } from '@shared/shared.module';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { SUUTPSharedModule } from "@widgets/SUUTP/shared/shared.module";
 
 @NgModule({
-  declarations: [SuutpComplexesTableComponent, SuutpHeaderComponent],
-    imports: [CommonModule, MatTableModule, SharedModule, AngularSvgIconModule],
+  declarations: [SuutpComplexesTableComponent],
+    imports: [CommonModule, MatTableModule, SharedModule, AngularSvgIconModule, SUUTPSharedModule],
 })
 export class SuutpComplexesTableModule {
     enterComponent = SuutpComplexesTableComponent;
